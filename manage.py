80fdffd4244f4206068f3ce3c6a36a6880251a76
Jeff Balogh <jbalogh@mozilla.com>
2010-12-15 15:43:43 -0800

    add a vendor lib

--- manage.py
+++ manage.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+import os
+import site
+import sys
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+path = lambda *a: os.path.join(ROOT,*a)
+
+
+# Adjust the python path and put local packages in front.
+prev_sys_path = list(sys.path)
+
+site.addsitedir(path('apps'))
+site.addsitedir(path('lib'))
+site.addsitedir(path('vendor'))
+site.addsitedir(path('vendor/lib/python'))
+
+# Move the new items to the front of sys.path. (via virtualenv)
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
