5d3b97a035994b824d56d22bec1065105f46b76f
Jeff Balogh <jbalogh@mozilla.com>
2010-12-16 14:01:10 -0800

    contenttypes is needed for syncdb

--- settings.py
+++ settings.py
@@ -83,7 +83,7 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
-    # 'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
     # 'django.contrib.sessions',
     # 'django.contrib.sites',
     # 'django.contrib.messages',
