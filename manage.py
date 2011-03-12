6facd54d00704bb76070df6a2c44c5054107ecbb
Fred Wenzel <fwenzel@mozilla.com>
2011-03-12 13:36:21 -0500

    Typo in vendor-local dir

--- manage.py
+++ manage.py
@@ -13,11 +13,11 @@ prev_sys_path = list(sys.path)
 site.addsitedir(path('apps'))
 site.addsitedir(path('lib'))
 
-# Global (upstream) vendor library
-site.addsitedir(path('vendor'))
-site.addsitedir(path('vendor/lib/python'))
-
 # Local (project) vendor library
+site.addsitedir(path('vendor-local'))
+site.addsitedir(path('vendor-local/lib/python'))
+
+# Global (upstream) vendor library
 site.addsitedir(path('vendor'))
 site.addsitedir(path('vendor/lib/python'))
 
