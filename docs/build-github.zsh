197eafc377a9da92ab004a6abf7f3768ee048e37
Fred Wenzel <fwenzel@mozilla.com>
2011-06-03 14:27:10 -0700

    gh-pages build script cleanup.

--- docs/build-github.zsh
+++ docs/build-github.zsh
@@ -3,7 +3,8 @@
 # Should be run from the docs directory: (cd docs && ./build-github.zsh)
 
 REPO=$(git config remote.origin.url)
-GH=_gh-pages
+HERE=$(dirname $0)
+GH=$HERE/_gh-pages
 
 
 # Checkout the gh-pages branch, if necessary.
@@ -15,16 +16,22 @@ if [[ ! -d $GH ]]; then
 fi
 
 # Update and clean out the _gh-pages target dir.
-pushd $GH && git pull && rm -rf * && popd
+pushd $GH
+git pull && rm -rf *
+popd
 
 # Make a clean build.
+pushd $HERE
 make clean dirhtml
 
 # Move the fresh build over.
 cp -r _build/dirhtml/* $GH
-cd $GH
+pushd $GH
 
 # Commit.
 git add .
 git commit -am "gh-pages build on $(date)"
 git push origin gh-pages
+
+popd
+popd
