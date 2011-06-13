09db014d2ea91bfde8ee61e85c081ca135166110
Fred Wenzel <fwenzel@mozilla.com>
2011-06-13 16:11:38 -0700

    Added Bleach to the standard set of libraries. Closes issue #42.

--- apps/examples/urls.py
+++ apps/examples/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('examples.views',
     url(r'^$', 'home', name='examples.home'),
+    url(r'^bleach/?$', 'bleach_test', name='examples.bleach'),
 )
--- apps/examples/views.py
+++ apps/examples/views.py
@@ -1,10 +1,29 @@
 """Example views. Feel free to delete this app."""
 
 from django import http
+from django.views.decorators.csrf import csrf_exempt
 
+import bleach
 import jingo
 
 
 def home(request):
-    data = {}
+    """Main example view."""
+    data = {}  # You'd add data here that you're sending to the template.
     return jingo.render(request, 'examples/home.html', data)
+
+
+@csrf_exempt
+def bleach_test(request):
+    """A view outlining bleach's input sanitization."""
+    allowed_tags = ('strong', 'em')
+
+    data = {}
+
+    if request.method == 'POST':
+        bleachme = request.POST.get('bleachme', None)
+        data['bleachme'] = bleachme
+        if bleachme:
+            data['bleached'] = bleach.clean(bleachme, tags=allowed_tags)
+
+    return jingo.render(request, 'examples/bleach.html', data)
