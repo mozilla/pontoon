3ee026c598fa3b69632e1d62f7965202f03e2591
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-07-21 11:45:18 -0500

    Fixes example app settings which were meant to live in playdoh

---
+++ settings.py
@@ -0,0 +1,46 @@
+# This is your project's main settings file that can be committed to your
+# repo. If you need to override a setting locally, use settings_local.py
+
+from funfactory.settings_base import *
+
+# Logging
+SYSLOG_TAG = "http_app_playdoh"  # Make this unique to your project.
+
+
+# Bundles is a dictionary of two dictionaries, css and js, which list css files
+# and js files that can be bundled together by the minify app.
+MINIFY_BUNDLES = {
+    'css': {
+        'example_css': (
+            'css/examples/main.css',
+        ),
+    },
+    'js': {
+        'example_js': (
+            'js/libs/jquery-1.4.4.min.js',
+        ),
+    }
+}
+
+
+INSTALLED_APPS = list(INSTALLED_APPS) + [
+    # Example code. Can (and should) be removed for actual projects.
+    'examples',
+]
+
+
+# Tells the extract script what files to look for L10n in and what function
+# handles the extraction. The Tower library expects this.
+
+# # Use this if you have localizable HTML files:
+# DOMAIN_METHODS['lhtml'] = [
+#    ('**/templates/**.lhtml',
+#        'tower.management.commands.extract.extract_tower_template'),
+# ]
+
+# # Use this if you have localizable HTML files:
+# DOMAIN_METHODS['javascript'] = [
+#    # Make sure that this won't pull in strings from external libraries you
+#    # may use.
+#    ('media/js/**.js', 'javascript'),
+# ]
