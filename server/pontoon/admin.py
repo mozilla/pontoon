8a04f28b54c93f64949220ce725f378d7464a6a3
Matjaž Horvat <matjaz.horvat@gmail.com>
2011-08-31 20:17:08 +0200

    Model and app admin.

---
+++ server/pontoon/admin.py
@@ -0,0 +1,4 @@
+from pontoon.models import Project
+from django.contrib import admin
+
+admin.site.register(Project)
--- server/pontoon/urls.py
+++ server/pontoon/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('apps.pontoon.views',
+urlpatterns = patterns('pontoon.views',
     (r'^$', 'stats'),
     (r'^push/', 'push'),
 )
