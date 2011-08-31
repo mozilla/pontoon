524f0ec5f0491ec27e3738f99779d7cbdf00fa6c
Matjaž Horvat <matjaz.horvat@gmail.com>
2011-08-31 19:46:32 +0200

    Design URLs

--- server/urls.py
+++ server/urls.py
@@ -4,11 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
-    # url(r'^server/', include('server.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('pontoon.urls')),
 )
