fb356d6207efc687dac68d41c04c8f3624ccb13f
Matjaž Horvat <matjaz.horvat@gmail.com>
2011-10-06 12:42:19 +0200

    CSP is not what I'm looking for.

--- projects/testpilot/index.php
+++ projects/testpilot/index.php
@@ -1,7 +1,3 @@
-<?php
-header("X-Content-Security-Policy: allow 'self'; options inline-script; frame-ancestors *; xhr-src *");
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php require_once('../../hook/php/pontoon.php'); ?>
