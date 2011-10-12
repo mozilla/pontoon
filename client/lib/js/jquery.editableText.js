470df6a9071d3f6af22040492f966d831976d603
Matjaž Horvat <matjaz.horvat@gmail.com>
2011-10-12 02:41:29 +0200

    Removing lots of useless plugin code

--- client/lib/js/jquery.editableText.js
+++ client/lib/js/jquery.editableText.js
@@ -1,151 +1,137 @@
 /**
- * editableText plugin that uses contentEditable property
- * Copyright (c) 2009 Andris Valums, http://valums.com
- * Licensed under the MIT license (http://valums.com/mit-license/)
- */
- (function(){
+* editableText plugin that uses contentEditable property
+* Copyright (c) 2009 Andris Valums, http://valums.com
+* Licensed under the MIT license (http://valums.com/mit-license/)
+*/
+(function(){
+  // The dollar sign could be overwritten globally, but jQuery should always stay accesible
+  var $ = jQuery;
 
-      // The dollar sign could be overwritten globally, but jQuery should always stay accesible
-      var $ = jQuery;
+  // Usage $('selector).editableText(optionArray);
+  $.fn.editableText = function(){
 
-      // Extending jQuery namespace, we could add public methods here
-      $.editableText = {};
-      $.editableText.defaults;
-
-      // Usage $('selector).editableText(optionArray);
-      $.fn.editableText = function(){
-          var doc = this.get(0).ownerDocument,
-              body = $(doc.body);
-
-          // Create edit/save buttons
-          function showToolbar(elem) {
-            if ($(elem).attr('class') === 'editableToolbar') {
-              var toolbar = $(elem),
-                  curTarget =  toolbar.get(0).target,
-                  newTarget = null;
-              curTarget.entity.hover();
-              return true;
-            } else {       
-              var body = $($(elem).get(0).ownerDocument.body),
-                  win = $(elem).get(0).ownerDocument.defaultView,
-                  toolbar = body.find('.editableToolbar'),
-                  curTarget =  toolbar.get(0).target,
-                  newTarget = elem;
-              if ($(curTarget).attr('contentEditable') === 'true') {
-                return;
-              }
-              if (curTarget && curTarget !== newTarget) {
-                hideToolbar(curTarget);
-              }
-              var left = newTarget.getBoundingClientRect().left + win.scrollX,
-                  top = newTarget.getBoundingClientRect().top + win.scrollY;
-              toolbar.css('left', left + 'px')
-                     .css('top', top-21 + 'px');
-            }           
-            var toolbarNode = toolbar.get(0);
-            if (toolbarNode.I !== null) {
-              clearTimeout(toolbarNode.I);
-              toolbarNode.I = null;
-            }
-            if (newTarget) {
-              toolbarNode.target = newTarget;
-            }
-            $(newTarget).addClass('hovered');
-            toolbar.css('display', 'block');
-          }
-
-          function hideToolbar(elem) {
-            if ($(elem).attr('class') === 'editableToolbar') {
-              var toolbar = $(elem);
-            } else {
-              var body = $($(elem).get(0).ownerDocument.body),
-                  toolbar = body.find('.editableToolbar');
-            }
-            var toolbarNode = toolbar.get(0),
-                target = toolbarNode.target;
-            if ($(target).attr('contentEditable') === 'true') {
-              return;
-            }
-            function hide() {
-              if (target) {
-                target.blur();
-                stopEditing(toolbar);
-                if (target === toolbar.get(0).target) {
-                  toolbar.get(0).target = null;
-                  $(target).removeClass('hovered');
-                  toolbar.css('display', 'none');
-                } else {
-                  $(target).removeClass('hovered');
-                }
-              }
-            }
-            toolbar.get(0).I = setTimeout(hide, 50);
-          }
-
-          /**
-           * Makes element editable
-           */
-          function startEditing(toolbar) {
-            toolbar.children().css('display','inline');
-            toolbar.find('.edit').css('display','none');
-            var target = toolbar.get(0).target;
-            $(target).attr('contentEditable', true);
-            target.focus();
-            $(target.entity.ui).addClass("active");
-          }
-          /**
-           * Makes element non-editable
-           */
-          function stopEditing(toolbar) {
-            toolbar.children().css('display','none');
-            toolbar.find('.edit').css('display','inline');
-            var target = toolbar.get(0).target;
-            $(target).attr('contentEditable', false);
-            $(target.entity.ui).removeClass("active");
-          }
-          if (!body.find('.editableToolbar').length) {
-            var toolbar = $(
-                  "<div class='editableToolbar'>" +
-                    "<a href='#' class='edit'></a>" +
-                    "<a href='#' class='save'></a>" +
-                    "<a href='#' class='cancel'></a>" +
-                "</div>", doc).appendTo(body);
-            toolbar.hover(function() {
-              showToolbar(this);
-            },function() {
-             this.target.entity.unhover()
-            })
-
-            // Save references and attach events            
-            var editEl = toolbar.find('.edit').click(function() {
-                startEditing(toolbar);
-                return false;
-            });                            
-
-            toolbar.find('.save, .cancel').click(function(){
-                stopEditing(toolbar);
-                return false;
-            });
-
-            // Display only edit button            
-            toolbar.children().css('display', 'none');
-            editEl.show();
-
-          }
-          return this.each(function(){
-               // Add jQuery methods to the element
-              var editable = $(this);
-
-              // Save value to restore if user presses cancel
-              this.prevValue = editable.html().toString();
-              this.txtPrevValue = editable.text().toString();
-              this.showToolbar = function(){showToolbar(this)}
-              this.hideToolbar = function(){hideToolbar(this)}
-              editable.hover(function() {
-                this.entity.hover();
-              },function() {
-                this.entity.unhover();
-              });
-          });
+    // Show toolbar
+    function showToolbar(element) {
+      if ($(element).is('.editableToolbar')) {
+        $(element).get(0).target.entity.hover();
+        return true;
+      } else {       
+        var toolbar = $('.editableToolbar'),
+            curTarget = toolbar.get(0).target,
+            newTarget = element;
+        if ($(curTarget).attr('contentEditable') === 'true') {
+          return;
+        }
+        if (curTarget && curTarget !== newTarget) {
+          hideToolbar(curTarget);
+        }
+        var left = newTarget.getBoundingClientRect().left + window.scrollX,
+            top = newTarget.getBoundingClientRect().top + window.scrollY;
+        toolbar.css('left', left + 'px')
+               .css('top', top-21 + 'px');
+      }           
+      var toolbarNode = toolbar.get(0);
+      if (toolbarNode.I !== null) {
+        clearTimeout(toolbarNode.I);
+        toolbarNode.I = null;
       }
-  })();
+      if (newTarget) {
+        toolbarNode.target = newTarget;
+      }
+      $(newTarget).addClass('hovered');
+      toolbar.show();
+    }
+
+    // Hide toolbar
+    function hideToolbar(element) {
+      if ($(element).is('.editableToolbar')) {
+        var toolbar = $(element);
+      } else {
+        var toolbar = $('.editableToolbar');
+      }
+      var toolbarNode = toolbar.get(0),
+          target = toolbarNode.target;
+      if ($(target).attr('contentEditable') === 'true') {
+        return;
+      }
+      function hide() {
+        if (target) {
+          target.blur();
+          stopEditing(toolbar);
+          if (target === toolbar.get(0).target) {
+            toolbar.get(0).target = null;
+            $(target).removeClass('hovered');
+            toolbar.hide();
+          } else {
+            $(target).removeClass('hovered');
+          }
+        }
+      }
+      toolbar.get(0).I = setTimeout(hide, 50);
+    }
+
+    // Makes element editable
+    function startEditing(toolbar) {
+      toolbar.children().show().end()
+        .find('.edit').hide();
+      var target = toolbar.get(0).target;
+      $(target).attr('contentEditable', true);
+      $(target.entity.ui).addClass("active");
+      target.focus();
+    }
+
+    // Makes element non-editable
+    function stopEditing(toolbar) {
+      toolbar.children().hide().end()
+        .find('.edit').show();
+      var target = toolbar.get(0).target;
+      $(target).attr('contentEditable', false);
+      $(target.entity.ui).removeClass("active");
+    }
+
+    if (!$('.editableToolbar').length) {
+      // Define toolbar
+      var toolbar = $(
+        "<div class='editableToolbar'>" +
+          "<a href='#' class='edit'></a>" +
+          "<a href='#' class='save'></a>" +
+          "<a href='#' class='cancel'></a>" +
+        "</div>").appendTo($('body'));
+
+      // Attach events            
+      toolbar.hover(function () {
+        showToolbar(this);
+      }, function () {
+        this.target.entity.unhover();
+      })
+      .find('.edit').click(function () {
+        startEditing(toolbar);
+        return false;
+      }).end()
+      .find('.save, .cancel').click(function () {
+        stopEditing(toolbar);
+        return false;
+      });
+    }
+
+    return this.each(function () {
+      // Save value to restore if user presses cancel
+      this.prevValue = $(this).html().toString();
+
+      // Show/hide toolbar
+      this.showToolbar = function () {
+        showToolbar(this);
+      }
+      this.hideToolbar = function () {
+        hideToolbar(this);
+      }
+
+      // Hover handler
+      $(this).hover(function () {
+        this.entity.hover();
+      }, function() {
+        this.entity.unhover();
+      });
+    });
+  }
+})();
