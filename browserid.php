0a7fc9cc9a4d5dc8f253a279dc35f5f63ae222d0
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-01-26 21:34:49 +0100

    BrowserID assertion verification through PHP POST request

--- browserid.php
+++ browserid.php
@@ -1,17 +1,11 @@
 <?php
   /* Verify BrowserID assertion */ 
-  $url = 'https://browserid.org/verify';
-
-  $data = array(
-    'assertion' => $_GET['assertion'],
-    'audience' => $_GET['audience']
-  );
 
   $params = array('http' => array(
     'method' => 'POST',
-    'content' => $data,
-    'header' => 'Content-Type: application/json'              
+    'content' => 'assertion='.$_GET['assertion'].'&audience='.$_GET['audience']
   ));
-  $fp = @fopen($url, 'r', false, stream_context_create($params));  
-  echo @stream_get_contents($fp);  
+
+  $stream = fopen('https://browserid.org/verify', 'r', false, stream_context_create($params));  
+  echo stream_get_contents($stream);  
 ?>
