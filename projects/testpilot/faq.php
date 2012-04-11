406952112ec807ef1c1267f0e9ffdce4195c35e5
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-04-12 00:20:02 +0200

    Rename hook to hooks

--- projects/testpilot/faq.php
+++ projects/testpilot/faq.php
@@ -2,7 +2,7 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<?php require_once('../../hook/php/pontoon.php'); ?>
+	<?php require_once('../../hooks/php/pontoon.php'); ?>
 	<?php Pontoon::header_tags('testpilot', 'index', 'pontoon.json');?>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title><?php echo _w('Mozilla Labs Test Pilot')?></title>
