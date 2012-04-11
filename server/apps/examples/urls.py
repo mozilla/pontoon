99bd5afb65a15db54edde1a997903ce8878a6b27
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-04-11 23:33:24 +0200

    Move server folder contents to /

--- server/apps/examples/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-from . import views
-
-
-urlpatterns = patterns('',
-    url(r'^$', views.home, name='examples.home'),
-    url(r'^bleach/?$', views.bleach_test, name='examples.bleach'),
-)
--- server/apps/pontoon/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from . import views
-
-
-urlpatterns = patterns('',
-    url(r'^$', views.home, name='pontoon.home'),
-)
