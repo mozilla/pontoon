c4a9dceccc611e46987cd25ede99a7297e45dc35
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-04-12 11:01:59 +0200

    New include script URL

--- media/js/app/script.js
+++ media/js/app/script.js
@@ -154,7 +154,7 @@
 
     // Set include script URL
     $("#install")
-      .find("code").html('&lt;script src="' + window.location.href.split("?")[0] + 'client/lib/js/pontoon.js"&gt;&lt;/script&gt;')
+      .find("code").html('&lt;script src="' + window.location.href.split("?")[0] + 'media/js/project/pontoon.js"&gt;&lt;/script&gt;')
       .end().css("visibility", "visible");
 
     // Set locale
