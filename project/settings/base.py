d30da7e0d3ecc99383d806303d80aa288d2dd42d
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-04-27 10:52:32 -0500

    Fixed translation messages search path (issue 106)
    Also made projects module settable in a single var

--- project/settings/base.py
+++ project/settings/base.py
@@ -3,6 +3,12 @@
 
 from funfactory.settings_base import *
 
+# Name of the top-level module where you put all your apps.
+# If you did not install Playdoh with the funfactory installer script
+# you may need to edit this value. See the docs about installing from a
+# clone.
+PROJECT_MODULE = 'project'
+
 # Bundles is a dictionary of two dictionaries, css and js, which list css files
 # and js files that can be bundled together by the minify app.
 MINIFY_BUNDLES = {
@@ -24,13 +30,13 @@ MINIFY_BUNDLES = {
 }
 
 # Defines the views served for root URLs.
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = '%s.urls' % PROJECT_MODULE
 
 INSTALLED_APPS = list(INSTALLED_APPS) + [
     # Application base, containing global templates.
-    'project.base',
+    '%s.base' % PROJECT_MODULE,
     # Example code. Can (and should) be removed for actual projects.
-    'project.examples',
+    '%s.examples' % PROJECT_MODULE,
 ]
 
 
@@ -43,6 +49,14 @@ JINGO_EXCLUDE_APPS = [
 
 # Tells the extract script what files to look for L10n in and what function
 # handles the extraction. The Tower library expects this.
+DOMAIN_METHODS['messages'] = [
+    ('%s/**.py' % PROJECT_MODULE,
+        'tower.management.commands.extract.extract_tower_python'),
+    ('%s/**/templates/**.html' % PROJECT_MODULE,
+        'tower.management.commands.extract.extract_tower_template'),
+    ('templates/**.html',
+        'tower.management.commands.extract.extract_tower_template'),
+],
 
 # # Use this if you have localizable HTML files:
 # DOMAIN_METHODS['lhtml'] = [
@@ -50,7 +64,7 @@ JINGO_EXCLUDE_APPS = [
 #        'tower.management.commands.extract.extract_tower_template'),
 # ]
 
-# # Use this if you have localizable HTML files:
+# # Use this if you have localizable JS files:
 # DOMAIN_METHODS['javascript'] = [
 #    # Make sure that this won't pull in strings from external libraries you
 #    # may use.
