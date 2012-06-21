7fefc6e2baab8bb174736d996015972bc823cd5f
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-06-21 16:43:38 +0200

    Changing source, impossible to handle using comment nodes as identifiers

--- hooks/php/test/testpilot/principles.php
+++ hooks/php/test/testpilot/principles.php
@@ -68,7 +68,7 @@
 			
 			</div>
 			
-<div id="footer"><img class="mozLogo" src="images/mozilla-logo.png"><?php echo _w('Copyright &copy; 2005-2009 Mozilla. All rights reserved. &nbsp; &nbsp; <a href="http://labs.mozilla.com/">Mozilla Labs</a> &nbsp; &nbsp; <a href="http://www.mozilla.com/en-US/privacy-policy.html">Privacy Policy</a> &nbsp; &nbsp; <a href="http://www.mozilla.com/en-US/about/legal.html">Legal Notices</a>')?></div>
+<div id="footer"><?php echo _w('<img class="mozLogo" src="images/mozilla-logo.png">Copyright &copy; 2005-2009 Mozilla. All rights reserved. &nbsp; &nbsp; <a href="http://labs.mozilla.com/">Mozilla Labs</a> &nbsp; &nbsp; <a href="http://www.mozilla.com/en-US/privacy-policy.html">Privacy Policy</a> &nbsp; &nbsp; <a href="http://www.mozilla.com/en-US/about/legal.html">Legal Notices</a>')?></div>
 
 		
 			
