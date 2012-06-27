fedb7edf92f0b54320d3dc4159bb40c2deaabbee
Matjaž Horvat <matjaz.horvat@gmail.com>
2012-06-28 01:50:30 +0200

    Prevent JS errors on fast mouse moves

--- pontoon/base/static/js/project/pontoon.js
+++ pontoon/base/static/js/project/pontoon.js
@@ -396,6 +396,9 @@
         toolbar.children().hide().end()
           .find('.edit').show();
         var target = toolbar.get(0).target;
+        if (!target) {
+          return;
+        }
         $(target).attr('contentEditable', false);
         postMessage("INACTIVE", target.entity.id);
       }
