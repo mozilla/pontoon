7d9ef2b08d468d56d72166d53a3163d60d2f93a6
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-14 12:18:40 +0100

    Django 1.5 compatibility

--- pontoon/urls.py
+++ pontoon/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 
 from funfactory.monkeypatches import patch
 patch()
@@ -23,14 +25,14 @@ urlpatterns = patterns('',
     url(r'^signout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='signout'),
 
     # 404 and 500
-    url(r'^404/$', 'django.views.generic.simple.direct_to_template', {'template': '404.html'}),
-    url(r'^500/$', 'django.views.generic.simple.direct_to_template', {'template': '500.html'}),
+    url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+    url(r'^500/$', TemplateView.as_view(template_name='500.html')),
 
     # Favicon
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/img/favicon.ico'}),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
 
     # Include script
-    url(r'^pontoon\.js$', 'django.views.generic.simple.redirect_to', {'url': '/static/js/pontoon.js'}),
+    url(r'^pontoon\.js$', RedirectView.as_view(url='/static/js/pontoon.js')),
 
     # Django admin
     (r'^a/', include(admin.site.urls)),
