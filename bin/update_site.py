7584ca64ef6a3dc35cf73f2fb0e13acb3347dfc2
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-20 14:11:09 +0100

    Use south instead of schematic for migrations

--- bin/update_site.py
+++ bin/update_site.py
@@ -87,7 +87,7 @@ def update_site(env, debug):
         (EXEC,  GIT_PULL % vendor_branch),
         (EXEC,  GIT_SUBMODULE),
         (CHDIR, os.path.join(here)),
-        (EXEC, 'python2.6 vendor/src/schematic/schematic migrations/'),
+        (EXEC, 'python2.6 manage.py migrate'),
         (EXEC, 'python2.6 manage.py collectstatic --noinput'),
         # un-comment if you haven't moved to django-compressor yet
         #(EXEC, 'python2.6 manage.py compress_assets'),
