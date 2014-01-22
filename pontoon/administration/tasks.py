5e0dde9e6bb6dc8f24c4eb47e63c5dd68ede809c
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-22 18:48:42 +0100

    Use master path as the argument, which is crucial for single-locale repositories

--- pontoon/administration/tasks.py
+++ pontoon/administration/tasks.py
@@ -1,7 +1,9 @@
 
 import commonware
+import os
 
 from celery.task import task
+from django.conf import settings
 from pontoon.base.models import Project
 from pontoon.administration.views import _update_from_repository
 
@@ -15,7 +17,9 @@ def update_projects_from_repository():
         try:
             repository_type = project.repository_type
             repository_url = project.repository
-            repository_path_master = project.repository_path
+            repository_path_master = os.path.join(settings.MEDIA_ROOT,
+                repository_type, project.name)
+
             _update_from_repository(
                 project, repository_type, repository_url,
                 repository_path_master)
