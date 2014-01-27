6b9c297c0fa2aebb5b9a4b6f9421b78a6a91946e
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-27 22:49:43 +0100

    Return proper forbidden response status (403) instead of redirecting

--- pontoon/urls.py
+++ pontoon/urls.py
@@ -25,9 +25,9 @@ urlpatterns = patterns('',
     url(r'^signout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='signout'),
 
     # Error pages
-    url(r'^403/$', TemplateView.as_view(template_name='403.html'), name='403'),
-    url(r'^404/$', TemplateView.as_view(template_name='404.html'), name='404'),
-    url(r'^500/$', TemplateView.as_view(template_name='500.html'), name='500'),
+    url(r'^403/$', TemplateView.as_view(template_name='403.html')),
+    url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+    url(r'^500/$', TemplateView.as_view(template_name='500.html')),
 
     # Favicon
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
