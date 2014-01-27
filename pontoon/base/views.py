d37486134a4316270af38c197c8f0b330d573f23
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-01-27 18:35:09 +0100

    Reset entities on each file in a loop

--- pontoon/base/views.py
+++ pontoon/base/views.py
@@ -516,8 +516,8 @@ def _update_files(p, locale, locale_repository_path):
                 l10nobject = format_parser.get_structure(f.read())
 
                 short_path = '/' + path.split('/' + locale.code + '/')[-1]
-                entities = entities.filter(source=short_path)
-                for entity in entities:
+                entities_with_path = entities.filter(source=short_path)
+                for entity in entities_with_path:
                     key = entity.key
                     translations = Translation.objects.filter(entity=entity, locale=locale).order_by('date')
                     if len(translations) == 0:
