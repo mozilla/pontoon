152f6bf3b836fbfdfab39425743c378e2c022173
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-02-20 16:48:09 +0100

    Use public variable instead of selector

--- pontoon/base/static/js/main.js
+++ pontoon/base/static/js/main.js
@@ -818,7 +818,7 @@ var Pontoon = (function () {
             Pontoon.openEditor(entity);
           }
         } else if (message.type === "INACTIVE") {
-          if ($("#sidebar:not('.advanced') #editor").is('.opened')) {
+          if (!this.app.external && $("#editor").is('.opened')) {
             $('#cancel').click();
             $('#editor')[0].entity.ui.removeClass('hovered');
           }
