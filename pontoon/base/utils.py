97d5d446845dab5dcd77dcbbedf98ed428e84b78
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-05-23 18:21:08 +0200

    Much optimized Mozillians request

--- pontoon/base/utils.py
+++ pontoon/base/utils.py
@@ -14,33 +14,31 @@ log = commonware.log.getLogger('pontoon')
 
 def add_can_localize(user):
 
+    email = user.email
+    log.debug(email)
+
     # Grant permission to Mozilla localizers
     url = "https://mozillians.org/api/v1/users/"
     payload = {
         "app_name": "pontoon",
         "app_key": settings.MOZILLIANS_API_KEY,
+        "email": email,
+        "is_vouched": True,
         "groups": "localization",
-        "format": "json",
-        "limit": 2000,  # By default, limited to 20
-        "is_vouched": True
     }
 
     try:
-        mozillians = requests.get(url, params=payload, timeout=20)
-        email = user.email
-        log.debug(email)
+        response = requests.get(url, params=payload)
+        mozillians = response.json()["objects"]
 
-        for mozillian in mozillians.json()["objects"]:
-            if email == mozillian["email"]:
+        if len(mozillians) > 0:
+            can_localize = Permission.objects.get(codename="can_localize")
+            user.user_permissions.add(can_localize)
+            log.debug("Permission can_localize set.")
 
-                can_localize = Permission.objects.get(codename="can_localize")
-                user.user_permissions.add(can_localize)
-                log.debug("Permission can_localize set.")
-
-                # Fallback if profile does not allow accessing data
-                user.first_name = mozillian.get("full_name", email)
-                user.save()
-                break
+            # Fallback if profile does not allow accessing data
+            user.first_name = mozillians[0].get("full_name", email)
+            user.save()
 
     except Exception as e:
         log.debug(e)
