1af951de8cc04fb86acce1dd01bc00191f59bcd2
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-05-30 19:07:49 +0200

    Comments make more sense now

--- pontoon/base/static/js/locale.js
+++ pontoon/base/static/js/locale.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  // Edit project if selected from the menu
+  // Translate selected project
   $('.project .menu li').unbind('click.main').bind('click', function (e) {
     e.preventDefault();
     window.location = $('#server').data('locale') + '/' + $(this).find('.project-name').data('slug');
--- pontoon/base/static/js/project.js
+++ pontoon/base/static/js/project.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  // Edit project if selected from the menu
+  // Translate selected locale
   $('.locale .menu li').unbind('click.main').bind('click', function (e) {
     e.preventDefault();
     window.location = $(this).find('.code').html() + '/' + $('#server').data('project');
