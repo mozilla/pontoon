59c644120292c1bb55f25423d50c6f6d5c69994c
Matjaž Horvat <matjaz.horvat@gmail.com>
2014-11-29 17:21:54 +0100

    Limit maximum number of characters per line

--- pontoon/base/static/js/locale.js
+++ pontoon/base/static/js/locale.js
@@ -3,7 +3,8 @@ $(function() {
   // Translate selected project
   $('.project .menu li').unbind('click.main').bind('click', function (e) {
     e.preventDefault();
-    window.location = $('#server').data('locale') + '/' + $(this).find('.name').data('slug');
+    window.location = $('#server')
+      .data('locale') + '/' + $(this).find('.name').data('slug');
   });
 
 });
