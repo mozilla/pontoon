b7bf48e49c94e6d0a75f34fd9625a10429a761c5
Michael Kelly <mkelly@mozilla.com>
2015-04-22 01:27:54 -0700

    Move WSGI file and update it to Django template.
    Updates the WSGI file to look like the WSGI file generated by recent
    Django versions.

---
+++ pontoon/wsgi.py
@@ -0,0 +1,13 @@
+"""
+WSGI config for Pontoon.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontoon.settings')
+application = get_wsgi_application()
