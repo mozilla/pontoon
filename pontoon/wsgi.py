c133ce78513554f98ae58cbada8e81310a78f673
Michael Kelly <mkelly@mozilla.com>
2015-04-23 10:18:08 -0700

    Use dj-static to serve up static files.

--- pontoon/wsgi.py
+++ pontoon/wsgi.py
@@ -8,6 +8,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontoon.settings')
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
