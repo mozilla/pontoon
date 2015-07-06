de7f9c2a254a12148ac992536b73ac26b3fe5394
Matjaž Horvat <matjaz.horvat@gmail.com>
2015-07-06 21:52:34 +0200

    This will work

--- pontoon/administration/management/commands/commit_projects.py
+++ pontoon/administration/management/commands/commit_projects.py
@@ -3,7 +3,7 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
-from pontoon.administration.files import dump_from_database, update_from_repository
+from pontoon.administration.files import dump_from_database, update_from_repository, extract_to_database
 from pontoon.administration.vcs import commit_to_vcs
 from pontoon.base.models import (
     Project,
