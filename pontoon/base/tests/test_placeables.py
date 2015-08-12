4d36216639a9662d6b73b8f07a2aba27c5636525
Matjaž Horvat <matjaz.horvat@gmail.com>
2015-08-12 20:46:24 +0200

    Add mark_placeables test

--- pontoon/base/tests/test_placeables.py
+++ pontoon/base/tests/test_placeables.py
@@ -1,7 +1,7 @@
 from django_nose.tools import assert_equal
 
 from pontoon.base.tests import TestCase
-from pontoon.base.utils import NewlineEscapePlaceable
+from pontoon.base.utils import NewlineEscapePlaceable, mark_placeables
 
 
 class PlaceablesTests(TestCase):
@@ -14,3 +14,26 @@ class PlaceablesTests(TestCase):
         assert_equal(placeable.parse(u'A\\nstring')[1], placeable([u'\\n']))
         assert_equal(placeable.parse(u'A string'), None)
         assert_equal(placeable.parse(u'A\nstring'), None)
+
+    def test_mark_newline_escape_placeables(self):
+        """Test detecting newline escape sequences"""
+        assert_equal(
+            mark_placeables(u'A string\\n'),
+            u'A string<mark class="placeable" title="Escaped newline">\\n</mark>'
+        )
+        assert_equal(
+            mark_placeables(u'\\nA string'),
+            u'<mark class="placeable" title="Escaped newline">\\n</mark>A string'
+        )
+        assert_equal(
+            mark_placeables(u'A\\nstring'),
+            u'A<mark class="placeable" title="Escaped newline">\\n</mark>string'
+        )
+        assert_equal(
+            mark_placeables(u'A string'),
+            u'A string'
+        )
+        assert_equal(
+            mark_placeables(u'A\nstring'),
+            u'A\nstring'
+        )
