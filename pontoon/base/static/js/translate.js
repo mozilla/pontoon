mozilla/pontoon

commit 197bc8c70a810d9391bc9aaec158170bc9ca433c

Enable searching by key

pontoon/base/static/js/translate.js
@@ -214,7 +214,7 @@ var Pontoon = (function (my) {
       }
       if (entity.key && entity.key != entity.original) {
         // Only show the actual key, the second part is the source string
-        self.appendMetaData('Context', entity.key.split('\x04')[0]);
+        self.appendMetaData('Context', entity.key);
       }
       if (entity.source) {
         if (typeof(entity.source) === 'object') {
@@ -408,9 +408,14 @@ var Pontoon = (function (my) {
             val = $('#search').val();
 
         ul
-          .find('.limited').show()
-            .find('.string-wrapper' + ':not(":containsi(\'' + val + '\')")')
-          .parent().hide();
+          .find('.limited').hide()
+            .find('.string-wrapper:containsi("' + val + '")')
+          .parents('.limited').show();
+
+        // Also search by key - separate selector for case-insensitivity
+        ul.find('.limited .source-string').filter(function() {
+          return this.dataset.key.toLowerCase().indexOf(val.toLowerCase()) > -1;
+        }).parents('.limited').show();
 
         if ($('.uneditables li:visible').length === 0) {
           $('#not-on-page').hide();
@@ -485,7 +490,7 @@ var Pontoon = (function (my) {
           (!this.body ? ' uneditable' : '') + '">' +
           '<span class="status fa"></span>' +
           '<p class="string-wrapper">' +
-            '<span class="source-string">' + this.marked + '</span>' +
+            '<span class="source-string" data-key="' + this.key + '">' + this.marked + '</span>' +
             '<span class="translation-string" dir="auto" lang="' + self.locale.code + '">' +
               self.doNotRender(this.translation[0].string || '') +
             '</span>' +

pontoon/base/models.py
@@ -750,7 +750,9 @@ class Entity(DirtyFieldsMixin, models.Model):
                 'marked': entity.marked,
                 'original_plural': entity.string_plural,
                 'marked_plural': entity.marked_plural,
-                'key': entity.key,
+                # Remove source string from the key and the delimiter
+                # as required by Translate Toolkit
+                'key': entity.key.split('\x04')[0],
                 'path': entity.resource.path,
                 'format': entity.resource.format,
                 'comment': entity.comment,

commit 4aeaba65b06ea0aeafb35a373eae75346f1cdec7

Fix bug 1217753: broken entity list
It was caused by:
https://github.com/mozilla/pontoon/commit/197bc8c70a810d9391bc9aaec158170bc9ca433c

Which implemented searching by string key by storing key in the data-key
attribute of each entity in the list. Since some keys also contain
strings and strings can contain HTML markup, we broke HTML of the
entity list.

This fix only puts keys that are not equal to source strings into the
attribute and also escapes keys.

pontoon/base/static/js/translate.js
@@ -490,7 +490,8 @@ var Pontoon = (function (my) {
           (!this.body ? ' uneditable' : '') + '">' +
           '<span class="status fa"></span>' +
           '<p class="string-wrapper">' +
-            '<span class="source-string" data-key="' + this.key + '">' + this.marked + '</span>' +
+            '<span class="source-string" data-key="' +
+            ((this.key !== this.original) ? self.doNotRender(this.key) : '') + '">' + this.marked + '</span>' +
             '<span class="translation-string" dir="auto" lang="' + self.locale.code + '">' +
               self.doNotRender(this.translation[0].string || '') +
             '</span>' +