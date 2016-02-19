80e8618b844852b6d2a90f665ea629e67801b6f9
Matjaž Horvat <matjaz.horvat@gmail.com>
2016-02-19 15:30:40 +0100

    Redirect sync/ to sync/log/

--- pontoon/sync/urls.py
+++ pontoon/sync/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from pontoon.sync import views
 
 
 urlpatterns = [
+    # Redirect until we use it for something more meaningful
+    url(r'^sync/$',
+        RedirectView.as_view(pattern_name='pontoon.sync.logs.list', permanent=True)),
+
     url(r'^sync/log/$', views.sync_log_list, name='pontoon.sync.logs.list'),
 
     url(r'^sync/log/(?P<sync_log_pk>\d+)/', views.sync_log_details,
