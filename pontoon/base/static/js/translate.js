815db8d9366c7391a0adf524f90a94062e1c8d81
Michal Vašíček <me@michalvasicek.eu>
2016-05-04 16:04:41 +0200

    Bug 1243118 - [translate] Highlight matches when searching entities

--- pontoon/base/static/js/translate.js
+++ pontoon/base/static/js/translate.js
@@ -54,7 +54,7 @@ var Pontoon = (function (my) {
         '" data-entry-pk="' + entity.pk + '">' +
         '<span class="status fa' + (self.user.isTranslator ? '' : ' unselectable') + '"></span>' +
         '<p class="string-wrapper">' +
-          '<span class="source-string">' + source_string + '</span>' +
+          '<span class="source-string">' + ((source_string.indexOf('<') == -1) ? source_string.replace(new RegExp("("+ Pontoon.getSearch().replace(/(\s+)/,"(?<!<[^>]*)$1(?<![^>]*<)") +")", "gi"), "<span class=\"searchHighlight\">$1</span>").replace(/(<span class="searchHighlight">[^<>]*)((<[^>]+>)+)([^<>]*<\/span>)/,"$1</span>$2<span class=\"searchHighlight\">$4") : source_string) + '</span>' +
           '<span class="translation-string" dir="auto" lang="' + self.locale.code + '">' +
             self.doNotRender(entity.translation[0].string || '') +
           '</span>' +
@@ -63,7 +63,6 @@ var Pontoon = (function (my) {
         '</li>', self.app.win);
       li[0].entity = entity;
       entity.ui = li; /* HTML Element representing string in the main UI */
-
       // Hover editable entities on the page
       if (entity.body) {
         li.hover(function () {
