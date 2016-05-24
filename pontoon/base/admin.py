4afb663e66ba421c29414483371b279fc143bc91
Matjaž Horvat <matjaz.horvat@gmail.com>
2016-05-24 23:36:31 +0200

    Bug 1203788: Use inlines in Project, User and Sync models (#404)
    Also use raw_id_fields and readonly_fields to make page loads faster.

--- pontoon/base/admin.py
+++ pontoon/base/admin.py
@@ -1,37 +1,75 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.models import User
+
 from pontoon.base import models
 
 
-class UserAdmin(admin.ModelAdmin):
+AGGREGATED_STATS_FIELDS = (
+    'total_strings',
+    'approved_strings',
+    'translated_strings',
+    'fuzzy_strings',
+)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = models.UserProfile
+    max_num = 1
+    can_delete = False
+    fields = ('quality_checks', 'force_suggestions',)
+    verbose_name_plural = 'Settings'
+
+
+class UserAdmin(AuthUserAdmin):
     search_fields = ['email', 'first_name']
     list_display = ('email', 'first_name', 'last_login', 'date_joined')
     list_filter = ('is_staff', 'is_superuser')
+    inlines = (UserProfileInline,)
 
 
 class LocaleAdmin(admin.ModelAdmin):
     search_fields = ['name', 'code']
     list_display = ('pk', 'name', 'code', 'nplurals', 'plural_rule', 'cldr_plurals')
+    readonly_fields = AGGREGATED_STATS_FIELDS + ('latest_translation',)
 
 
-class ProjectLocaleAdmin(admin.ModelAdmin):
-    search_fields = ['project__name', 'project__slug', 'locale__name', 'locale__code']
-    list_display = ('pk', 'project', 'locale')
+class ProjectLocaleInline(admin.TabularInline):
+    model = models.ProjectLocale
+    extra = 0
+    verbose_name_plural = 'Locales'
+    fields = ('locale',)
+
+
+class RepositoryInline(admin.TabularInline):
+    model = models.Repository
+    extra = 0
+    verbose_name_plural = 'Repositories'
+    fields = ('type', 'url', 'permalink_prefix', 'source_repo',)
+
+
+class SubpageInline(admin.TabularInline):
+    model = models.Subpage
+    extra = 0
+    verbose_name_plural = 'Subpages'
+    fields = ('project', 'name', 'url', 'resources',)
+    raw_id_fields = ('resources',)
 
 
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ['name', 'slug']
-    list_display = ('pk', 'name', 'slug', 'url', 'disabled', 'has_changed')
+    list_display = ('name', 'slug', 'pk', 'disabled', 'has_changed')
 
-
-class RepositoryAdmin(admin.ModelAdmin):
-    search_fields = ['url', 'permalink_prefix', 'project__name', 'project__slug']
-    list_display = ('pk', 'project', 'type', 'url', 'permalink_prefix')
-
-
-class SubpageAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'url', 'project__name', 'project__slug']
-    list_display = ('pk', 'project', 'name', 'url')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'info_brief',),
+        }),
+        ('WEBSITE', {
+            'fields': ('url', 'width', 'links'),
+        }),
+    )
+    readonly_fields = AGGREGATED_STATS_FIELDS + ('latest_translation', 'has_changed')
+    inlines = (ProjectLocaleInline, RepositoryInline, SubpageInline)
 
 
 class ResourceAdmin(admin.ModelAdmin):
@@ -42,40 +80,37 @@ class ResourceAdmin(admin.ModelAdmin):
 class TranslatedResourceAdmin(admin.ModelAdmin):
     search_fields = ['resource__path', 'locale__name', 'locale__code']
     list_display = ('pk', 'resource', 'locale')
+    readonly_fields = AGGREGATED_STATS_FIELDS + ('latest_translation',)
+    raw_id_fields = ('resource',)
+
+
+class EntityAdmin(admin.ModelAdmin):
+    raw_id_fields = ('resource',)
+
+
+class TranslationAdmin(admin.ModelAdmin):
+    raw_id_fields = ('entity',)
 
 
 class TranslationMemoryEntryAdmin(admin.ModelAdmin):
     search_fields = ['source', 'target', 'locale__name', 'locale__code']
     list_display = ('pk', 'source', 'target', 'locale')
+    raw_id_fields = ('entity', 'translation',)
 
 
 class ChangedEntityLocaleAdmin(admin.ModelAdmin):
     search_fields = ['entity__string', 'locale__name', 'locale__code']
     # Entity column should come last, for it can be looong
     list_display = ('pk', 'when', 'locale', 'entity')
+    raw_id_fields = ('entity',)
 
 
-class UserProfileAdmin(admin.ModelAdmin):
-    search_fields = ['user__email', 'user__first_name']
-    list_display = ('pk', 'user_email', 'quality_checks', 'force_suggestions')
-
-    def user_email(self, obj):
-        return obj.user.email
-    user_email.short_description = 'User'
-
-
-admin.site.register(Group)
 admin.site.register(User, UserAdmin)
-
 admin.site.register(models.Locale, LocaleAdmin)
-admin.site.register(models.ProjectLocale, ProjectLocaleAdmin)
 admin.site.register(models.Project, ProjectAdmin)
-admin.site.register(models.Repository, RepositoryAdmin)
-admin.site.register(models.Subpage, SubpageAdmin)
 admin.site.register(models.Resource, ResourceAdmin)
 admin.site.register(models.TranslatedResource, TranslatedResourceAdmin)
-admin.site.register(models.Entity)
-admin.site.register(models.Translation)
+admin.site.register(models.Entity, EntityAdmin)
+admin.site.register(models.Translation, TranslationAdmin)
 admin.site.register(models.TranslationMemoryEntry, TranslationMemoryEntryAdmin)
 admin.site.register(models.ChangedEntityLocale, ChangedEntityLocaleAdmin)
-admin.site.register(models.UserProfile, UserProfileAdmin)
