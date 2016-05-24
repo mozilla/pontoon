a6f93f7b487f66033b169c03fa876b0c704b325d
Extremely Bored Panda <jotes@users.noreply.github.com>
2016-05-24 12:26:06 +0200

    Add gzip compression to django views. (#402)

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
 BLOCKED_IPS = os.environ.get('BLOCKED_IPS', '').split(',')
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'sslify.middleware.SSLifyMiddleware',
     'pontoon.base.middleware.RaygunExceptionMiddleware',
     'pontoon.base.middleware.BlockedIpMiddleware',
