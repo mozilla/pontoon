mozilla/pontoon

commit 7f284b0d12a14c2eea93821b6cc4ec347b38a7d0

Bug 1329003: improve diff experience * Use diff-match-patch options for better readability * Hide diff by default and use toggle to turn it on and off

pontoon/base/static/css/style.css
@@ -1184,7 +1184,7 @@ body > .container {
   text-align: left;
 }
 
-#helpers > section ul li > p.translation-raw {
+#helpers > section ul li > p.translation {
   display: none;
 }
 
@@ -1195,12 +1195,12 @@ body > .container {
 
 #helpers > section ul li > p ins {
   background: #4B6259;
-  color: #7BC876;
+  color: #9CD699;
 }
 
 #helpers > section ul li > p del {
   background: #674B54;
-  color: #FE7777;
+  color: #FE8F8F;
 }
 
 #helpers > section ul li > p ins mark,

pontoon/base/static/js/main.js
@@ -105,8 +105,11 @@ var Pontoon = (function (my) {
      */
     diff: function (reference, string) {
       var self = this,
-          diff = diff_main(reference, string),
+          diff_obj = new diff_match_patch(),
+          diff = diff_obj.diff_main(reference, string),
           output = '';
+      diff_obj.diff_cleanupSemantic(diff);
+      diff_obj.diff_cleanupEfficiency(diff);
 
       $.each(diff, function() {
         var type = this[0],

pontoon/base/static/js/translate.js
@@ -265,6 +265,7 @@ var Pontoon = (function (my) {
                       '<time class="stress" datetime="' + this.date_iso + '">' + this.date + ' UTC</time>' +
                     '</div>' +
                     '<menu class="toolbar">' +
+                      ((i > 0) ? '<a href="#" class="toggle-diff" data-alternative-text="Hide diff" title="Show diff against the currently active translation">Show diff</a>' : '') +
                       '<button class="' + (this.approved ? 'unapprove' : 'approve') + ' fa" title="' +
                        (this.approved ? 'Unapprove' : 'Approve')  + '"></button>' +
                       ((self.user.id && (self.user.id === this.uid) || self.user.canTranslate()) ? '<button class="delete fa" title="Delete"></button>' : '') +
@@ -1911,6 +1912,16 @@ var Pontoon = (function (my) {
             self.endLoader('Oops, something went wrong.', 'error');
           }
         });
+      });
+
+      // Toggle suggestion diff
+      $('#helpers .history').on('click', '.toggle-diff', function (e) {
+        e.preventDefault();
+
+        var oldText = $(this).html();
+        $(this).html($(this).data('alternative-text'));
+        $(this).data('alternative-text', oldText);
+        $(this).parents('li').find('p').toggle();
       });
     },
 

pontoon/base/static/css/translate.css
@@ -1708,6 +1708,14 @@ p#sign-in-required > a#sidebar-signin {
   float: left;
 }
 
+#helpers > section ul li > header .toggle-diff {
+  margin-right: 6px;
+}
+
+#helpers > section ul li > header .toggle-diff.active {
+  color: #7BC876;
+}
+
 #helpers > section ul li > header .toolbar {
   float: right;
 }

commit c9380f023cadd3655dbb915bf8164841fbbca35a

Fix bug 1333659: Restore &nbsp; highlights
A regression from 7f284b0

pontoon/base/static/css/style.css
@@ -1184,7 +1184,8 @@ body > .container {
   text-align: left;
 }
 
-#helpers > section ul li > p.translation {
+#helpers > section ul li > p.translation-clipboard,
+#helpers > section ul li > p.translation-diff {
   display: none;
 }
 

pontoon/base/static/js/main.js
@@ -210,7 +210,7 @@ var Pontoon = (function (my) {
             '<p class="translation" dir="auto" lang="' + self.locale.code + '">' +
               self.markPlaceables(data.translation) +
             '</p>' +
-            '<p class="translation-raw">' +
+            '<p class="translation-clipboard">' +
               self.doNotRender(data.translation) +
             '</p>' +
           '</li>');

pontoon/base/static/js/translate.js
@@ -135,7 +135,7 @@ var Pontoon = (function (my) {
                 '<p class="translation" dir="auto" lang="' + this.locale__code + '">' +
                   self.markPlaceables(this.string) +
                 '</p>' +
-                '<p class="translation-raw">' +
+                '<p class="translation-clipboard">' +
                   self.doNotRender(this.string) +
                 '</p>' +
               '</li>');
@@ -272,9 +272,12 @@ var Pontoon = (function (my) {
                     '</menu>' +
                   '</header>' +
                   '<p class="translation" dir="auto" lang="' + self.locale.code + '">' +
+                    self.markPlaceables(this.translation) +
+                  '</p>' +
+                  '<p class="translation-diff" dir="auto" lang="' + self.locale.code + '">' +
                     ((i > 0) ? self.diff(data[0].translation, this.translation) : self.markPlaceables(this.translation)) +
                   '</p>' +
-                  '<p class="translation-raw">' +
+                  '<p class="translation-clipboard">' +
                     self.doNotRender(this.translation) +
                   '</p>' +
                 '</li>');
@@ -1756,7 +1759,7 @@ var Pontoon = (function (my) {
           return;
         }
 
-        var source = $(this).find('.translation-raw').text();
+        var source = $(this).find('.translation-clipboard').text();
 
         $('#translation').val(source).focus();
         self.moveCursorToBeginning();
@@ -1921,7 +1924,7 @@ var Pontoon = (function (my) {
         var oldText = $(this).html();
         $(this).html($(this).data('alternative-text'));
         $(this).data('alternative-text', oldText);
-        $(this).parents('li').find('p').toggle();
+        $(this).parents('li').find('.translation, .translation-diff').toggle();
       });
     },
 