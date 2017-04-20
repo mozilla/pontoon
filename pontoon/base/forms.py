f1cc2ce9b0d054b4abdfc2b12cd916d0379e9052
Matjaž Horvat <matjaz.horvat@gmail.com>
2017-04-20 21:09:26 +0200

    Mode related form code blocks together

--- pontoon/base/forms.py
+++ pontoon/base/forms.py
@@ -25,6 +25,40 @@ class DownloadFileForm(forms.Form):
     part = NoTabStopCharField()
 
 
+class UploadFileForm(DownloadFileForm):
+    uploadfile = NoTabStopFileField()
+
+    def clean(self):
+        cleaned_data = super(UploadFileForm, self).clean()
+        part = cleaned_data.get("part")
+        uploadfile = cleaned_data.get("uploadfile")
+
+        if uploadfile:
+            limit = 5000
+
+            # File size validation
+            if uploadfile.size > limit * 1000:
+                current = round(uploadfile.size/1000)
+                message = (
+                    'Upload failed. Keep filesize under {limit} kB. Your upload: {current} kB.'
+                    .format(limit=limit, current=current)
+                )
+                raise forms.ValidationError(message)
+
+            # File format validation
+            if part:
+                file_extension = os.path.splitext(uploadfile.name)[1].lower()
+                part_extension = os.path.splitext(part)[1].lower()
+
+                # For now, skip if uploading file while using subpages
+                if part_extension in SUPPORTED_FORMAT_PARSERS.keys() and part_extension != file_extension:
+                    message = (
+                        'Upload failed. File format not supported. Use {supported}.'
+                        .format(supported=part_extension)
+                    )
+                    raise forms.ValidationError(message)
+
+
 class UserPermissionGroupForm(object):
     def assign_users_to_groups(self, group_name, users):
         """
@@ -104,40 +138,6 @@ ProjectLocalePermsFormsSet = forms.modelformset_factory(
     ProjectLocalePermsForm,
     formset=ProjectLocaleFormSet,
 )
-
-
-class UploadFileForm(DownloadFileForm):
-    uploadfile = NoTabStopFileField()
-
-    def clean(self):
-        cleaned_data = super(UploadFileForm, self).clean()
-        part = cleaned_data.get("part")
-        uploadfile = cleaned_data.get("uploadfile")
-
-        if uploadfile:
-            limit = 5000
-
-            # File size validation
-            if uploadfile.size > limit * 1000:
-                current = round(uploadfile.size/1000)
-                message = (
-                    'Upload failed. Keep filesize under {limit} kB. Your upload: {current} kB.'
-                    .format(limit=limit, current=current)
-                )
-                raise forms.ValidationError(message)
-
-            # File format validation
-            if part:
-                file_extension = os.path.splitext(uploadfile.name)[1].lower()
-                part_extension = os.path.splitext(part)[1].lower()
-
-                # For now, skip if uploading file while using subpages
-                if part_extension in SUPPORTED_FORMAT_PARSERS.keys() and part_extension != file_extension:
-                    message = (
-                        'Upload failed. File format not supported. Use {supported}.'
-                        .format(supported=part_extension)
-                    )
-                    raise forms.ValidationError(message)
 
 
 class UserProfileForm(forms.ModelForm):
