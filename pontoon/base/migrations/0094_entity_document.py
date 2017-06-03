f962a9e09ee8b743d8cb2df5b8639763b7a03855
Matjaž Horvat <matjaz.horvat@gmail.com>
2017-06-03 18:26:09 +0200

    Update

--- pontoon/base/migrations/0094_entity_document.py
+++ pontoon/base/migrations/0094_entity_document.py
@@ -6,12 +6,36 @@ from django.db import migrations
 from django.db.models import TextField, Value
 from django.db.models.functions import Concat
 
+from pontoon.db.migrations import (
+    MultiFieldTRGMIndex,
+)
+
 
 def set_entity_document(apps, schema_editor):
     Entity = apps.get_model('base', 'Entity')
     Entity.objects.update(
         document=Concat('key', Value(' '), 'string', Value(' '), 'string_plural', Value(' '), 'comment')
     )
+
+entity_document_update_trigger_create_sql = """
+    CREATE FUNCTION base_translation_entity_document_update() RETURNS TRIGGER AS $$
+        BEGIN
+          NEW.entity_document = (
+            SELECT (e.key || ' ' || e.string || ' ' || e.string_plural || ' ' || e.comment) as document
+            FROM base_entity as e
+            WHERE id=NEW.entity_id
+          );
+        RETURN NEW;
+    END;
+    $$ LANGUAGE 'plpgsql';
+    CREATE TRIGGER base_translation_entity_document_update BEFORE INSERT OR UPDATE ON "base_translation"
+    FOR EACH ROW EXECUTE PROCEDURE base_translation_entity_document_update()
+"""
+
+entity_document_update_trigger_drop_sql = '''
+    DROP TRIGGER base_translation_entity_document_update ON "base_translation";
+    DROP FUNCTION base_translation_entity_document_update();
+'''
 
 
 class Migration(migrations.Migration):
@@ -27,5 +51,10 @@ class Migration(migrations.Migration):
             field=TextField(blank=True),
         ),
 
-        migrations.RunPython(set_entity_document, migrations.RunPython.noop)
+        migrations.RunPython(set_entity_document, migrations.RunPython.noop),
+
+        migrations.RunSQL(
+            entity_document_update_trigger_drop_sql,
+            entity_document_update_trigger_create_sql,
+        ),
     ]
