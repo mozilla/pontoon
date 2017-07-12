9947aad0d93adca4c5fcef890565871d202a76f9
Staś Małolepszy <stas@mozilla.com>
2017-07-12 14:27:34 +0200

    Paginate using Django Paginator
        {
          projects {
    	totalCount
    	items {
    	  name
    	  locales {
    	    items {
    	      locale {
    		name
    	      }
    	    }
    	  }
    	}
          }
        }

--- pontoon/graphql.py
+++ pontoon/graphql.py
@@ -1,7 +1,7 @@
 import graphene
-from graphene import relay
-from graphene_django import DjangoObjectType, DjangoConnectionField
+from graphene_django import DjangoObjectType
 from graphene_django.debug import DjangoDebug
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .base.models import (
     Project as ProjectModel,
@@ -10,44 +10,105 @@ from .base.models import (
 )
 
 
-class ProjectNode(DjangoObjectType):
-    class Meta:
-        model = ProjectModel
-        interfaces = (relay.Node, )
-        only_fields = (
-            'name', 'info', 'deadline', 'priority', 'contact',
-            'total_strings', 'approved_strings', 'translated_strings',
-            'fuzzy_strings', 'project_locale', 'resources')
+class PageInfo(graphene.ObjectType):
+    page_size = graphene.Int()
+    page_number = graphene.Int()
+    has_next = graphene.Boolean()
+    has_previous = graphene.Boolean()
 
 
-class LocaleNode(DjangoObjectType):
-    class Meta:
-        model = LocaleModel
-        interfaces = (relay.Node, )
-        only_fields = (
-            'name', 'code', 'direction', 'script', 'population',
-            'total_strings', 'approved_strings', 'translated_strings',
-            'fuzzy_strings', 'project_locale')
+class Page(graphene.AbstractType):
+    total_count = graphene.Int()
+    page_info = graphene.Field(PageInfo)
 
 
-class ProjectLocaleNode(DjangoObjectType):
+class ProjectLocale(DjangoObjectType):
     class Meta:
         model = ProjectLocaleModel
-        interfaces = (relay.Node, )
         only_fields = (
             'total_strings', 'approved_strings', 'translated_strings',
             'fuzzy_strings', 'project', 'locale')
 
 
+class ProjectLocalePage(graphene.ObjectType, Page):
+    items = graphene.List(ProjectLocale)
+
+
+class Project(DjangoObjectType):
+    class Meta:
+        model = ProjectModel
+        only_fields = (
+            'name', 'info', 'deadline', 'priority', 'contact',
+            'total_strings', 'approved_strings', 'translated_strings',
+            'fuzzy_strings')
+
+    locales = graphene.Field(ProjectLocalePage, page=graphene.Int())
+
+    @graphene.resolve_only_args
+    def resolve_locales(self, page=1):
+        return get_page(ProjectLocalePage, self.project_locale.all(), page)
+
+
+class ProjectPage(graphene.ObjectType, Page):
+    items = graphene.List(Project)
+
+
+class Locale(DjangoObjectType):
+    class Meta:
+        model = LocaleModel
+        only_fields = (
+            'name', 'code', 'direction', 'script', 'population',
+            'total_strings', 'approved_strings', 'translated_strings',
+            'fuzzy_strings')
+
+    projects = graphene.Field(ProjectLocalePage, page=graphene.Int())
+
+    @graphene.resolve_only_args
+    def resolve_projects(self, page=1):
+        return get_page(ProjectLocalePage, self.project_locale.all(), page)
+
+
+class LocalePage(graphene.ObjectType, Page):
+    items = graphene.List(Locale)
+
+
 class Query(graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name='__debug')
-    node = relay.Node.Field()
 
-    project = relay.Node.Field(ProjectNode)
-    projects = DjangoConnectionField(ProjectNode)
+    projects = graphene.Field(ProjectPage, page=graphene.Int(default_value=1))
+    locales = graphene.Field(LocalePage, page=graphene.Int(default_value=1))
 
-    locale = relay.Node.Field(LocaleNode)
-    locales = DjangoConnectionField(LocaleNode)
+    @graphene.resolve_only_args
+    def resolve_projects(self, page=1):
+        query = ProjectModel.objects.prefetch_related('project_locale__locale')
+        return get_page(ProjectPage, query, page)
+
+    @graphene.resolve_only_args
+    def resolve_locales(self, page=1):
+        query = LocaleModel.objects.prefetch_related('project_locale__project')
+        return get_page(LocalePage, query, page)
+
+
+def get_page(shape, queryset, page):
+    paginator = Paginator(queryset, 10)
+
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = []
+
+    page_info = PageInfo(
+        page_size=10,
+        page_number=page,
+        has_next=items.has_next(),
+        has_previous=items.has_previous())
+
+    return shape(
+        total_count=paginator.count,
+        items=items,
+        page_info=page_info)
 
 
 schema = graphene.Schema(query=Query)
