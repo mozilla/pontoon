3c58c01fd9312a1fc1de3599b0ad788c4addcc61
Emin Mastizada <emin@mastizada.com>
2017-08-01 18:55:09 +0300

    missing semicolon and container.find for all classes

--- pontoon/teams/static/js/info.js
+++ pontoon/teams/static/js/info.js
@@ -6,23 +6,23 @@ $(function() {
     e.preventDefault();
     var infoBox = container.find('.info');
     var content = infoBox.html();
-    var textArea = $('.read-write-info textarea').val($.trim(content));
-    $('.edit-info').hide();
-    $('.read-only-info').hide();
-    $('.read-write-info').toggleClass('hidden');
+    var textArea = container.find('.read-write-info textarea').val($.trim(content));
+    container.find('.edit-info').hide();
+    container.find('.read-only-info').hide();
+    container.find('.read-write-info').toggleClass('hidden');
     textArea.focus();
   });
 
   container.on('click', '.cancel', function(e) {
     e.preventDefault();
-    $('.edit-info').show();
-    $('.read-only-info').show();
-    $('.read-write-info').toggleClass('hidden');
+    container.find('.edit-info').show();
+    container.find('.read-only-info').show();
+    container.find('.read-write-info').toggleClass('hidden');
     return false;
   });
 
   container.on('click', '.save', function(e) {
-    var textArea = $('.read-write-info textarea');
+    var textArea = container.find('.read-write-info textarea');
     var content = textArea.val();
     $.ajax({
       url: textArea.parent().data('url'),
@@ -33,15 +33,15 @@ $(function() {
       },
       success: function(data) {
         container.find('.info').html(data);
-        $('.read-write-info').toggleClass('hidden');
-        $('.edit-info').show();
-        $('.read-only-info').show();
+        container.find('.read-write-info').toggleClass('hidden');
+        container.find('.edit-info').show();
+        container.find('.read-only-info').show();
         Pontoon.endLoader('Team info saved.');
       },
       error: function(request) {
         Pontoon.endLoader(request.responseText, 'error');
       }
-    })
+    });
     return false;
   });
 
