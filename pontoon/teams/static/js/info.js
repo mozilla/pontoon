d0b4a5bbf6dc6ec9eaee1dd63ecb6ed781b86d6b
Emin Mastizada <emin@mastizada.com>
2017-08-03 01:23:09 +0300

    js and no results improvements.

--- pontoon/teams/static/js/info.js
+++ pontoon/teams/static/js/info.js
@@ -2,47 +2,46 @@
 $(function() {
   var container = $('#main .container');
 
+  function toggleWidgets() {
+    container
+      .find('.edit-info').toggle().end()
+      .find('.read-only-info').toggle().end()
+      .find('.read-write-info').toggleClass('hidden');
+  }
+
   container.on('click', '.edit-info', function(e) {
     e.preventDefault();
-    var infoBox = container.find('.info');
-    var content = infoBox.html();
+    var content = container.find('.info').html();
     var textArea = container.find('.read-write-info textarea').val($.trim(content));
-    container.find('.edit-info').hide();
-    container.find('.read-only-info').hide();
-    container.find('.read-write-info').toggleClass('hidden');
+    toggleWidgets();
     textArea.focus();
   });
 
   container.on('click', '.cancel', function(e) {
     e.preventDefault();
-    container.find('.edit-info').show();
-    container.find('.read-only-info').show();
-    container.find('.read-write-info').toggleClass('hidden');
-    return false;
+    toggleWidgets();
   });
 
   container.on('click', '.save', function(e) {
+    e.preventDefault();
     var textArea = container.find('.read-write-info textarea');
-    var content = textArea.val();
     $.ajax({
       url: textArea.parent().data('url'),
       type: 'POST',
       data: {
         csrfmiddlewaretoken: textArea.parent().data('csrf'),
-        team_info: content
+        team_info: textArea.val()
       },
       success: function(data) {
-        container.find('.info').html(data);
-        container.find('.read-write-info').toggleClass('hidden');
-        container.find('.edit-info').show();
-        container.find('.read-only-info').show();
+        container.find('.info').html(data).toggle(data !== '');
+        container.find('.no-results').toggle(data === '');
+        toggleWidgets();
         Pontoon.endLoader('Team info saved.');
       },
       error: function(request) {
         Pontoon.endLoader(request.responseText, 'error');
       }
     });
-    return false;
   });
 
 });
