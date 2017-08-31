6aba0cbfdcea663409c3ee3dc34f6a5f33c7b426
Matjaž Horvat <matjaz.horvat@gmail.com>
2017-08-31 17:22:21 +0200

    add parent class to multiple locale selector to prevent event selector collision with other locale selectors

--- pontoon/base/static/js/multiple_locale_selector.js
+++ pontoon/base/static/js/multiple_locale_selector.js
@@ -4,7 +4,7 @@ $(function() {
    * Function keeps track of inputs that contain informations abouyt the order of selected locales.
    */
   function updateSelectedLocales() {
-    var $selectedList = $('.locale.selected'),
+    var $selectedList = $('.multiple-locale-selector .locale.selected'),
         $selectedLocalesField = $selectedList.find('input[type=hidden]'),
         selectedLocalesLength = $selectedList.find('li[data-id]').length,
         selectedLocales = $selectedList.find('li[data-id]').map(function(index) {
@@ -15,25 +15,27 @@ $(function() {
   }
 
   // Choose locales
-  $('body').on('click', '.locale.select li', function (e) {
+  $('body').on('click', '.multiple-locale-selector .locale.select li', function (e) {
     var target = $(this).parents('.locale.select').siblings('.locale.select').find('ul'),
         clone = $(this).remove();
+
     target.prepend(clone);
     updateSelectedLocales();
   });
 
   // Choose/remove all locales
-  $('body').on('click', '.choose-all, .remove-all', function (e) {
+  $('body').on('click', '.multiple-locale-selector .choose-all, .multiple-locale-selector .remove-all', function (e) {
     e.preventDefault();
     var ls = $(this).parents('.locale.select'),
         target = ls.siblings('.locale.select').find('ul'),
         items = ls.find('li:visible:not(".no-match")').remove();
+
     target.prepend(items);
     updateSelectedLocales();
   });
 
   if ($.ui && $.ui.sortable) {
-    $('.locale.select .sortable').sortable({
+    $('.multiple-locale-selector .locale.select .sortable').sortable({
       axis: 'y',
       containment: 'parent',
       update: updateSelectedLocales,
