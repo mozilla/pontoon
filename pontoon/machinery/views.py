bee81e93ea6f01e196f956ab1167c11ba0c5ef52
Hadi <h.alshakhori@gmail.com>
2017-10-09 16:30:36 -0500

    update the comment with the missing special character

--- pontoon/machinery/views.py
+++ pontoon/machinery/views.py
@@ -62,7 +62,7 @@ def translation_memory(request):
                 suggestions[entry['target']].update(entry)
             suggestions[entry['target']]['count'] += 1
     except DataError as e:
-        # Catches argument exceeds the maximum length of 255 bytes' Error
+        # Catches 'argument exceeds the maximum length of 255 bytes' Error
         return HttpResponse('Not Implemented: {error}'.format(error=e), status=501)
 
     return JsonResponse(
