aa53cd132774e48731f5bd9f791c50c827c63a89
Ryan Northey <ryan@synca.io>
2018-01-17 17:55:37 +0000

    Touch Bug 1243129: React utils and widgets #803
    - Add/update npm deps
      - add identity-obj-proxy to prevent jest failing on css loaders
    - Add tests for js utils
    - Add tests for react columns widget
    - Add pontoon/django ajax util
    - Add js string strip util
    - Add react columns widget

---
+++ pontoon/static/js/utils/strip.js
@@ -0,0 +1,29 @@
+
+export class Strip {
+
+    /**
+     * This provides a utility for stripping chars from beginning or end
+     * of strings.
+     *
+     * By default it will strip whitespace - spaces,
+     */
+
+    strip (str, char) {
+        return this.rstrip(this.lstrip(str, char), char)
+    }
+
+    lstrip (str, char) {
+        return str.replace(new RegExp('^' + (char || '\\s') + '+'), '');
+    }
+
+    rstrip (str, char) {
+        const match = new RegExp('^[^' + (char || '\\s') + ']')
+        for (var i = str.length - 1; i >= 0; i--) {
+            if (match.test(str.charAt(i))) {
+                return str.substring(0, i + 1);
+            }
+        }
+    }
+}
+
+export const strip = new Strip();
---
+++ tests/js/widgets.js
@@ -0,0 +1,106 @@
+
+import React from 'react';
+
+import {shallow} from 'enzyme';
+
+import {Columns, Container, Column} from 'widgets/columns';
+
+
+test('Columns render', () => {
+
+    class MockColumns extends Columns {
+
+        get columns () {
+            return [['a', 2], ['b', 3], ['c', 7]];
+        }
+    }
+
+    const columns = shallow(<MockColumns />);
+    const container = columns.find(Container);
+    expect(container.length).toBe(1);
+    expect(container.props().columns).toBe(3);
+    expect(columns.props().ratios).toEqual([2, 3, 7]);
+    expect(container.props().children.length).toBe(3);
+    expect(container.props().children[0].props.children).toBe('a');
+    expect(container.props().children[0].type).toBe(Column);
+    expect(container.props().children[1].props.children).toBe('b');
+    expect(container.props().children[1].type).toBe(Column);
+    expect(container.props().children[2].props.children).toBe('c');
+    expect(container.props().children[2].type).toBe(Column);
+    expect(columns.text()).toBe('<Container />');
+});
+
+
+test('Column render', () => {
+    let column = shallow(<Column />);
+    expect(column.text()).toBe('');
+    let div = column.find('div');
+    expect(div.length).toBe(1);
+    expect(div.props().className).toBe('column');
+    column = shallow(<Column>xyz</Column>);
+    expect(column.text()).toBe('xyz');
+    column = shallow(<Column className='special-column'></Column>);
+    div = column.find('div');
+    expect(div.length).toBe(1);
+    expect(div.props().className).toBe('column special-column');
+});
+
+
+test('Container render', () => {
+    let container = shallow(<Container />);
+    expect(container.text()).toBe('');
+    expect(container.instance().columnStyles).toEqual([]);
+    let div = container.find('div');
+    expect(div.props().className).toBe('container');
+    expect(div.props().style).toEqual(
+        {"content": "", "display": "table", "width": "100%"});
+    expect(div.props().children).toEqual([]);
+    container = shallow(<Container columns={3} />);
+    expect(container.text()).toBe('');
+    div = container.find('div');
+    expect(container.instance().columnStyles).toEqual(
+        [{"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "33.333333333333336%"},
+         {"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "33.333333333333336%"},
+         {"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "33.333333333333336%"}]);
+    expect(div.props().children).toEqual([]);
+    expect(div.props().style).toEqual(
+        {"content": "", "display": "table", "width": "100%"});
+    expect(div.props().children).toEqual([]);
+    container = shallow(<Container columns={3} ratios={[2, 3, 7]} />);
+    expect(container.text()).toBe('');
+    expect(container.instance().columnStyles).toEqual(
+        [{"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "16.666666666666664%"},
+         {"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "25%"},
+         {"boxSizing": "border-box", "float": "left", "overflow": "hidden", "width": "58.333333333333336%"}]);
+});
+
+
+test('Container render children', () => {
+    const children = [<div>FOO...</div>, <div>...BAR</div>]
+    const styles = jest.fn(() => {
+        return [
+            {background: 'black', color: 'red'},
+            {background: 'green', color: 'gold'}]})
+
+    class MockContainer extends Container {
+
+        get columnStyles () {
+            return styles();
+        }
+    }
+
+    const container = shallow(<MockContainer children={children} />);
+    expect(container.text()).toBe('FOO......BAR');
+    expect(styles.mock.calls).toEqual([[], [], [], []])
+    const div = container.find('div.container');
+    expect(div.length).toBe(1)
+    expect(div.props().children.length).toBe(2)
+    const child0 = div.props().children[0]
+    const child1 = div.props().children[1]
+    expect(child0.props.style).toEqual(
+        {background: 'black', color: 'red'})
+    expect(child0.props.children).toEqual(children[0])
+    expect(child1.props.style).toEqual(
+        {background: 'green', color: 'gold'})
+    expect(child1.props.children).toEqual(children[1])
+});
