6d8980944cc4778ccb70533eda954aeb838c09b0
Ryan Northey <ryan@synca.io>
2018-01-21 20:30:17 +0000

    Touch Bug #1416256: String tiers resources util #877
    - Add tag exceptions
    - Add tests for TagsResourcesTool
    - Add TagsResourcesTool

--- pontoon/tags/utils/__init__.py
+++ pontoon/tags/utils/__init__.py
@@ -1,8 +1,10 @@
 
+from .resources import TagsResourcesTool
 from .stats import TagsStatsTool
 from .translations import TagsLatestTranslationsTool
 
 
 __all__ = (
+    'TagsResourcesTool',
     'TagsStatsTool',
     'TagsLatestTranslationsTool')
