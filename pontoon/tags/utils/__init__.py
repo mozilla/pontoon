6b1e05fe0a3aedf700cf86e59e936bea6d1cc7b7
Ryan Northey <ryan@synca.io>
2018-02-10 09:02:26 +0000

    Add utils to tagged.utils __init__

--- pontoon/tags/utils/__init__.py
+++ pontoon/tags/utils/__init__.py
@@ -1,10 +1,18 @@
 
+from .activity import (
+    LatestActivity, LatestActivityUser)
 from .resources import TagsResourcesTool
 from .stats import TagsStatsTool
+from .tagged import TaggedLocale
 from .translations import TagsLatestTranslationsTool
 
 
 __all__ = (
+    'LatestActivity',
+    'LatestActivityTranslation',
+    'LatestActivityUser',
+    'TagChart',
+    'TaggedLocale',
     'TagsResourcesTool',
     'TagsStatsTool',
     'TagsLatestTranslationsTool')
