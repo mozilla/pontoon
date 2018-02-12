0052bfee3c8bd08444724bca3e502023e7a41db2
Ryan Northey <ryan@synca.io>
2018-02-12 20:48:25 +0000

    Add tests for js stats tables

--- pontoon/static/js/widgets/tables/header.js
+++ pontoon/static/js/widgets/tables/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default class TableHeader extends React.PureComponent {
 
     render () {
-        const {children, id, className} = this.props;
+        const {id, className} = this.props;
         return (
             <div id={id} className={className}>
               {this.props.children}
