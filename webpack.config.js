4afa89c3d837a811d71ed3ebcafab708ef38f2c8
Ryan Northey <ryan@synca.io>
2018-03-09 13:45:30 +0000

    Add webpack plugins
    - extract text plugin - allows CSS to be served separately
    - file loader - handle media files and resources

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var BundleTracker = require('webpack-bundle-tracker');
 
 
@@ -22,10 +23,16 @@ module.exports = {
       loaders: ['babel-loader'],
     },{
         test: /\.css$/,
-        loaders: [ 'style-loader', 'css-loader' ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
     },{
 	test: /\.json$/,
 	loader: 'json-loader',
+    },{
+        test: /\.(eot|svg|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader?outputPath=./files/'
     }],
   },
   resolve: {
@@ -41,6 +48,7 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new BundleTracker({filename: './webpack-stats.json'}),
+    new ExtractTextPlugin("[name].css"),
   ],
   // This will expose source map files so that errors will point to your
   // original source files instead of the transpiled files.
