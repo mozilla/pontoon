e8a2453c215a3353e3350a571e17202f970d0de8
Ryan Northey <ryan@synca.io>
2018-04-02 21:57:08 +0100

    Npm and webpack update #921
    - Update npm package versions
      this updates minor versions for various packages, and shifts
      webpack to 4+
    - Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleTracker = require('webpack-bundle-tracker');
 
 
 module.exports = {
@@ -11,17 +12,22 @@ module.exports = {
     // This copies each source entry into the extension dist folder named
     // after its entry config key.
       path: path.resolve(__dirname, 'assets/webpack_bundles/'),
-      filename: '[name].js'
+      filename: '[name].entry.chunk.js',
+      chunkFilename: '[name].[chunkhash].js',
+      publicPath: '/static/webpack_bundles/'
   },
   module: {
     // This transpiles all code (except for third party modules) using Babel.
-    loaders: [{
+    rules: [{
       exclude: /node_modules/,
       test: /\.js$/,
       loaders: ['babel-loader'],
     },{
         test: /\.css$/,
-        loaders: [ 'style-loader', 'css-loader' ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
     },{
 	test: /\.json$/,
 	loader: 'json-loader',
@@ -40,8 +46,14 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new BundleTracker({filename: './webpack-stats.json'}),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
   ],
   // This will expose source map files so that errors will point to your
   // original source files instead of the transpiled files.
-  devtool: 'sourcemap',
+  devtool: 'sourcemap'
 };
