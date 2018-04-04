mozilla/pontoon

commit 70b85239b4836f469b8f7548a2078b8960dab254

If no textarea has focus, select first

pontoon/base/static/js/translate.js
@@ -1744,7 +1744,7 @@ var Pontoon = (function (my) {
           return;
         }
 
-        var textarea = $('#editor textarea:visible:focus'),
+        var textarea = $('#editor textarea:visible:focus, #editor textarea:visible:first');
             selectionStart = textarea.prop('selectionStart'),
             selectionEnd = textarea.prop('selectionEnd'),
             placeable = $(this).text(),

commit 219dca7300b654f742645c3182cab06ef518312d

Fix textarea detection for placeable insertion
Regression from:
https://github.com/mozilla/pontoon/pull/840/commits/70b85239b4836f469b8f7548a2078b8960dab254

pontoon/base/static/js/translate.js
@@ -1764,13 +1764,17 @@ var Pontoon = (function (my) {
           return;
         }
 
-        var textarea = $('#editor textarea:visible:focus, #editor textarea:visible:first'),
-            selectionStart = textarea.prop('selectionStart'),
-            selectionEnd = textarea.prop('selectionEnd'),
-            placeable = $(this).text(),
-            cursorPos = selectionStart + placeable.length,
-            before = textarea.val(),
-            after = before.substring(0, selectionStart) + placeable + before.substring(selectionEnd);
+        var textarea = $('#editor textarea:visible:focus');
+        if (!textarea.length) {
+          textarea = $('#editor textarea:visible:first');
+        }
+
+        var selectionStart = textarea.prop('selectionStart');
+        var selectionEnd = textarea.prop('selectionEnd');
+        var placeable = $(this).text();
+        var cursorPos = selectionStart + placeable.length;
+        var before = textarea.val();
+        var after = before.substring(0, selectionStart) + placeable + before.substring(selectionEnd);
 
         textarea.val(after).focus();
         textarea[0].setSelectionRange(cursorPos, cursorPos);