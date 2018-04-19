8cb01c1ae6ee2e8f9dcb581b1a6550e93ddc3957
Adrian Gaudebert <adrian@gaudebert.fr>
2018-04-19 11:34:21 +0200

    Moved batch tests back to pontoon.

--- pontoon/batch/tests/test_views.py
+++ pontoon/batch/tests/test_views.py
@@ -1,6 +1,70 @@
 import pytest
 
+from django.contrib.auth import get_user_model
 from django.urls import reverse
+
+from pontoon.base.models import (
+    Entity,
+    Group,
+    Locale,
+    Project,
+    ProjectLocale,
+    Resource,
+)
+
+
+@pytest.fixture
+def fakeuser():
+    return get_user_model().objects.create(
+        username="fakeuser",
+        email="fakeuser@example.org"
+    )
+
+
+@pytest.fixture
+def member(client, fakeuser):
+    """Provides a `LoggedInMember` with the attributes `user` and `client`
+    the `client` is authenticated
+    """
+
+    class LoggedInMember(object):
+
+        def __init__(self, user, client):
+            client.force_login(user)
+            self.client = client
+            self.user = user
+
+    return LoggedInMember(fakeuser, client)
+
+
+@pytest.fixture
+def locale():
+    translators_group = Group.objects.create(
+        name='locale translators',
+    )
+    managers_group = Group.objects.create(
+        name='locale managers',
+    )
+    return Locale.objects.create(
+        code="kg",
+        name="Klingon",
+        translators_group=translators_group,
+        managers_group=managers_group,
+    )
+
+
+@pytest.fixture
+def entity(locale):
+    project = Project.objects.create(
+        slug="project", name="Project"
+    )
+    ProjectLocale.objects.create(project=project, locale=locale)
+    resource = Resource.objects.create(
+        project=project, path="resource.po", format="po"
+    )
+    return Entity.objects.create(
+        resource=resource, string="entity"
+    )
 
 
 def test_batch_edit_translations_no_user(client):
@@ -11,7 +75,7 @@ def test_batch_edit_translations_no_user(client):
 
 
 @pytest.mark.django_db
-def test_batch_edit_translations_bad_request(client, member0, locale0):
+def test_batch_edit_translations_bad_request(client, member, locale):
     # No `locale` parameter.
     response = client.post(
         reverse('pontoon.batch.edit.translations'),
@@ -24,7 +88,7 @@ def test_batch_edit_translations_bad_request(client, member0, locale0):
     # No `action` parameter.
     response = client.post(
         reverse('pontoon.batch.edit.translations'),
-        {'locale': locale0.code},
+        {'locale': locale.code},
         HTTP_X_REQUESTED_WITH='XMLHttpRequest',
     )
     assert response.status_code == 400
@@ -33,7 +97,7 @@ def test_batch_edit_translations_bad_request(client, member0, locale0):
     # Incorrect `action` parameter.
     response = client.post(
         reverse('pontoon.batch.edit.translations'),
-        {'action': 'unknown', 'locale': locale0.code},
+        {'action': 'unknown', 'locale': locale.code},
         HTTP_X_REQUESTED_WITH='XMLHttpRequest',
     )
     assert response.status_code == 400
@@ -51,14 +115,14 @@ def test_batch_edit_translations_bad_request(client, member0, locale0):
 
 @pytest.mark.django_db
 def test_batch_edit_translations_no_permissions(
-    client, member0, locale0, entity0
+    client, member, locale, entity
 ):
     response = client.post(
         reverse('pontoon.batch.edit.translations'),
         {
             'action': 'reject',
-            'locale': locale0.code,
-            'entities': entity0.id,
+            'locale': locale.code,
+            'entities': entity.id,
         },
         HTTP_X_REQUESTED_WITH='XMLHttpRequest',
     )
