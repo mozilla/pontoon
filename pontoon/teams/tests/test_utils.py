47511d1a620d419e27cef65a6cd0c5d8a7054189
Adrian Gaudebert <adrian@gaudebert.fr>
2018-04-19 11:45:33 +0200

    Fixed teams tests.

--- pontoon/teams/tests/test_utils.py
+++ pontoon/teams/tests/test_utils.py
@@ -1,7 +1,10 @@
 """
 Tests related to the utils provided in pontoon.teams.libraries
 """
+import factory
 import pytest
+
+from django.contrib.auth import get_user_model
 
 from pontoon.base.models import (
     PermissionChangelog,
@@ -10,183 +13,268 @@ from pontoon.teams.utils import (
     log_user_groups,
     log_group_members,
 )
+from pontoon.base.models import (
+    Group,
+)
+
+
+class GroupFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Group
+
+
+def group_factory(batch=1):
+    return [
+        GroupFactory(
+            name='group of users %s' % i,
+        )
+        for i in range(batch)
+    ]
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+
+def user_factory(batch=1):
+    return [
+        UserFactory(
+            username='fake user %s' % i,
+            email='user%s@example.org' % i,
+        )
+        for i in range(batch)
+    ]
+
+
+@pytest.fixture
+def fake_user():
+    return UserFactory(
+        username="fake_user",
+        email="fake_user@example.org"
+    )
+
+
+@pytest.fixture
+def other_user():
+    return UserFactory(
+        username="other_user",
+        email="other_user@example.org"
+    )
+
+
+@pytest.fixture
+def assert_permissionchangelog():
+    """
+    Shortcut assert function for freshly created permission changeset objects.
+    """
+    def test(changelog_item, action_type, performed_by, performed_on, group):
+        assert changelog_item.action_type == action_type
+        assert changelog_item.performed_by == performed_by
+        assert changelog_item.performed_on == performed_on
+        assert changelog_item.group == group
+
+    return test
+
+
+@pytest.fixture
+def translators_group():
+    return Group.objects.create(
+        name='some translators',
+    )
 
 
 @pytest.mark.django_db
-def test_log_group_members_empty(translators_group0, user0):
-    log_group_members(translators_group0, user0, ([], []))
+def test_log_group_members_empty(translators_group, fake_user):
+    log_group_members(translators_group, fake_user, ([], []))
 
     assert list(PermissionChangelog.objects.all()) == []
 
 
 @pytest.mark.django_db
 def test_log_group_members_added(
-        translators_group0,
-        user0,
-        user_factory,
-        assert_permissionchangelog):
+    translators_group,
+    fake_user,
+    assert_permissionchangelog
+):
     member0, member1, member2 = user_factory(batch=3)
     added_members = [member0, member2]
 
-    log_group_members(user0, translators_group0, (added_members, []))
+    log_group_members(fake_user, translators_group, (added_members, []))
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
 
     assert_permissionchangelog(
         changelog_entry0,
         'added',
-        user0,
+        fake_user,
         member0,
-        translators_group0
+        translators_group
     )
     assert_permissionchangelog(
         changelog_entry1,
         'added',
-        user0,
+        fake_user,
         member2,
-        translators_group0
+        translators_group
     )
 
 
 @pytest.mark.django_db
 def test_log_group_members_removed(
-        translators_group0,
-        user0,
-        user_factory,
-        assert_permissionchangelog):
+    translators_group,
+    fake_user,
+    assert_permissionchangelog
+):
     member0, member1, member2 = user_factory(batch=3)
     removed_members = [member0, member2]
 
-    log_group_members(user0, translators_group0, ([], removed_members))
+    log_group_members(fake_user, translators_group, ([], removed_members))
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
 
     assert_permissionchangelog(
         changelog_entry0,
         'removed',
-        user0,
+        fake_user,
         member0,
-        translators_group0
+        translators_group
     )
     assert_permissionchangelog(
         changelog_entry1,
         'removed',
-        user0,
+        fake_user,
         member2,
-        translators_group0
+        translators_group
     )
 
 
 @pytest.mark.django_db
 def test_log_group_members_mixed(
-        translators_group0,
-        user0,
-        user_factory,
-        assert_permissionchangelog):
+    translators_group,
+    fake_user,
+    assert_permissionchangelog
+):
     member0, member1, member2 = user_factory(batch=3)
     added_members = [member2]
     removed_members = [member0]
 
-    log_group_members(user0, translators_group0, (added_members, removed_members))
+    log_group_members(
+        fake_user,
+        translators_group,
+        (added_members, removed_members)
+    )
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
 
     assert_permissionchangelog(
         changelog_entry0,
         'added',
-        user0,
+        fake_user,
         member2,
-        translators_group0
+        translators_group
     )
 
     assert_permissionchangelog(
         changelog_entry1,
         'removed',
-        user0,
+        fake_user,
         member0,
-        translators_group0
+        translators_group
     )
 
 
 @pytest.mark.django_db
-def test_log_user_groups_empty(user0, user1):
-    log_user_groups(user0, user1, ([], []))
+def test_log_user_groups_empty(fake_user, other_user):
+    log_user_groups(fake_user, other_user, ([], []))
 
     assert list(PermissionChangelog.objects.all()) == []
 
 
 @pytest.mark.django_db
-def test_log_user_groups_added(user0, user1, group_factory, assert_permissionchangelog):
+def test_log_user_groups_added(
+    fake_user,
+    other_user,
+    assert_permissionchangelog,
+):
     group0, group1, group2 = group_factory(batch=3)
     added_groups = [group0, group2]
 
-    log_user_groups(user0, user1, (added_groups, []))
+    log_user_groups(fake_user, other_user, (added_groups, []))
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
 
     assert_permissionchangelog(
         changelog_entry0,
         'added',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group0
     )
     assert_permissionchangelog(
         changelog_entry1,
         'added',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group2
     )
 
 
 @pytest.mark.django_db
-def test_log_user_groups_removed(user0, user1, group_factory, assert_permissionchangelog):
+def test_log_user_groups_removed(
+    fake_user,
+    other_user,
+    assert_permissionchangelog,
+):
     group0, group1, group2 = group_factory(batch=3)
     removed_members = [group0, group2]
 
-    log_user_groups(user0, user1, ([], removed_members))
+    log_user_groups(fake_user, other_user, ([], removed_members))
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
 
     assert_permissionchangelog(
         changelog_entry0,
         'removed',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group0
     )
 
     assert_permissionchangelog(
         changelog_entry1,
         'removed',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group2
     )
 
 
 @pytest.mark.django_db
-def test_log_user_groups_mixed(user0, user1, group_factory, assert_permissionchangelog):
+def test_log_user_groups_mixed(
+    fake_user,
+    other_user,
+    assert_permissionchangelog,
+):
     group0, group1, group2 = group_factory(batch=3)
     added_groups = [group2]
     removed_groups = [group0]
 
-    log_user_groups(user0, user1, (added_groups, removed_groups))
+    log_user_groups(fake_user, other_user, (added_groups, removed_groups))
 
     changelog_entry0, changelog_entry1 = PermissionChangelog.objects.all()
     assert_permissionchangelog(
         changelog_entry0,
         'added',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group2
     )
 
     assert_permissionchangelog(
         changelog_entry1,
         'removed',
-        user0,
-        user1,
+        fake_user,
+        other_user,
         group0
     )
--- pontoon/teams/tests/test_views.py
+++ pontoon/teams/tests/test_views.py
@@ -1,68 +1,113 @@
+import factory
 import pytest
-
-from django.shortcuts import render
 from mock import patch
 
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
 
-@pytest.yield_fixture
-def sorted_locale0_translators(
-    locale0,
-    user_factory
-):
-    """
-    Assign (& yield) sorted translators to locale0
-    """
-    sorted_translators = sorted(
-        user_factory(batch=3),
-        key=lambda u: u.email
+from pontoon.base.models import (
+    Group,
+    Locale,
+    Project,
+    ProjectLocale,
+    Resource,
+)
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+
+@pytest.fixture
+def fakeuser():
+    return UserFactory(
+        username="fakeuser",
+        email="fakeuser@example.org"
     )
 
-    locale0.translators_group.user_set.add(*sorted_translators)
-    yield sorted_translators
+
+@pytest.fixture
+def member(client, fakeuser):
+    """Provides a `LoggedInMember` with the attributes `user` and `client`
+    the `client` is authenticated
+    """
+
+    class LoggedInMember(object):
+
+        def __init__(self, user, client):
+            client.force_login(user)
+            self.client = client
+            self.user = user
+
+    return LoggedInMember(fakeuser, client)
 
 
-@pytest.yield_fixture
-def sorted_locale0_managers(
-    locale0,
-    user_factory
-):
-    """
-    Assign (& yield) sorted managers to locale0
-    """
-    sorted_managers = sorted(
-        user_factory(batch=3),
-        key=lambda u: u.email
+@pytest.fixture
+def locale():
+    translators_group = Group.objects.create(
+        name='Klingon translators',
     )
-    locale0.managers_group.user_set.add(*sorted_managers)
+    managers_group = Group.objects.create(
+        name='Klingon managers',
+    )
+    return Locale.objects.create(
+        code="kg",
+        name="Klingon",
+        translators_group=translators_group,
+        managers_group=managers_group,
+    )
 
-    yield sorted_managers
+
+@pytest.fixture
+def project():
+    return Project.objects.create(
+        slug="project", name="Project"
+    )
 
 
-@pytest.yield_fixture
-def sorted_project_locale0_translators(
-        project_locale0,
-        user_factory
-):
-    """
-    Assign (& yield) sorted managers to project_locale0.
-    Users are passed as a list of dictionaries to permissions template.
-    """
+@pytest.fixture
+def project_locale(project, locale):
+    pl = ProjectLocale.objects.create(project=project, locale=locale)
+    # ProjectLocale doesn't work without at least one resource.
+    Resource.objects.create(
+        project=project,
+        path='foo.lang',
+        total_strings=1
+    )
+    return pl
+
+
+@pytest.fixture
+def translators():
     translators = [
-        {
-            'id': u.pk,
-            'first_name': u.first_name,
-            'email': u.email
-        }
-        for u in user_factory(batch=3)
+        UserFactory(
+            username='Translator %s' % x,
+            email='translator%s@example.org' % x,
+        )
+        for x in range(3)
     ]
-
     sorted_translators = sorted(
         translators,
-        key=lambda u: u['email']
+        key=lambda u: u.email
     )
-    project_locale0.translators_group.user_set.add(*[u['id'] for u in translators])
+    return sorted_translators
 
-    yield sorted_translators
+
+@pytest.fixture
+def managers():
+    managers = [
+        UserFactory(
+            username='Manager %s' % x,
+            email='manager%s@example.org' % x,
+        )
+        for x in range(3)
+    ]
+    sorted_managers = sorted(
+        managers,
+        key=lambda u: u.email
+    )
+    return sorted_managers
 
 
 @pytest.mark.django_db
@@ -70,92 +115,104 @@ def sorted_project_locale0_translators(
 def test_ajax_permissions_locale_translators_managers_order(
         render_mock,
         admin_client,
-        locale0,
-        sorted_locale0_translators,
-        sorted_locale0_managers,
+        locale,
+        translators,
+        managers,
 ):
     """
-    Translators and managers of a locale should be sorted by email in "Permissions" tab.
+    Translators and managers of a locale should be sorted by email in
+    "Permissions" tab.
     """
-    admin_client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
-    ))
+    locale.translators_group.user_set.add(*translators)
+    locale.managers_group.user_set.add(*managers)
+
+    admin_client.get('/%s/ajax/permissions/' % locale.code)
     response_context = render_mock.call_args[0][2]
 
-    assert list(response_context['translators']) == sorted_locale0_translators
-    assert list(response_context['managers']) == sorted_locale0_managers
+    assert list(response_context['translators']) == translators
+    assert list(response_context['managers']) == managers
 
 
 @pytest.mark.django_db
 @patch('pontoon.teams.views.render', wraps=render)
 def test_ajax_permissions_project_locale_translators_order(
-        render_mock,
-        admin_client,
-        locale0,
-        project_locale0,
-        sorted_project_locale0_translators,
+    render_mock,
+    admin_client,
+    locale,
+    project_locale,
+    translators,
 ):
     """
-    Translators and managers of a locale should be sorted by email in "Permissions" tab.
+    Translators and managers of a locale should be sorted by email in
+    "Permissions" tab.
     """
-    admin_client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
-    ))
+    project_locale.translators_group.user_set.add(*translators)
+
+    admin_client.get('/%s/ajax/permissions/' % locale.code)
     response_context = render_mock.call_args[0][2]
     locale_projects = response_context['locale_projects']
 
-    # Check project_locale0 id in the permissions form
-    assert locale_projects[0][0] == project_locale0.pk
+    # Check project_locale id in the permissions form
+    assert locale_projects[0][0] == project_locale.pk
 
-    # Check project_locale0 translators
-    assert locale_projects[0][4] == sorted_project_locale0_translators
+    # Check project_locale translators
+    translators_list = [
+        {
+            'id': u.id,
+            'email': u.email,
+            'first_name': u.first_name,
+        }
+        for u in translators
+    ]
+    assert locale_projects[0][4] == translators_list
 
 
 @pytest.mark.django_db
 def test_users_permissions_for_ajax_permissions_view(
     client,
-    locale0,
-    member0
+    locale,
+    member,
 ):
     """
-    Check if anonymous users and users without permissions can't access Permissions Tab.
+    Check if anonymous users and users without permissions can't access
+    Permissions Tab.
     """
 
     response = client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
+        locale=locale.code
     ))
     assert response.status_code == 403
     assert '<title>Forbidden page</title>' in response.content
 
     # Check if users without permissions for the locale can get this tab.
-    response = member0.client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
+    response = member.client.get('/{locale}/ajax/permissions/'.format(
+        locale=locale.code
     ))
     assert response.status_code == 403
     assert '<title>Forbidden page</title>' in response.content
 
-    locale0.managers_group.user_set.add(member0.user)
+    locale.managers_group.user_set.add(member.user)
 
     # Bump up permissions for user0 and check if the view is accessible.
-    response = member0.client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
+    response = member.client.get('/{locale}/ajax/permissions/'.format(
+        locale=locale.code
     ))
     assert response.status_code == 200
     assert '<title>Forbidden page</title>' not in response.content
 
     # Remove permissions for user0 and check if the view is not accessible.
-    locale0.managers_group.user_set.clear()
+    locale.managers_group.user_set.clear()
 
-    response = member0.client.get('/{locale}/ajax/permissions/'.format(
-        locale=locale0.code
+    response = member.client.get('/{locale}/ajax/permissions/'.format(
+        locale=locale.code
     ))
     assert response.status_code == 403
     assert '<title>Forbidden page</title>' in response.content
 
     # All unauthorized attempts to POST data should be blocked
-    response = member0.client.post(
+    response = member.client.post(
         '/{locale}/ajax/permissions/'.format(
-            locale=locale0.code
+            locale=locale.code
         ),
         data={'smth': 'smth'}
     )
@@ -164,7 +221,7 @@ def test_users_permissions_for_ajax_permissions_view(
 
     response = client.post(
         '/{locale}/ajax/permissions/'.format(
-            locale=locale0.code
+            locale=locale.code
         ),
         data={'smth': 'smth'}
     )
