a5427c2f6934db4c895bd55ce62c20b53d88cc11
Jarek <poke@jotes.work>
2018-05-01 22:26:56 +0200

    Fix bug 1458343 - Run compare-locales checks in Translate view (#856)
    In addition to Translate Toolkit checks, we now also run compare-locales
    checks when submitting a translation and also some pontoon-specific
    checks, which were previously spread around the codebase. As of now, all
    the checks code is located in the newly created pontoon.checks module.
    
    Failed checks are either errors (prevent translations from being
    submitted) or warnings (can be bypassed). Errors are detected using
    compare-locales library and Pontoon’s internal checks, while warnings
    are a product of compare-locales and Translate Toolkit libraries.
    
    Since Translate Toolkit checks may result in many false positives in some
    scenarios, they can be turned off using the Translate Toolkit Checks
    toggle (previously called Quality Checks).
    
    Other notable changes:
    * Added tests for integration with compare-locales and Translate Toolkit.
    * Added ability to disable specific Translate Toolkit checks (e.g. if
      the same check is already handled by compare-locales).
    * Slight UI Refresh: Prevent long check messages from being hidden behind
      the button and visually separate errors and warnings.
    * Move checks execution on the top of the /update/ view to reduce
      complexity and simplify testing.
    * Update to Fluent 0.7.0.

---
+++ tests/checks/translate_toolkit.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+import pytest
+from mock import MagicMock
+
+from pontoon.checks.libraries.translate_toolkit import run_checks
+
+
+@pytest.yield_fixture()
+def mock_locale():
+    """Small mock of Locale object to make faster unit-tests."""
+    mock = MagicMock()
+    mock.code = 'en-US'
+    yield mock
+
+
+def test_tt_invalid_translation(mock_locale):
+    """
+    Check if translate toolkit returns errors if chek
+    """
+    assert run_checks(
+        'Original string',
+        'Translation \q',
+        mock_locale,
+    ) == {
+        'ttWarnings': ['Escapes']
+    }
+
+
+def test_tt_disabled_checks(mock_locale):
+    """
+    Disabled checks should be respected by the run_checks.
+    """
+    assert run_checks(
+        'Original string',
+        'Translation \q',
+        mock_locale,
+        disabled_checks={'escapes'}
+    ) == {}
+
+
+def test_tt_correct_translation(mock_locale):
+    """
+    Quality check should return empty dictionary if everything is okay (no warnings).
+    """
+    assert run_checks(
+        'Original string',
+        'Translation string',
+        mock_locale
+    ) == {}
