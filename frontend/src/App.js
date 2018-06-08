77a3f7e454fa7ddd73f9b538f9942351404ee14f
Adrian Gaudebert <adrian@gaudebert.fr>
2018-06-08 19:38:10 +0400

    Add React frontend for Translate app. This has been set up using create-react-app.

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Well that works, heh!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
