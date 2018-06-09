088ee2d1740f6eec8c2be6594668c5ea5f969274
Matjaž Horvat <matjaz.horvat@gmail.com>
2018-06-09 09:41:03 +0200

    Bug 1465824: Make sure log messages are encoded as unicode

--- pontoon/sync/vcs/models.py
+++ pontoon/sync/vcs/models.py
@@ -347,7 +347,7 @@ class VCSProject(object):
             try:
                 resources[path] = VCSResource(self, path, locales=locales)
             except ParseError as err:
-                log.error('Skipping resource {path} due to ParseError: {err}'.format(
+                log.error(u'Skipping resource {path} due to ParseError: {err}'.format(
                     path=path, err=err
                 ))
 
@@ -478,7 +478,7 @@ class VCSResource(object):
 
             # File doesn't exist or is invalid: log it and move on
             except (IOError, ParseError) as err:
-                log.error('Skipping resource {path} due to {type}: {err}'.format(
+                log.error(u'Skipping resource {path} due to {type}: {err}'.format(
                     path=path,
                     type=type(err).__name__,
                     err=err
