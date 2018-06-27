0423fae69d60e0deafd1c62180baaf508344bfd9
Matjaž Horvat <matjaz.horvat@gmail.com>
2018-06-27 15:32:44 +0200

    Fix bug 1470337: De-optimize calculate_stats (#986)
    To prevent IntegrityError. And add a note to the help text.

--- pontoon/base/management/commands/calculate_stats.py
+++ pontoon/base/management/commands/calculate_stats.py
@@ -1,14 +1,10 @@
 import logging
 
-from bulk_update.helper import bulk_update
-
 from django.core.management.base import BaseCommand
 from django.db.models import Count
 
 from pontoon.base.models import (
-    Locale,
     Project,
-    ProjectLocale,
     TranslatedResource,
 )
 
@@ -17,8 +13,22 @@ log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = ('Re-calculate statistics for all translated resources and '
-            'corresponding objects.')
+    help = (
+        """
+        Re-calculate statistics for all translated resources and corresponding
+        objects.
+
+        Note: while unlikely, it's possible that running this command may
+        result in IntegrityErrors. That happens if at the same time when
+        calculate_stats() is being executed for a TranslatedResource instance,
+        translation is added, accepted or rejected for that very instance.
+
+        To be completely sure errors don't occur, command needs to run in a
+        maintenance mode.
+
+        See bug 1470337 for more details.
+        """
+    )
 
     def handle(self, *args, **options):
         # Start with enabled projects in ascending order of resource count
@@ -43,22 +53,6 @@ class Command(BaseCommand):
             )
 
             for translated_resource in translated_resources:
-                translated_resource.calculate_stats(save=False)
-
-            bulk_update(translated_resources, update_fields=[
-                'total_strings',
-                'approved_strings',
-                'fuzzy_strings',
-                'unreviewed_strings',
-            ])
-
-            project.aggregate_stats()
-
-            project_locales = ProjectLocale.objects.filter(project=project)
-            for project_locale in project_locales:
-                project_locale.aggregate_stats()
-
-        for locale in Locale.objects.all():
-            locale.aggregate_stats()
+                translated_resource.calculate_stats()
 
         log.info('Calculating stats complete for all projects.')
