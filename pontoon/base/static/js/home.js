c5637f6090b62c0d318fba303a3cf30459e372f1
Pramit Singhi <singhi.pramit@gmail.com>
2018-08-01 15:51:28 +0530

    Fixed scroll ease option and header color on scrolling

--- pontoon/base/static/js/home.js
+++ pontoon/base/static/js/home.js
@@ -5,29 +5,35 @@ $(function() {
 		section:".snaps",
     scrollbars:false,
     interstitialSection: "",
-    scrollSpeed: 2800,
+    easing: "easeInOutSine",
+    scrollSpeed: 975,
     before: function(i,snaps) {
       var ref = snaps[i].attr("data-section-name");
 
       $(".pagination.active").removeClass("active");
 
       $(".pagination[href=#" + ref + "]").addClass("active");
-      $('.home-header').css('background-color','#272A2F');
-      if (ref === "section-1")
-        $('.home-header').css({'background-color':'transparent','transition-duration':'0.3s'})
-      else
-        $('.home-header').css('background-color','#272A2F')
+
+      $('.home-header').css('background-color','#272A2F')
     },
     after: function(i,snaps) {
       var ref = snaps[i].attr("data-section-name");
 
-      if (ref === "section-6" || ref === "section-1")
+      if (ref === "section-6")
+        $('.home-header').css({'background-color':'transparent','transition-duration':'0.3s'})
+      else if (ref === "section-1")
         $('.home-header').css({'background-color':'transparent','transition-duration':'0.3s'})
       else
         $('.home-header').css('background-color','#272A2F')
     },
     updateHash: false,
 	});
+
+  $(window).on('load', function() {
+    setTimeout(function() {
+      $(window).scrollTop(0);
+    });
+  });
 
   $(".pagination").on("click",function() {
     $.scrollify.move($(this).attr("href"));
@@ -37,3 +43,11 @@ $(function() {
     $.scrollify.move("#section-2");
   });
 });
+
+$.easing['easeInOutCubic'] = function (x, t, b, c, d) {
+  return (t/=d/2) < 1 ? c/2*t*t*t + b : c/2*((t-=2)*t*t + 2) + b;
+};
+
+$.easing['easeInOutSine'] = function (x, t, b, c, d) {
+  return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+};
