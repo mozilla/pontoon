e268f829abc5673a9e2209b16da1c4eb0dc684cc
Adrian Gaudebert <adrian@gaudebert.fr>
2018-08-06 14:40:29 +0200

    Bug 1417530 - Move administration tests to pytest.

--- pontoon/tags/tests/conftest.py
+++ pontoon/tags/tests/conftest.py
@@ -1,13 +1,1 @@
-import pytest
-
-from pontoon.test.factories import UserFactory
 from .fixtures import *  # noqa
-
-
-@pytest.fixture
-def admin():
-    """Admin - a superuser"""
-    return UserFactory.create(
-        username="admin",
-        is_superuser=True,
-    )
