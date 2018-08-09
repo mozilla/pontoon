da31f881e50d1230b6dee12b4d42b1f7b0b87c4c
Vishal Sharma <vishaliitr7@gmail.com>
2018-08-10 02:02:00 +0530

    Fix this.project not defined in Pontoon.getEditorEntity() (#1055)

--- pontoon/base/static/js/tour.js
+++ pontoon/base/static/js/tour.js
@@ -1,4 +1,8 @@
 $(function () {
+  // Run the tour only on project with slug 'demo'
+  if (Pontoon.state.project !== "demo") {
+    return;
+  }
 
   var isEntityClicked = false;
   var isSubmitClicked = false;
@@ -310,8 +314,8 @@ $(function () {
     ]
   });
 
-  // Run the tour only on project with slug 'demo' and if not completed by user
-  if (Pontoon.state.project === "demo" && tourStatus !== -1) {
+  // Run the tour only if not completed by user
+  if (tourStatus !== -1) {
     Sideshow.start({ listAll: true });
 
     // If a user closes the tour at step 3 or step 5
