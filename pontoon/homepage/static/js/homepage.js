10dcbd05014caeb6971fac130aaf74d6fe0885b9
Matjaž Horvat <matjaz.horvat@gmail.com>
2018-08-13 22:00:19 +0200

    Fix tests

--- pontoon/homepage/static/js/homepage.js
+++ pontoon/homepage/static/js/homepage.js
@@ -1,22 +1,24 @@
-/*
- * fullPage plugin by Alvaro Trigo
- * URL: https://github.com/alvarotrigo/fullPage.js/
- */
-new fullpage('#main', {
-  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-  navigation: true,
-  navigationPosition: 'left',
-  onLeave: function(origin, destination, direction) {
-    $('body > header').css('background-color', '#272A2F');
-  },
-  afterLoad: function(origin, destination, direction) {
-    color = '#272A2F';
-    if (destination.item.id === 'section-1' || destination.item.id === 'section-6') {
-      color = 'transparent';
-    }
-    $('body > header').css('background-color', color);
-  },
-});
+(function() {
+  /*
+  * fullPage plugin by Alvaro Trigo
+  * URL: https://github.com/alvarotrigo/fullPage.js/
+  */
+  new fullpage('#main', {
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    navigation: true,
+    navigationPosition: 'left',
+    onLeave: function() {
+      $('body > header').css('background-color', '#272A2F');
+    },
+    afterLoad: function(origin, destination) {
+      var color = '#272A2F';
+      if (destination.item.id === 'section-1' || destination.item.id === 'section-6') {
+        color = 'transparent';
+      }
+      $('body > header').css('background-color', color);
+    },
+  });
+})();
 
 $(function() {
   // Scroll from Section 1 to Section 2
