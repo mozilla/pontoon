b84bd1f8779b87e78eeff0d5178e7fcd7da52c8a
missuniverse <31463320+missuniverse@users.noreply.github.com>
2018-11-17 23:56:49 -0500

    Update HTTP to HTTPS
    Update HTTP to HTTPS

--- pontoon/base/management/commands/updatefxaprovider.py
+++ pontoon/base/management/commands/updatefxaprovider.py
@@ -1,8 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# This file has been copied from http://github.com/mozilla/testpilot
+# This file has been copied from https://github.com/mozilla/testpilot
 # and has been modified in order to use Fxa provider from the allauth package.
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
