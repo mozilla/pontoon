4c7eb0507812678acf5c681c6605c2fc84c6ba49
missuniverse <31463320+missuniverse@users.noreply.github.com>
2018-11-18 00:00:56 -0500

    Update HTTP to HTTPS
    Update HTTP to HTTPS

--- pontoon/base/migrations/0068_auto_20161102_2302.py
+++ pontoon/base/migrations/0068_auto_20161102_2302.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='locale',
             name='cldr_plurals',
-            field=models.CommaSeparatedIntegerField(blank=True, help_text=b'\n        A comma separated list of <a href="http://www.unicode.org/cldr/charts/dev/supplemental/language_plural_rules.html">CLDR plural rules</a>,\n        where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.\n        E.g. 1,5\n        ', max_length=11, validators=[pontoon.base.models.validate_cldr], verbose_name=b'CLDR Plurals'),
+            field=models.CommaSeparatedIntegerField(blank=True, help_text=b'\n        A comma separated list of <a href="https://www.unicode.org/cldr/charts/dev/supplemental/language_plural_rules.html">CLDR plural rules</a>,\n        where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.\n        E.g. 1,5\n        ', max_length=11, validators=[pontoon.base.models.validate_cldr], verbose_name=b'CLDR Plurals'),
         ),
         migrations.AlterField(
             model_name='locale',
