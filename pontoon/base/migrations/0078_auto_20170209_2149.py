1c2ed351831d9d0f270e42d5c156192de9ac74f4
missuniverse <31463320+missuniverse@users.noreply.github.com>
2018-11-18 00:01:18 -0500

    Update HTTP to HTTPS
    Update HTTP to HTTPS

--- pontoon/base/migrations/0078_auto_20170209_2149.py
+++ pontoon/base/migrations/0078_auto_20170209_2149.py
@@ -18,6 +18,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='locale',
             name='cldr_plurals',
-            field=models.CharField(blank=True, help_text=b'\n        A comma separated list of <a href="http://www.unicode.org/cldr/charts/dev/supplemental/language_plural_rules.html">CLDR plural rules</a>,\n        where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.\n        E.g. 1,5\n        ', max_length=11, validators=[django.core.validators.RegexValidator(re.compile('^\\d+(?:\\,\\d+)*\\Z'), code='invalid', message='Enter only digits separated by commas.'), pontoon.base.models.validate_cldr], verbose_name=b'CLDR Plurals'),
+            field=models.CharField(blank=True, help_text=b'\n        A comma separated list of <a href="https://www.unicode.org/cldr/charts/dev/supplemental/language_plural_rules.html">CLDR plural rules</a>,\n        where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.\n        E.g. 1,5\n        ', max_length=11, validators=[django.core.validators.RegexValidator(re.compile('^\\d+(?:\\,\\d+)*\\Z'), code='invalid', message='Enter only digits separated by commas.'), pontoon.base.models.validate_cldr], verbose_name=b'CLDR Plurals'),
         ),
     ]
