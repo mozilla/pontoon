18965acfbb9267d984e5b30fa3dd9a69bc4ab528
missuniverse <31463320+missuniverse@users.noreply.github.com>
2018-11-17 23:51:33 -0500

    Update HTTP to HTTPS
    Update HTTP to HTTPS

--- pontoon/base/static/js/lib/jquery.timeago.js
+++ pontoon/base/static/js/lib/jquery.timeago.js
@@ -6,10 +6,10 @@
  * @version 1.4.1
  * @requires jQuery v1.2.3+
  * @author Ryan McGeary
- * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ * @license MIT License - https://www.opensource.org/licenses/mit-license.php
  *
  * For usage and examples, visit:
- * http://timeago.yarp.com/
+ * https://timeago.yarp.com/
  *
  * Copyright (c) 2008-2013, Ryan McGeary (ryan -[at]- mcgeary [*dot*] org)
  */
