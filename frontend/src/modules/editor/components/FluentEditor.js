a5fad403f31c49e659be00d95c58ff7df9911140
Staś Małolepszy <stas@mozilla.com>
2019-01-14 18:27:39 +0100

    Use fluent-syntax to annotate the translation source

--- frontend/src/modules/editor/components/FluentEditor.js
+++ frontend/src/modules/editor/components/FluentEditor.js
@@ -2,11 +2,34 @@
 
 import * as React from 'react';
 import AceEditor from 'react-ace';
+import { FluentParser, lineOffset, columnOffset }
+    from 'fluent-syntax';
 
 import './editor-mode-fluent';
 import './editor-theme-fluent';
 
 import type { EditorProps } from './GenericEditor';
+
+const fluent_parser = new FluentParser();
+
+function annotate(source) {
+    let resource = fluent_parser.parse(source);
+    let junks = resource.body.filter(entry => entry.type === 'Junk');
+    let annotations = [];
+
+    for (let junk of junks) {
+        for (let annot of junk.annotations) {
+            annotations.push({
+                type: 'error',
+                text: `${annot.code}: ${annot.message}`,
+                row: lineOffset(source, annot.span.start),
+                column: columnOffset(source, annot.span.start),
+            });
+        }
+    }
+
+    return annotations;
+}
 
 
 /*
@@ -40,6 +63,7 @@ export default class FluentEditor extends React.Component<EditorProps> {
             wrapEnabled={ true }
             setOptions={ options }
             value={ this.props.translation }
+            annotations={ annotate(this.props.translation) }
             onChange={ this.props.updateTranslation }
         />;
     }
