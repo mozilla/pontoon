07ca2a05d3bd428cf261399af83d34680162b1fa
Adrian Gaudebert <adrian@gaudebert.fr>
2019-01-31 11:03:16 +0100

    Meeh

--- frontend/src/modules/machinery/index.js
+++ frontend/src/modules/machinery/index.js
@@ -5,8 +5,6 @@ export { default as reducer } from './reducer';
 
 export { default as Machinery } from './components/Machinery';
 
-// export type { HistoryState } from './reducer';
-
 // Name of this module.
 // Used as the key to store this module's reducer.
 export const NAME: string = 'machinery';
