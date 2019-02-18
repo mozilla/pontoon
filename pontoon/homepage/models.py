3cdcb5b70b9d988db6200c786016c9464de1d7ff
Vishal Sharma <vishaliitr7@gmail.com>
2019-02-18 23:12:08 +0530

    Fix bug-1522520: Add homepage model to store dynamic homepage html.

--- pontoon/homepage/models.py
+++ pontoon/homepage/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Homepage(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at', )
