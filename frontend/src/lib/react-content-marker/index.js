afb24ecfb75abab3fbeef9fa4a89e7b82c01a9d9
Adrian Gaudebert <adrian@gaudebert.fr>
2019-02-25 15:32:50 +0100

    Bug 1492166 - Add a tool to mark patterns in a string. (#1210)
    This tool can be used to generate a React component that will apply a list of parsers to a string, and replace each matching pattern or term with a special marker. It currently supports RegExp and simple string terms. The markers can be anything, allowing us to define specific tags to use for each parser.
    
    Note that this tool is in a `lib` folder because I intend to extract it to a library in the future.

---
+++ frontend/src/lib/react-content-marker/index.js
@@ -0,0 +1,3 @@
+/* @flow */
+
+export { default as createMarker } from './createMarker';
---
+++ frontend/src/lib/react-content-marker/markRegExp.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import markRegExp from './markRegExp';
+
+
+describe('markRegExp', () => {
+    it('correctly marks matches of a simple pattern', () => {
+        const content = 'A horse, a horse, my kingdom for a horse.';
+
+        const res = markRegExp(content, /(horse)/, x => <mark>{x}</mark>);
+        const expected = [
+            'A ',
+            <mark>{ 'horse' }</mark>,
+            ', a ',
+            <mark>{ 'horse' }</mark>,
+            ', my kingdom for a ',
+            <mark>{ 'horse' }</mark>,
+            '.',
+        ];
+        expect(res).toEqual(expected);
+    });
+
+    it('correctly marks matches of a more complex pattern', () => {
+        const content = 'Foux du fa fa';
+
+        const res = markRegExp(content, /(f\w+)/i, x => <mark>{x}</mark>);
+        const expected = [
+            <mark>{ 'Foux' }</mark>,
+            ' du ',
+            <mark>{ 'fa' }</mark>,
+            ' ',
+            <mark>{ 'fa' }</mark>,
+        ];
+        expect(res).toEqual(expected);
+    });
+
+    it('correctly marks the entire content', () => {
+        const content = 'horse';
+
+        const res = markRegExp(content, /(horse)/, x => <mark>{x}</mark>);
+        const expected = [
+            <mark>{ 'horse' }</mark>,
+        ];
+        expect(res).toEqual(expected);
+    });
+
+    it('supports attributes in tag', () => {
+        const content = 'word';
+
+        const res = markRegExp(
+            content, /(word)/, x => <mark title='Word Finder'>{x}</mark>
+        );
+
+        const expected = [
+            <mark title='Word Finder'>{ 'word' }</mark>,
+        ];
+        expect(res).toEqual(expected);
+    });
+
+    it('returns the input as an array if there is no match', () => {
+        const content = 'A horse, a horse, my kingdom for a horse.';
+
+        const res = markRegExp(content, /(missing)/, x => <mark>{x}</mark>);
+
+        const expected = [content];
+        expect(res).toEqual(expected);
+    });
+});
