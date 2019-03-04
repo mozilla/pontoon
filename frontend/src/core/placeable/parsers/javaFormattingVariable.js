61aff6bdf35a58f5ec4232e6cda295c6559b147b
Adrian Gaudebert <adrian@gaudebert.fr>
2019-03-04 17:49:16 +0100

    Fix wrong tag titles.

--- frontend/src/core/placeable/parsers/javaFormattingVariable.js
+++ frontend/src/core/placeable/parsers/javaFormattingVariable.js
@@ -6,7 +6,7 @@ import * as React from 'react';
 const javaFormattingVariable = {
     rule: /({[0-9]+(,\s*(number(,\s*(integer|currency|percent|[-0#.,E;%\u2030\u00a4']+)?)?|(date|time)(,\s*(short|medium|long|full|.+?))?|choice,([^{]+({.+})?)+)?)?})/,
     matchIndex: 0,
-    tag: (x: string) => <mark className='placeable' title='XML entity'>
+    tag: (x: string) => <mark className='placeable' title='Java Message formatting variable'>
         { x }
     </mark>,
 };
--- frontend/src/core/placeable/parsers/pythonFormattingVariable.js
+++ frontend/src/core/placeable/parsers/pythonFormattingVariable.js
@@ -6,7 +6,7 @@ import * as React from 'react';
 const pythonFormattingVariable = {
     rule: /(%(%|(\([^)]+\)){0,1}[-+0\s#]{0,1}(\d+|\*){0,1}(\.(\d+|\*)){0,1}[hlL]{0,1}[diouxXeEfFgGcrs]{1}))/,
     matchIndex: 0,
-    tag: (x: string) => <mark className='placeable' title='XML entity'>
+    tag: (x: string) => <mark className='placeable' title='Python string formatting variable'>
         { x }
     </mark>,
 };
