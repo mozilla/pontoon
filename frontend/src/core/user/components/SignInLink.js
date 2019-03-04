ac9dd4e1644c4c430cf88e27b4fa30d0a4a849e3
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-03-04 19:48:52 +0100

    Redirect to the same page after logging in

--- frontend/src/core/user/components/SignInLink.js
+++ frontend/src/core/user/components/SignInLink.js
@@ -13,8 +13,21 @@ type Props = {|
  * Render a link to the Sign In process.
  */
 export default class SignInLink extends React.Component<Props> {
+    generateSignInURL() {
+        const base = this.props.url;
+        const startSign = base.match(/\?/) ? '&': '?';
+
+        return (
+            base +
+            startSign +
+            'next=' +
+            window.location.pathname +
+            window.location.search
+        );
+    }
+
     render() {
-        return <a href={ this.props.url }>
+        return <a href={ this.generateSignInURL() }>
             { this.props.children }
         </a>;
     }
