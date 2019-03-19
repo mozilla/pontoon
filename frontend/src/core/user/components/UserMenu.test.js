8039929015d3d43dc0870aeb9605b7ff1631d172
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-03-19 18:38:48 +0100

    Factor out createShallowUserMenu

--- frontend/src/core/user/components/UserMenu.test.js
+++ frontend/src/core/user/components/UserMenu.test.js
@@ -8,38 +8,43 @@ import { UserMenuBase } from './UserMenu';
 import { findLocalizedById } from 'test/utils';
 
 
+function createShallowUserMenu({
+    isAdmin = false,
+    isReadOnly = false,
+    isTranslator = true,
+    isAuthenticated = true,
+    locale = 'mylocale',
+    project = 'myproject',
+    resource = 'myresource',
+} = {}) {
+    return shallow(
+        <UserMenuBase
+            isReadOnly={ isReadOnly }
+            isTranslator={ isTranslator }
+            user={ { isAuthenticated: isAuthenticated, isAdmin: isAdmin } }
+            parameters={ { locale: locale, project: project, resource: resource } }
+        />
+    );
+}
+
+
 describe('<UserMenuBase>', () => {
     it('shows the user avatar when the user is logged in', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject' } }
-            />
-        );
+        const wrapper = createShallowUserMenu();
 
         expect(wrapper.find('img')).toHaveLength(1);
         expect(wrapper.find('.menu-icon')).toHaveLength(0);
     });
 
     it('shows the general menu icon when the user is logged out', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                user={ { isAuthenticated: false } }
-                parameters={ { locale: 'mylocale', project: 'myproject' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ isAuthenticated: false });
 
         expect(wrapper.find('img')).toHaveLength(0);
         expect(wrapper.find('.menu-icon')).toHaveLength(1);
     });
 
     it('shows the right menu items when the user is logged in', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'locale', project: 'project' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ locale: 'locale', project: 'project' });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find('.details')).toHaveLength(1);
@@ -48,12 +53,7 @@ describe('<UserMenuBase>', () => {
     });
 
     it('hides the right menu items when the user is logged out', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                user={ { isAuthenticated: false } }
-                parameters={ { locale: 'mylocale', project: 'myproject' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ isAuthenticated: false });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find('.details')).toHaveLength(0);
@@ -62,13 +62,7 @@ describe('<UserMenuBase>', () => {
     });
 
     it('shows upload & download menu items', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                isTranslator={ true }
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject', resource: 'myresource' } }
-            />
-        );
+        const wrapper = createShallowUserMenu();
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find(FileUpload)).toHaveLength(1);
@@ -76,13 +70,7 @@ describe('<UserMenuBase>', () => {
     });
 
     it('hides upload & download menu items when translating all projects', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                isTranslator={ true }
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'all-projects', resource: 'myresource' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ project: 'all-projects' });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
@@ -90,53 +78,29 @@ describe('<UserMenuBase>', () => {
     });
 
     it('hides upload & download menu items when translating all resources', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                isTranslator={ true }
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject', resource: 'all-resources' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ resource: 'all-resources' });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
         expect(findLocalizedById(wrapper, 'user-UserMenu--download-translations')).toHaveLength(0);
     });
 
-    it('hides upload menu item fors users without permission to review translations', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                isTranslator={ false }
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject', resource: 'myresource' } }
-            />
-        );
+    it('hides upload menu item for users without permission to review translations', () => {
+        const wrapper = createShallowUserMenu({ isTranslator: false });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
     });
 
     it('hides upload menu for read-only strings', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                isReadOnly={ true }
-                isTranslator={ true }
-                user={ { isAuthenticated: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject', resource: 'myresource' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ isReadOnly: true });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
     });
 
     it('shows the admin menu items when the user is an admin', () => {
-        const wrapper = shallow(
-            <UserMenuBase
-                user={ { isAuthenticated: true, isAdmin: true } }
-                parameters={ { locale: 'mylocale', project: 'myproject' } }
-            />
-        );
+        const wrapper = createShallowUserMenu({ isAdmin: true });
         wrapper.instance().setState({visible: true});
 
         expect(wrapper.find('a[href="/admin/"]')).toHaveLength(1);
