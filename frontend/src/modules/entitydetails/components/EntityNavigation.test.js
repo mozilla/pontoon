2b71a6dad97f53e8da9234432b46e5924b3f9696
Adrian Gaudebert <adrian@gaudebert.fr>
2019-03-29 18:48:35 +0100

    Bug 1525984 - Add navigation keyboard shortcuts. (#1243)

---
+++ frontend/src/modules/entitydetails/components/EntityNavigation.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import EntityNavigation from './EntityNavigation';
+
+
+describe('<EntityNavigation>', () => {
+    function getEntityNav({ create = shallow } = {}) {
+        const nextMock = sinon.stub();
+        const prevMock = sinon.stub();
+        const wrapper = create(<EntityNavigation
+            goToNextEntity={ nextMock }
+            goToPreviousEntity={ prevMock }
+        />);
+
+        return {
+            wrapper,
+            nextMock,
+            prevMock,
+        };
+    }
+
+    it('goes to the next entity on click on the Next button', () => {
+        const { wrapper, nextMock } = getEntityNav();
+
+        expect(nextMock.calledOnce).toBeFalsy();
+        wrapper.find('button.next').simulate('click');
+        expect(nextMock.calledOnce).toBeTruthy();
+    });
+
+    it('goes to the next entity on Alt + Down', () => {
+        // Simulating the key presses on `document`.
+        // See https://github.com/airbnb/enzyme/issues/426
+        const eventsMap = {};
+        document.addEventListener = sinon.spy((event, cb) => {
+            eventsMap[event] = cb;
+        });
+
+        const { nextMock } = getEntityNav(mount);
+
+        expect(nextMock.calledOnce).toBeFalsy();
+        const event = {
+            preventDefault: sinon.spy(),
+            keyCode: 40,  // Down
+            altKey: true,
+            ctrlKey: false,
+            shiftKey: false,
+        };
+        eventsMap.keydown(event);
+        expect(nextMock.calledOnce).toBeTruthy();
+    });
+
+    it('goes to the previous entity on click on the Previous button', () => {
+        const { wrapper, prevMock } = getEntityNav();
+
+        expect(prevMock.calledOnce).toBeFalsy();
+        wrapper.find('button.previous').simulate('click');
+        expect(prevMock.calledOnce).toBeTruthy();
+    });
+
+    it('goes to the previous entity on Alt + Up', () => {
+        // Simulating the key presses on `document`.
+        // See https://github.com/airbnb/enzyme/issues/426
+        const eventsMap = {};
+        document.addEventListener = sinon.spy((event, cb) => {
+            eventsMap[event] = cb;
+        });
+
+        const { prevMock } = getEntityNav(mount);
+
+        expect(prevMock.calledOnce).toBeFalsy();
+        const event = {
+            preventDefault: sinon.spy(),
+            keyCode: 38,  // Up
+            altKey: true,
+            ctrlKey: false,
+            shiftKey: false,
+        };
+        eventsMap.keydown(event);
+        expect(prevMock.calledOnce).toBeTruthy();
+    });
+});
