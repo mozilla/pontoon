b16c1f098b0406efbd152838ecc55e8734172dec
Adrian Gaudebert <adrian@gaudebert.fr>
2019-03-29 18:48:35 +0100

    Bug 1525984 - Add navigation keyboard shortcuts. (#1243)

--- frontend/src/modules/entitydetails/components/EntityNavigation.js
+++ frontend/src/modules/entitydetails/components/EntityNavigation.js
@@ -18,12 +18,44 @@ type Props = {|
  * Shows next/previous buttons.
  */
 export default class EntityNavigation extends React.Component<Props> {
+    componentDidMount() {
+        // $FLOW_IGNORE (errors that I don't understand, no help from the Web)
+        document.addEventListener('keydown', this.handleShortcuts);
+    }
+
+    componentWillUnmount() {
+        // $FLOW_IGNORE (errors that I don't understand, no help from the Web)
+        document.removeEventListener('keydown', this.handleShortcuts);
+    }
+
     goToNextEntity = () => {
         this.props.goToNextEntity();
     }
 
     goToPreviousEntity = () => {
         this.props.goToPreviousEntity();
+    }
+
+    handleShortcuts = (event: SyntheticKeyboardEvent<>) => {
+        const key = event.keyCode;
+
+        let handledEvent = false;
+
+        // On Alt + Up, move to the previous entity.
+        if (key === 38 && event.altKey && !event.ctrlKey && !event.shiftKey) {
+            handledEvent = true;
+            this.goToPreviousEntity();
+        }
+
+        // On Alt + Down, move to the next entity.
+        if (key === 40 && event.altKey && !event.ctrlKey && !event.shiftKey) {
+            handledEvent = true;
+            this.goToNextEntity();
+        }
+
+        if (handledEvent) {
+            event.preventDefault();
+        }
     }
 
     render(): React.Node {
