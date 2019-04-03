e3618e62e45ae9479da8e1ab68956fac011526c9
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-04-03 22:29:12 +0200

    Dedent

--- frontend/src/modules/otherlocales/components/Translation.js
+++ frontend/src/modules/otherlocales/components/Translation.js
@@ -25,45 +25,45 @@ type Props = {|
  * Show the translation of a given entity in a different locale, as well as the
  * locale and its code.
  */
- export default class Translation extends React.Component<Props> {
-     copyTranslationIntoEditor = () => {
-         if (this.props.isReadOnlyEditor) {
-             return;
-         }
-         this.props.updateEditorTranslation(this.props.translation.translation);
-     }
+export default class Translation extends React.Component<Props> {
+    copyTranslationIntoEditor = () => {
+        if (this.props.isReadOnlyEditor) {
+            return;
+        }
+        this.props.updateEditorTranslation(this.props.translation.translation);
+    }
 
-     render() {
-         const { translation, parameters, lastPreferred } = this.props;
+    render() {
+        const { translation, parameters, lastPreferred } = this.props;
 
-         const className = lastPreferred ? 'translation last-preferred' : 'translation';
+        const className = lastPreferred ? 'translation last-preferred' : 'translation';
 
-         return <Localized id='otherlocales-translation-copy' attrs={{ title: true }}>
-             <li
-                className={ className }
-                title='Copy Into Translation (Tab)'
-                onClick={ this.copyTranslationIntoEditor }
-             >
-                 <header>
-                     <a
-                         href={ `/translate/${translation.code}/${parameters.project}/${parameters.resource}/?string=${parameters.entity}` }
-                         target="_blank"
-                         rel="noopener noreferrer"
-                     >
-                         { translation.locale }
-                         <span>{ translation.code }</span>
-                     </a>
-                 </header>
-                 <p
-                     lang={ translation.code }
-                     dir={ translation.direction }
-                     script={ translation.script }
-                 >
-                     <WithPlaceables>
-                         { translation.translation }
-                     </WithPlaceables>
-                 </p>
-             </li>
-         </Localized>;
-     }
- }
+        return <Localized id='otherlocales-translation-copy' attrs={{ title: true }}>
+            <li
+            className={ className }
+            title='Copy Into Translation (Tab)'
+            onClick={ this.copyTranslationIntoEditor }
+            >
+                <header>
+                    <a
+                        href={ `/translate/${translation.code}/${parameters.project}/${parameters.resource}/?string=${parameters.entity}` }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        { translation.locale }
+                        <span>{ translation.code }</span>
+                    </a>
+                </header>
+                <p
+                    lang={ translation.code }
+                    dir={ translation.direction }
+                    script={ translation.script }
+                >
+                    <WithPlaceables>
+                        { translation.translation }
+                    </WithPlaceables>
+                </p>
+            </li>
+        </Localized>;
+    }
+}
