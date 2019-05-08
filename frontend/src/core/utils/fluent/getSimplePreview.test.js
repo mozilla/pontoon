c226a137d474f6b2a4c71833190bd1180e0eeff0
Adrian Gaudebert <adrian@gaudebert.fr>
2019-05-08 18:27:47 +0200

    Update tests to cover more cases and fix Fluent messages that were on multiple lines.

--- frontend/src/core/utils/fluent/getSimplePreview.test.js
+++ frontend/src/core/utils/fluent/getSimplePreview.test.js
@@ -6,6 +6,10 @@ describe('getSimplePreview', () => {
         expect(getSimplePreview('')).toEqual('');
     });
 
+    it('works for a null value', () => {
+        expect(getSimplePreview(null)).toEqual('');
+    });
+
     it('works for a non-FTL string', () => {
         expect(getSimplePreview('I am inevitable')).toEqual('I am inevitable');
     });
@@ -17,11 +21,10 @@ describe('getSimplePreview', () => {
     });
 
     it('returns the value for a multiline Message', () => {
-        const message = `
-            summary =
-                Heroes
-                beat
-                the Villain
+        const message = `summary =
+            Heroes
+            beat
+            the Villain
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('Heroes\nbeat\nthe Villain');
@@ -34,39 +37,46 @@ describe('getSimplePreview', () => {
     });
 
     it('returns the attribute when there are no values and an attribute', () => {
-        const message = `
-            hawkeye =
-                .real-name = Clint Barton
+        const message = `hawkeye =
+            .real-name = Clint Barton
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('Clint Barton');
     });
 
     it('returns the value when there is a value and an attribute', () => {
-        const message = `
-            ironman-slogan = I am Ironman!
-                .attributed-to = Tony Stark
+        const message = `ironman-slogan = I am Ironman!
+            .attributed-to = Tony Stark
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('I am Ironman!');
     });
 
     it('returns the first attribute when there are several', () => {
-        const message = `
-            thor =
-                .first-movie = Thor
-                .second-movie = The Dark World
-                .third-movie = Ragnarok
+        const message = `thor =
+            .first-movie = Thor
+            .second-movie = The Dark World
+            .third-movie = Ragnarok
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('Thor');
     });
 
+    it('returns the default value for plurals 2', () => {
+        const message = `key =
+        { $number ->
+            [1] Simple String
+           *[other] Other Simple String
+        }`;
+        const res = getSimplePreview(message);
+        expect(res).toEqual('Other Simple String');
+    });
+
     it('returns the default value for plurals', () => {
-        const message = `
-            stones-number =
+        const message = `stones-number =
             Thanos has { $number ->
                 [0] no Stones
+                [1] 1 Stone
                 [6] all the Stones
                *[other] { $number } Stones
             }
@@ -76,8 +86,7 @@ describe('getSimplePreview', () => {
     });
 
     it('returns the default value for selectors', () => {
-        const message = `
-            who-dies =
+        const message = `who-dies =
             { $who ->
                 [female] Black Widow
                 [male] Hawkeye
@@ -89,20 +98,20 @@ describe('getSimplePreview', () => {
     });
 
     it('returns the default value for a selector in an attribute', () => {
-        const message = `
-            ironman =
-                .talking-ia = { PLATFORM() ->
-                    [win] Friday
-                   *[other] Jarvis
-                }
+        const message = `ironman =
+            .talking-ia = { PLATFORM() ->
+                [win] Friday
+               *[other] Jarvis
+            }
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('Jarvis');
     });
 
     it('works with function reference', () => {
-        const message = `
-            explore = { LINK("Wikipedia", title: "Go to Wikipedia") }Read more
+        const message = `explore = {
+            LINK("Wikipedia", title: "Go to Wikipedia")
+        }Read more
         `;
         const res = getSimplePreview(message);
         expect(res).toEqual('{ LINK("Wikipedia", title: "Go to Wikipedia") }Read more');
