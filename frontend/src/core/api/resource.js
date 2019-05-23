fdd504dadfd1eb7a8656dffa7b0009188129fb47
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-05-23 17:09:34 +0200

    Nicer syntax

--- frontend/src/core/api/resource.js
+++ frontend/src/core/api/resource.js
@@ -5,7 +5,7 @@ import APIBase from './base';
 
 export default class ResourceAPI extends APIBase {
     async getAll(locale: string, project: string) {
-        const url = '/' + locale + '/' + project + '/parts/';
+        const url = `/${locale}/${project}/parts/`;
 
         const headers = new Headers();
         headers.append('X-Requested-With', 'XMLHttpRequest');
