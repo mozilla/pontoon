647490da1fe5afea0cc1b17f69ef84f660916fee
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-05-28 14:23:49 +0200

    Format numbers in resource progress and filters (#1304)
    Use comma to separate thousands in Resource Progress and Filter.
    
    Also: Do not show resource progress until stats are available.

--- frontend/src/core/utils/index.js
+++ frontend/src/core/utils/index.js
@@ -5,6 +5,7 @@ export { default as fluent } from './fluent';
 
 // Functions
 export { default as getOptimizedContent } from './getOptimizedContent';
+export { default as asLocaleString } from './asLocaleString';
 
 // Components and HOC
 export { default as withActionsDisabled } from './components/withActionsDisabled';
