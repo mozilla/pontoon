bca1340617e18df21c19cefef30c488918a17242
Jarosław Śmiejczak <jot@evilb.it>
2019-06-11 00:01:55 +0200

    unify __future__ imports

--- pontoon/base/models.py
+++ pontoon/base/models.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from __future__ import unicode_literals
-from __future__ import division
+from __future__ import absolute_import, division, unicode_literals
 
 import hashlib
 import json
--- pontoon/localizations/views.py
+++ pontoon/localizations/views.py
@@ -1,6 +1,4 @@
-from __future__ import absolute_import
-
-from __future__ import division
+from __future__ import absolute_import, division
 
 import math
 
