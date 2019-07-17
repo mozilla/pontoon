97264277c6b00fa6305be3aa0b28e8aa40ccb427
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-07-17 17:56:26 +0200

    Fix bug 1511357: Highlight search terms in entity list (#1336)

--- frontend/src/core/translation/components/FluentTranslation.js
+++ frontend/src/core/translation/components/FluentTranslation.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { withDiff } from 'core/diff';
 import { WithPlaceablesForFluent, WithPlaceablesForFluentNoLeadingSpace } from 'core/placeable';
 import { fluent } from 'core/utils';
+import { withSearch } from 'modules/search';
 
 import type { TranslationProps } from './GenericTranslation';
 
@@ -12,16 +13,25 @@ import type { TranslationProps } from './GenericTranslation';
 // $FLOW_IGNORE: I just can't get HOC working with Flow.
 const TranslationPlaceablesDiff = withDiff(WithPlaceablesForFluentNoLeadingSpace);
 
+// $FLOW_IGNORE: I just can't get HOC working with Flow.
+const TranslationPlaceablesSearch = withSearch(WithPlaceablesForFluentNoLeadingSpace);
+
 
 export default class FluentTranslation extends React.Component<TranslationProps> {
     render() {
-        const { content, diffTarget } = this.props;
+        const { content, diffTarget, search } = this.props;
 
         if (diffTarget) {
             const fluentTarget = fluent.getSimplePreview(diffTarget);
             return <TranslationPlaceablesDiff diffTarget={ fluentTarget }>
                 { fluent.getSimplePreview(content) }
             </TranslationPlaceablesDiff>;
+        }
+
+        if (search) {
+            return <TranslationPlaceablesSearch search={ search }>
+                { fluent.getSimplePreview(content) }
+            </TranslationPlaceablesSearch>;
         }
 
         return <WithPlaceablesForFluent>
---
+++ frontend/src/modules/search/withSearch.js
@@ -0,0 +1,53 @@
+/* @flow */
+
+import * as React from 'react';
+import shortid from 'shortid';
+import { mark } from 'react-content-marker';
+
+
+export function markSearchTerms(base: string, search: string){
+    // Split search string on spaces except if between non-escaped quotes.
+    const unusable = '☠';
+    const searchTerms = search.replace(/\\"/g, unusable).match(/[^\s"]+|"[^"]+"/g);
+
+    if (searchTerms) {
+        const reg = new RegExp(unusable, 'g');
+        var i = searchTerms.length;
+
+        while(i--) {
+            searchTerms[i] = searchTerms[i].replace(/^["]|["]$/g, '');
+            searchTerms[i] = searchTerms[i].replace(reg, '"');
+        }
+
+        // Sort array in decreasing order of string length
+        searchTerms.sort(function(a, b) {
+            return b.length - a.length;
+        });
+
+        for (let searchTerm of searchTerms) {
+            const rule = new RegExp(searchTerm, 'i');
+            const tag = (x: string) =>
+                <mark className='search' key={ shortid.generate() }>{ x }</mark>;
+
+            base = mark(base, rule, tag);
+        }
+    }
+
+    return base;
+}
+
+
+type Props = {
+    search: string,
+};
+
+
+export default function withSearch<Config: Object>(
+    WrappedComponent: React.AbstractComponent<Config>
+): React.AbstractComponent<Config> {
+    return function WithSearch(props: { ...Config, ...Props }) {
+        return <WrappedComponent { ...props }>
+            { markSearchTerms(props.children, props.search) }
+        </WrappedComponent>;
+    };
+}
