fb86afd462f0ba295a945e5b69b049d087c428db
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-09-17 15:46:38 +0200

    Fix bug 1543345: Implement Fluent Source Editor shortcuts (#1381)
    Details:
    * Wrap connectedEditor component into a withActionsDisabled HOC
    * Move handleShortcuts to connectedEditor and reuse it in Generic and Source Editor
    * Move handleShortcuts tests from GenericEditorForm to connectedEditor
    * Keep handleShortcuts in RichEditor, which uses custom functions

--- frontend/src/modules/genericeditor/components/GenericTranslationForm.test.js
+++ frontend/src/modules/genericeditor/components/GenericTranslationForm.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import { GenericTranslationFormBase } from './GenericTranslationForm';
+import GenericTranslationForm from './GenericTranslationForm';
 
 
 const DEFAULT_LOCALE = {
@@ -18,9 +18,9 @@ const EDITOR = {
 };
 
 
-describe('<GenericTranslationFormBase>', () => {
+describe('<GenericTranslationForm>', () => {
     it('renders a textarea with some content', () => {
-        const wrapper = shallow(<GenericTranslationFormBase
+        const wrapper = shallow(<GenericTranslationForm
             editor={ EDITOR }
             locale={ DEFAULT_LOCALE }
         />);
@@ -31,7 +31,7 @@ describe('<GenericTranslationFormBase>', () => {
 
     it('calls the updateTranslation function on change', () => {
         const mockUpdate = sinon.spy();
-        const wrapper = shallow(<GenericTranslationFormBase
+        const wrapper = shallow(<GenericTranslationForm
             editor={ EDITOR }
             locale={ DEFAULT_LOCALE }
             updateTranslation={ mockUpdate }
@@ -46,7 +46,7 @@ describe('<GenericTranslationFormBase>', () => {
         const resetMock = sinon.stub();
         const updateMock = sinon.stub();
 
-        const wrapper = mount(<GenericTranslationFormBase
+        const wrapper = mount(<GenericTranslationForm
             editor={ EDITOR }
             locale={ DEFAULT_LOCALE }
             unsavedchanges={ { shown: false } }
@@ -60,171 +60,5 @@ describe('<GenericTranslationFormBase>', () => {
         expect(updateMock.calledOnce).toBeTruthy();
         expect(updateMock.calledWith('hello world')).toBeTruthy();
         expect(resetMock.calledOnce).toBeTruthy();
-    });
-
-    it('sends the translation on Enter', () => {
-        const mockSend = sinon.spy();
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ EDITOR }
-            locale={ DEFAULT_LOCALE }
-            sendTranslation={ mockSend }
-            disableAction={ sinon.spy() }
-            unsavedchanges={ { shown: false } }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 13,  // Enter
-            altKey: false,
-            ctrlKey: false,
-            shiftKey: false,
-        };
-
-        expect(mockSend.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockSend.calledOnce).toBeTruthy();
-    });
-
-    it('approves the translation on Enter if failed checks triggered by approval', () => {
-        const mockSend = sinon.spy();
-
-        const editor = {
-            ...EDITOR,
-            errors: ['error1'],
-            warnings: ['warning1'],
-            source: 1,
-        }
-
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ editor }
-            locale={ DEFAULT_LOCALE }
-            updateTranslationStatus={ mockSend }
-            disableAction={ sinon.spy() }
-            unsavedchanges={ { shown: false } }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 13,  // Enter
-            altKey: false,
-            ctrlKey: false,
-            shiftKey: false,
-        };
-
-        expect(mockSend.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockSend.calledOnce).toBeTruthy();
-    });
-
-    it('ignores unsaved changes on Enter if unsaved changes popup is shown', () => {
-        const mockSend = sinon.spy();
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ EDITOR }
-            locale={ DEFAULT_LOCALE }
-            ignoreUnsavedChanges={ mockSend }
-            disableAction={ sinon.spy() }
-            unsavedchanges={ { shown: true } }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 13,  // Enter
-            altKey: false,
-            ctrlKey: false,
-            shiftKey: false,
-        };
-
-        expect(mockSend.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockSend.calledOnce).toBeTruthy();
-    });
-
-    it('closes unsaved changes popup if open on Esc', () => {
-        const mockSend = sinon.spy();
-
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ EDITOR }
-            locale={ DEFAULT_LOCALE }
-            hideUnsavedChanges={ mockSend }
-            unsavedchanges={ { shown: true } }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 27,  // Esc
-        };
-
-        expect(mockSend.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockSend.calledOnce).toBeTruthy();
-    });
-
-    it('closes failed checks popup if open on Esc', () => {
-        const mockSend = sinon.spy();
-
-        const editor = {
-            ...EDITOR,
-            errors: ['error1'],
-            warnings: ['warning1'],
-        }
-
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ editor }
-            locale={ DEFAULT_LOCALE }
-            resetFailedChecks={ mockSend }
-            unsavedchanges={ { shown: false } }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 27,  // Esc
-        };
-
-        expect(mockSend.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockSend.calledOnce).toBeTruthy();
-    });
-
-    it('copies the original into the Editor on Ctrl + Shift + C', () => {
-        const mockCopy = sinon.spy();
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ EDITOR }
-            locale={ DEFAULT_LOCALE }
-            copyOriginalIntoEditor={ mockCopy }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 67,  // C
-            altKey: false,
-            ctrlKey: true,
-            shiftKey: true,
-        };
-
-        expect(mockCopy.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockCopy.calledOnce).toBeTruthy();
-    });
-
-    it('clears the translation on Ctrl + Shift + Backspace', () => {
-        const mockUpdate = sinon.spy();
-        const wrapper = shallow(<GenericTranslationFormBase
-            editor={ EDITOR }
-            locale={ DEFAULT_LOCALE }
-            updateTranslation={ mockUpdate }
-        />);
-
-        const event = {
-            preventDefault: sinon.spy(),
-            keyCode: 8,  // Backspace
-            altKey: false,
-            ctrlKey: true,
-            shiftKey: true,
-        };
-
-        expect(mockUpdate.calledOnce).toBeFalsy();
-        wrapper.find('textarea').simulate('keydown', event);
-        expect(mockUpdate.calledOnce).toBeTruthy();
-        expect(mockUpdate.calledWith('')).toBeTruthy();
     });
 });
