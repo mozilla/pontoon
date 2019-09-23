01a76691d5e456c2b2d549bfdf83bf50a2c10b8a
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-09-23 20:15:09 +0200

    Add tests

---
+++ frontend/src/modules/interactivetour/components/InteractiveTour.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Tour from 'reactour'
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import { InteractiveTourBase } from './InteractiveTour';
+
+
+describe('<InteractiveTourBase>', () => {
+    it('renders correctly on the tutorial page for unauthenticated user', () => {
+        const wrapper = shallow(<InteractiveTourBase
+            locale={{ code: 'lc' }}
+            project={{ slug: 'tutorial' }}
+            user={{ isAuthenticated: false }}
+        />);
+
+        expect(wrapper.find(Tour)).toHaveLength(1);
+    });
+
+    it('does not render on non-tutorial page', () => {
+        const wrapper = shallow(<InteractiveTourBase
+            locale={{ code: 'lc' }}
+            project={{ slug: 'firefox' }}
+            user={{ isAuthenticated: false }}
+        />);
+
+        expect(wrapper.find(Tour)).toHaveLength(0);
+    });
+
+    it('does not render if the user has already seen the tutorial', () => {
+        const wrapper = shallow(<InteractiveTourBase
+            locale={{ code: 'lc' }}
+            project={{ slug: 'tutorial' }}
+            // the user has seen the tutorial
+            user={{ tourStatus: -1 }}
+        />);
+
+        expect(wrapper.find(Tour)).toHaveLength(0);
+    });
+});
