8d7a559daa345ac5c8e115ff09aa988de345f98d
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-09-26 15:08:24 +0200

    Add tests

--- frontend/src/core/placeable/parsers/fluentFunction.test.js
+++ frontend/src/core/placeable/parsers/fluentFunction.test.js
@@ -18,4 +18,15 @@ describe('fluentFunction', () => {
         expect(wrapper.find('mark')).toHaveLength(1);
         expect(wrapper.find('mark').text()).toEqual(mark);
     });
+
+    each([
+        ['{ DATETIME($date) }', '{ COPY() }', 'Hello { DATETIME($date) } and { COPY() }'],
+    ])
+    .it('marks `%s` and `%s` in `%s`', (mark1, mark2, content) => {
+        const Marker = createMarker([fluentFunction]);
+        const wrapper = shallow(<Marker>{ content }</Marker>);
+        expect(wrapper.find('mark')).toHaveLength(2);
+        expect(wrapper.find('mark').at(0).text()).toEqual(mark1);
+        expect(wrapper.find('mark').at(1).text()).toEqual(mark2);
+    });
 });
