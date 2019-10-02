8cbca71c084a3057db1366a2fbc0cb26aaf9a721
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-02 09:13:22 +0200

    Fix bug 1585087 - Escape search user input for RegExp. (#1404)

---
+++ frontend/src/modules/search/withSearch.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { markSearchTerms } from './withSearch';
+
+
+describe('markSearchTerms', () => {
+    it('does not break on regexp special characters', () => {
+        const marked = markSearchTerms('foo (bar)', '(bar');
+        const wrapper = shallow(<p>{ marked }</p>);
+        expect(wrapper.find('mark')).toHaveLength(1);
+        expect(wrapper.find('mark').text()).toEqual('(bar');
+    });
+});
