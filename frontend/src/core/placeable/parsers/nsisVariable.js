e1d582936b5674e24a05359901b5b4713093ba09
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-10 17:59:05 +0200

    Fix bug 1251186 - Add a new placeable parser for NSIS variables.

---
+++ frontend/src/core/placeable/parsers/nsisVariable.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+import * as React from 'react';
+import { Localized } from '@fluent/react';
+
+
+/**
+ * Marks NSIS variables.
+ *
+ * Example matches:
+ *
+ *   $Brand
+ *   $BrandShortName
+ */
+const nsisVariable = {
+    rule: /(^|\s)(\$[a-zA-Z][\w]*)/,
+    matchIndex: 2,
+    tag: (x: string) => {
+        return <Localized
+            id='placeable-parser-nsisVariable'
+            attrs={{ title: true }}
+        >
+            <mark className='placeable' title='NSIS variable'>
+                { x }
+            </mark>
+        </Localized>;
+    },
+};
+
+export default nsisVariable;
