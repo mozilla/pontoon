d375d12b1720bfaad947214212f95bc1d2f7b18e
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-10 15:57:12 +0200

    Bug 1251186 - Do not match numbers before letters.

--- frontend/src/core/placeable/parsers/numberString.js
+++ frontend/src/core/placeable/parsers/numberString.js
@@ -18,7 +18,7 @@ import { Localized } from '@fluent/react';
  * https://github.com/translate/translate/blob/2.3.1/translate/storage/placeables/general.py#L72
  */
 const numberString = {
-    rule: /([-+]?[0-9]+([\u00A0.,][0-9]+)*)/u,
+    rule: /([-+]?[0-9]+([\u00A0.,][0-9]+)*)\b/u,
     matchIndex: 0,
     tag: (x: string) => {
         return <Localized
