67d795e9698e85c52e116f9241683e010729eb1f
Adrian Gaudebert <adrian@gaudebert.fr>
2019-10-11 12:19:17 +0200

    Bug 1251186 - Add a placeable to match short combinations of a capital letter and a number.

---
+++ frontend/src/core/placeable/parsers/shortCapitalNumberString.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import each from 'jest-each';
+
+import createMarker from 'react-content-marker';
+
+import shortCapitalNumberString from './shortCapitalNumberString';
+
+
+describe('shortCapitalNumberString', () => {
+    each([
+        ['3D', '3D'],
+        ['A4', 'Use the A4 paper'],
+    ])
+    .it('marks `%s` in `%s`', (mark, content) => {
+        const Marker = createMarker([shortCapitalNumberString]);
+        const wrapper = shallow(<Marker>{ content }</Marker>);
+        expect(wrapper.find('mark')).toHaveLength(1);
+        expect(wrapper.find('mark').text()).toEqual(mark);
+    });
+
+    each([
+        ['I am'],
+        ['3d'],
+        ['3DS'],
+    ])
+    .it('does not mark anything in `%s`', (content) => {
+        const Marker = createMarker([shortCapitalNumberString]);
+        const wrapper = shallow(<Marker>{ content }</Marker>);
+        expect(wrapper.find('mark')).toHaveLength(0);
+    });
+});
