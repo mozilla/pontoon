b9cd279207aaa2456c03e34077ec3d798620234c
Karskaja <karskaja@gmail.com>
2019-10-13 18:52:57 -0400

    Fix bug 1583821 - Languages are deactivated when they don't have projects
    Final changes for: There are no projects enabled for the team yet.

--- pontoon/teams/static/js/request.js
+++ pontoon/teams/static/js/request.js
@@ -17,15 +17,18 @@ var Pontoon = (function (my) {
         if (type === 'locale-projects') {
           var localeProjects = $('#server').data('locale-projects');
 
+          //Show table and search box, hide no-result message
+            $('.search-wrapper:hidden').toggle();
+            $('.controls.no-projects').toggleClass('no-projects');
+            $('.project-list:hidden').toggle();
+            $('.no-results').toggle(false);
+
           // Hide all projects
           $('.items')
             .toggleClass('request', !show)
             .find('tbody tr')
               .toggleClass('limited', !show)
               .toggle(!show);
-
-          $('.project-list').toggle(!show);
-          $('.search-wrapper').toggle(!show);
 
           // Show requested projects
           $(localeProjects).each(function() {
@@ -35,6 +38,14 @@ var Pontoon = (function (my) {
                 .toggleClass('limited', show)
                 .toggle(show);
           });
+
+          //Hide search box and table, show no result message if no project displayed
+          if ($('tr.limited').length === 0 && show) {
+            $('.search-wrapper').toggle(!show);
+            $('.project-list').toggle(!show);
+            $('menu.controls').toggleClass('no-projects');
+            $('.no-results').toggle(show);
+          }
 
           Pontoon.requestItem.toggleButton(!show, 'locale-projects');
         }
@@ -66,6 +77,7 @@ var Pontoon = (function (my) {
 
         $('#request-item-note').toggle(show);
         $('#request-item').toggle(show);
+        a = $('tr.limited').length;
       },
 
       requestProjects: function(locale, projects, type) {
