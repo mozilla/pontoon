865d79d8b48b960f19b0b579728c7f195653e4c9
monikamaheshwari <monikamaheshwari1996@gmail.com>
2019-10-22 12:50:38 +0530

    middot changes

--- frontend/src/modules/entitydetails/components/Metadata.js
+++ frontend/src/modules/entitydetails/components/Metadata.js
@@ -170,6 +170,7 @@ export default class Metadata extends React.Component<Props> {
                     <a href={ `/${locale.code}/${entity.project.slug}/` }>
                         { entity.project.name }
                     </a>
+                    <span> &middot; </span>
                     <a
                         href={ `/${locale.code}/${entity.project.slug}/${entity.path}/` }
                         onClick={ this.navigateToPath }
