a9421ec2b9c711fe8f14d76a638e1fd12ed876b1
eraulia nadinda <dinda.rachmat@gmail.com>
2019-10-23 17:32:30 +0200

    Bug 1579655 - Write tests for fluentoriginal module (#1446)

---
+++ frontend/src/modules/fluentoriginal/components/RichString.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import RichString from './RichString';
+
+
+const ORIGINAL = `
+song-title = Hello
+    .genre = Pop
+    .album = Hello and Good Bye`;
+
+const ENTITY = {
+    original: ORIGINAL,
+};
+
+describe('<RichString>', () => {
+    it('renders value and each attribute correctly', () => {
+        const wrapper = shallow(<RichString
+            entity = { ENTITY }
+        />);
+
+        expect(wrapper.find('ContentMarker')).toHaveLength(3);
+
+        expect(wrapper.find('label').at(0).html()).toContain('Value');
+        expect(wrapper.find('ContentMarker').at(0).html()).toContain('Hello');
+
+        expect(wrapper.find('label').at(1).html()).toContain('genre');
+        expect(wrapper.find('ContentMarker').at(1).html()).toContain('Pop');
+
+        expect(wrapper.find('label').at(2).html()).toContain('album');
+        expect(wrapper.find('ContentMarker').at(2).html()).toContain('Hello and Good Bye');
+    });
+
+    it('renders select expression correctly', () => {
+        const input = `
+user-entry =
+    { PLATFORM() ->
+        [variant-1] Hello!
+       *[variant-2] Good Bye!
+    }`;
+
+        const entity = {
+            original: input,
+        };
+
+        const wrapper = shallow(<RichString
+            entity = { entity }
+        />);
+
+        expect(wrapper.find('ContentMarker')).toHaveLength(2);
+
+        expect(wrapper.find('label').at(0).html()).toContain('variant-1');
+        expect(wrapper.find('ContentMarker').at(0).html()).toContain('Hello!');
+
+        expect(wrapper.find('label').at(1).html()).toContain('variant-2');
+        expect(wrapper.find('ContentMarker').at(1).html()).toContain('Good Bye!');
+    });
+
+    it('calls the handleClickOnPlaceable function on click on .original', () => {
+        const handleClickOnPlaceable = sinon.spy();
+        const wrapper = shallow(<RichString
+            entity = { ENTITY }
+            handleClickOnPlaceable={ handleClickOnPlaceable }
+        />);
+
+        wrapper.find('.original').simulate('click');
+        expect(handleClickOnPlaceable.called).toEqual(true);
+    });
+});
