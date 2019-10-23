eecceaa40e59881fd5b72fc45a32d7e0cfaa9db5
eraulia nadinda <dinda.rachmat@gmail.com>
2019-10-23 17:32:30 +0200

    Bug 1579655 - Write tests for fluentoriginal module (#1446)

---
+++ frontend/src/modules/fluentoriginal/components/SimpleString.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import SimpleString from './SimpleString';
+
+
+const ORIGINAL = `header = 
+            .page-title = Hello
+            Simple
+            String`;
+
+const ENTITY = {
+    original: ORIGINAL,
+};
+
+describe('<SimpleString>', () => {
+    it('renders original input as simple string', () => {
+        const wrapper = shallow(<SimpleString
+            entity = { ENTITY }
+        />);
+
+        expect(wrapper.find('.original ContentMarker').children()).toHaveLength(1);
+        expect(wrapper.find('.original ContentMarker').children().text()).toEqual('Hello\nSimple\nString');
+    });
+
+    it('calls the handleClickOnPlaceable function on click on .original', () => {
+        const handleClickOnPlaceable = sinon.spy();
+        const wrapper = shallow(<SimpleString
+            entity = { ENTITY }
+            handleClickOnPlaceable={ handleClickOnPlaceable }
+        />);
+
+        wrapper.find('.original').simulate('click');
+        expect(handleClickOnPlaceable.called).toEqual(true);
+    });
+});
