c0ae5b97f3f8e9d9f57a41a4566cc2bfef529c0b
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-10-25 08:01:43 +0100

    Fix bug 1423854: Adds assignee to the list of bugs (#1437)

--- pontoon/teams/static/js/bugzilla.js
+++ pontoon/teams/static/js/bugzilla.js
@@ -22,7 +22,7 @@ var Pontoon = (function (my) {
             'type0-0-1': 'regexp',
             'value0-0-1': '^' + locale + ' / ',
             'resolution': '---',
-            'include_fields': 'id,summary,creation_time,last_change_time'
+            'include_fields': 'id,summary,last_change_time,assigned_to'
           },
           success: function(data) {
             if (data.bugs.length) {
@@ -61,9 +61,10 @@ var Pontoon = (function (my) {
                 }).appendTo(tr);
 
                 $('<td>', {
-                  class: 'date-created',
-                  html: formatter.format(new Date(bug.creation_time))
+                  class: 'assigned-to',
+                  html: bug.assigned_to
                 }).appendTo(tr);
+
 
                 tbody.append(tr);
               });
@@ -75,7 +76,7 @@ var Pontoon = (function (my) {
                     '<th class="id">ID</th>' +
                     '<th class="summary">Summary</th>' +
                     '<th class="last-changed">Last Changed</th>' +
-                    '<th class="date-created">Date Created</th>' +
+                    '<th class="assigned-to">Assigned To</th>' +
                   '</tr>' +
                 '</thead>'
               }).append(tbody);
