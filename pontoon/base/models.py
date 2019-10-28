mozilla/pontoon

commit ff4f1d2158278b95b55f338fb0d0ae4e7a883a90

Fix bug 1589506:  Use high DPI avatars (#1438)
Also: Set width/height on img elements with user avatars.

pontoon/base/models.py
@@ -103,7 +103,7 @@ def user_gravatar_url(self, size):
     data = {'s': str(size)}
 
     if not settings.DEBUG:
-        append = '_big' if size > 44 else ''
+        append = '_big' if size > 88 else ''
         data['d'] = settings.SITE_URL + static('img/anon' + append + '.jpg')
 
     return '//www.gravatar.com/avatar/{email}?{data}'.format(
@@ -2640,7 +2640,7 @@ class TranslationQuerySet(models.QuerySet):
             {'email': user.email,
              'display_name': user.name_or_email,
              'id': user.id,
-             'gravatar_url': user.gravatar_url(44),
+             'gravatar_url': user.gravatar_url(88),
              'translation_count': user.translations_count,
              'role': user.user_role}
             for user in users_with_translations_counts(None, Q(id__in=self), limit=100)

pontoon/base/templates/django/base.html
@@ -56,7 +56,7 @@
               <div class="button selector">
                 {% if user.is_authenticated %}
                   <span class="title noselect">{{ user.name_or_email|nospam }}</span>
-                  <img class="rounded" src="{{ request.user.gravatar_url }}">
+                  <img class="rounded" src="{{ request.user.gravatar_url(88) }}" width="44" height="44">
                 {% else %}
                   <div class="menu-icon fa fa-bars"></div>
                 {% endif %}

frontend/src/modules/search/components/FiltersPanel.js
@@ -352,6 +352,8 @@ export class FiltersPanelBase extends React.Component<Props, State> {
                                             alt=""
                                             className="rounded"
                                             src={ author.gravatar_url }
+                                            width="44"
+                                            height="44"
                                         />
                                     </span>
                                     <figcaption>

pontoon/base/templates/widgets/latest_activity.html
@@ -5,7 +5,7 @@
       {% set action = latest_activity.type + ' by' %}
       {% set user = latest_activity.user.name_or_email %}
       {% set link = url('pontoon.contributors.contributor.username', latest_activity.user.username) %}
-      {% set avatar = latest_activity.user.gravatar_url(44) %}
+      {% set avatar = latest_activity.user.gravatar_url(88) %}
     {% else %}
       {% set action = 'imported' %}
       {% set user = '' %}

pontoon/base/templates/widgets/profile.html
@@ -1,7 +1,7 @@
 {% macro button() %}
 <div class="button selector">
   {% if user.is_authenticated() %}
-    <img class="rounded" src="{{ user.gravatar_url(44) }}">
+    <img class="rounded" src="{{ user.gravatar_url(88) }}" width="44" height="44">
   {% else %}
     <div class="menu-icon fa fa-bars"></div>
   {% endif %}
@@ -12,7 +12,7 @@
 {% if user.is_authenticated() %}
   <li class="details">
     <a href="{{ url('pontoon.contributors.contributor.username', username=user) }}">
-      <img class="rounded" src="{{ user.gravatar_url(88) }}">
+      <img class="rounded" src="{{ user.gravatar_url(176) }}" width="88" height="88">
       <p class="name">{{ user.name_or_email|nospam }}</p>
       <p class="email">{{ user.email|nospam }}</p>
     </a>

pontoon/base/views.py
@@ -385,7 +385,7 @@ def get_translation_history(request):
             "user": "Imported" if u is None else u.name_or_email,
             "uid": "" if u is None else u.id,
             "username": "" if u is None else u.username,
-            "user_gravatar_url_small": "" if u is None else u.gravatar_url(44),
+            "user_gravatar_url_small": "" if u is None else u.gravatar_url(88),
             "date": t.date.strftime('%b %d, %Y %H:%M'),
             "date_iso": t.date.isoformat(),
             "approved_user": User.display_name_or_blank(t.approved_user),
@@ -1020,8 +1020,8 @@ def user_data(request):
         'preferred_locales': user.profile.sorted_locales_codes,
         'tour_status': user.profile.tour_status,
         'logout_url': logout_url,
-        'gravatar_url_small': user.gravatar_url(44),
-        'gravatar_url_big': user.gravatar_url(88),
+        'gravatar_url_small': user.gravatar_url(88),
+        'gravatar_url_big': user.gravatar_url(176),
         'notifications': user.serialized_notifications,
     })
 

commit b562453caf96fd106bcc57f648981ee738729eb6

No bug: Fix Django login avatar regression
Introduced in:
https://github.com/mozilla/pontoon/commit/ff4f1d2158278b95b55f338fb0d0ae4e7a883a90#diff-91c36325f8496b8a88e5688322739387

pontoon/base/models.py
@@ -111,6 +111,11 @@ def user_gravatar_url(self, size):
 
 
 @property
+def user_gravatar_url_small(self):
+    return user_gravatar_url(self, 88)
+
+
+@property
 def user_name_or_email(self):
     return self.first_name or self.email
 
@@ -311,6 +316,7 @@ def serialized_notifications(self):
 
 User.add_to_class('profile_url', user_profile_url)
 User.add_to_class('gravatar_url', user_gravatar_url)
+User.add_to_class('gravatar_url_small', user_gravatar_url_small)
 User.add_to_class('name_or_email', user_name_or_email)
 User.add_to_class('display_name', user_display_name)
 User.add_to_class('display_name_and_email', user_display_name_and_email)

pontoon/base/templates/django/base.html
@@ -55,8 +55,7 @@
             <div id="profile" class="select">
               <div class="button selector">
                 {% if user.is_authenticated %}
-                  <span class="title noselect">{{ user.name_or_email|nospam }}</span>
-                  <img class="rounded" src="{{ request.user.gravatar_url(88) }}" width="44" height="44">
+                  <img class="rounded" src="{{ request.user.gravatar_url_small }}" width="44" height="44">
                 {% else %}
                   <div class="menu-icon fa fa-bars"></div>
                 {% endif %}