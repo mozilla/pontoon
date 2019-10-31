1c6ae717ca87ad25727faf99cde20bf21371139e
April Bowler <abowler2@gmail.com>
2019-10-31 09:58:05 -0700

    Fix bug 1374605: Implement group and resource comments (#1431)

--- pontoon/base/models.py
+++ pontoon/base/models.py
@@ -2244,6 +2244,8 @@ class Entity(DirtyFieldsMixin, models.Model):
     string_plural = models.TextField(blank=True)
     key = models.TextField(blank=True)
     comment = models.TextField(blank=True)
+    group_comment = models.TextField(blank=True)
+    resource_comment = models.TextField(blank=True)
     order = models.PositiveIntegerField(default=0)
     source = JSONField(blank=True, default=list)  # List of paths to source code files
     obsolete = models.BooleanField(default=False)
@@ -2530,6 +2532,8 @@ class Entity(DirtyFieldsMixin, models.Model):
                 Q(string__icontains=search)
                 | Q(string_plural__icontains=search)
                 | Q(comment__icontains=search)
+                | Q(group_comment__icontains=search)
+                | Q(resource_comment__icontains=search)
                 | Q(key__icontains=search)
                 for search in search_list)
 
@@ -2595,6 +2599,8 @@ class Entity(DirtyFieldsMixin, models.Model):
                 'project': entity.resource.project.serialize(),
                 'format': entity.resource.format,
                 'comment': entity.comment,
+                'group_comment': entity.group_comment,
+                'resource_comment': entity.resource_comment,
                 'order': entity.order,
                 'source': entity.source,
                 'obsolete': entity.obsolete,
