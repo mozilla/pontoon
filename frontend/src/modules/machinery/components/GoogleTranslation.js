8a3e4d83150b902f3a525c998aa50443b4020b18
eraulia nadinda <dinda.rachmat@gmail.com>
2019-11-04 17:37:58 +0100

    Fix bug 1588931 - Localize Machinery sources (#1441)
    This refactors the machinery api endpoint to return only relevant data, and adds a component per machinery translation source type that is used to display said source in the Machinery tab. Localization of that content is done in these components.

---
+++ frontend/src/modules/machinery/components/GoogleTranslation.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+import React from 'react';
+import { Localized } from '@fluent/react';
+
+
+/**
+ * Show the translation source from Google Translate.
+ */
+export default function GoogleTranslation() {
+    return <li>
+        <Localized
+            id= "machinery-GoogleTranslation--visit-google"
+            attrs={{ title: true }}
+        >
+            <a
+                className="translation-source"
+                href="https://translate.google.com/"
+                title="Visit Google Translate"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={ (e: SyntheticMouseEvent<>) => e.stopPropagation() }
+            >
+                <span>Google Translate</span>
+            </a>
+        </Localized>
+    </li>;
+}
---
+++ frontend/src/modules/machinery/components/TransvisionMemory.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import React from 'react';
+import { Localized } from '@fluent/react';
+
+import type { Locale } from 'core/locale';
+
+
+export type Props = {|
+    original: string,
+    locale: Locale,
+|};
+
+
+/**
+ * Show the translation source from Transvision.
+ */
+export default function TransvisionMemory(props: Props) {
+    return <li>
+        <Localized
+            id= "machinery-TransvisionMemory--visit-transvision"
+            attrs={{ title: true }}
+        >
+            <a
+                className="translation-source"
+                href={ "https://transvision.mozfr.org/?repo=global" +
+                        "&recherche=" + encodeURIComponent(props.original) +
+                        "&locale=" + props.locale.code }
+                title="Visit Transvision"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={ (e: SyntheticMouseEvent<>) => e.stopPropagation() }
+            >
+                <span>Mozilla</span>
+            </a>
+        </Localized>
+    </li>;
+}
