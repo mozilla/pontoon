ec7f42f94edc783c520f43416ad7d9b33a133622
sccofield <michael_umanah@yahoo.com>
2019-11-07 00:47:08 +0100

    Bug 1593237 Replace String list with spinner
    - remove circleloader files
    - add translation skeleton

--- frontend/src/core/loaders/components/SkeletonLoader.js
+++ frontend/src/core/loaders/components/SkeletonLoader.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import './SkeletonLoader.css';
 
-const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const list = [...Array(30).keys()];
 
 const SkeletonLoader = () => (
-    <div className="skeleton-loader entities unselectable">
+    <div className='skeleton-loader entities'>
         <ul>
             { list.map((i) => {
-                const classes = `entity missing ${i ===1 ? 'selected' : null}`
+                const classes = `entity missing ${i === 0 ? 'selected' : null}`
                 return <li className={ classes }>
                     <span className='status fa'/>
                     <div>
                         <p className='source-string'></p>
+                        <p className='translation-string'>meee</p>
                         <p className='text-2'></p>
                     </div>
                 </li>
