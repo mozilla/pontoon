4cbda8b2da931abf35ace1a5ba675a029216271e
sccofield <michael_umanah@yahoo.com>
2019-11-07 20:53:16 +0100

    Bug 1593237 modify skeleton for 3 column layout

--- frontend/src/core/loaders/components/SkeletonLoader.js
+++ frontend/src/core/loaders/components/SkeletonLoader.js
@@ -5,19 +5,17 @@ import './SkeletonLoader.css';
 const list = [...Array(30).keys()];
 
 export default function SkeletonLoader () {
-    return (
-        <ul className='skeleton-loader entities'>
-            { list.map((i) => {
-                const classes = `entity missing ${i === 0 ? 'selected' : null}`
-                return <li className={ classes }>
-                    <span className='status fa'/>
-                    <div>
-                        <p className='source-string'></p>
-                        <p className='translation-string'></p>
-                        <p className='text-2'></p>
-                    </div>
-                </li>
-            }) }
-        </ul>
-    )
+    return <ul className='skeleton-loader entities'>
+        { list.map((i) => {
+            const classes = `entity missing ${i === 0 ? 'selected' : null}`
+            return <li className={ classes } key={ i }>
+                <span className='status fa'/>
+                <div>
+                    <p className='source-string'></p>
+                    <p className='text-2'></p>
+                    <p className='translation-string'></p>
+                </div>
+            </li>
+        }) }
+    </ul>
 }
