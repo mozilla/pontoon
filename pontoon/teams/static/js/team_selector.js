13c9200fb83c34d68a4b2746379babca2613b838
abowler <abowler2@gmail.com>
2019-11-07 13:06:53 -0800

    Fixed selectors not individually changing

--- pontoon/teams/static/js/team_selector.js
+++ pontoon/teams/static/js/team_selector.js
@@ -1,5 +1,10 @@
 $(function() {
   $('.locale-selector .locale .menu li:not(".no-match")').click(function () {
-    $('.locale .selector').html($(this).html());
+    if ($(this).closest('#homepage').length == 1) {
+      $('#homepage .locale .selector').html($(this).html());
+    }
+    if ($(this).closest('#preferred-locale').length == 1) {
+      $('#preferred-locale .locale .selector').html($(this).html());
+    }
   });
 });
