mozilla/pontoon

commit 1c4c188b51b60f3fad5cb494ed4c70656ae41d7a

Fix bug 1588931 - Localize Machinery sources (#1441)
This refactors the machinery api endpoint to return only relevant data, and adds a component per machinery translation source type that is used to display said source in the Machinery tab. Localization of that content is done in these components.

frontend/src/modules/machinery/components/Count.js
@@ -17,7 +17,7 @@ export default class Count extends React.Component<Props> {
         const machineryCount = machinery.translations.length;
 
         const preferredCount = machinery.translations.reduce((count, item) => {
-            if (item.sources.find(source => source.type === 'Translation memory')) {
+            if (item.sources.find(source => source === 'Translation memory')) {
                 return count + 1;
             }
             return count;

frontend/src/modules/machinery/components/Count.test.js
@@ -8,8 +8,8 @@ describe('<Count>', () => {
     it('shows the correct number of preferred translations', () => {
         const machinery = {
             translations: [
-                { sources: [{ type: 'Translation memory' }] },
-                { sources: [{ type: 'Translation memory' }] },
+                { sources: ['Translation memory'] },
+                { sources: ['Translation memory'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);
@@ -26,9 +26,9 @@ describe('<Count>', () => {
     it('shows the correct number of remaining translations', () => {
         const machinery = {
             translations: [
-                { sources: [{ type: 'Microsoft' }] },
-                { sources: [{ type: 'Transvision' }] },
-                { sources: [{ type: 'Transvision' }] },
+                { sources: ['Microsoft'] },
+                { sources: ['Transvision'] },
+                { sources: ['Transvision'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);
@@ -46,11 +46,11 @@ describe('<Count>', () => {
     it('shows the correct numbers of preferred and remaining translations', () => {
         const machinery = {
             translations: [
-                { sources: [{ type: 'Translation memory' }] },
-                { sources: [{ type: 'Translation memory' }] },
-                { sources: [{ type: 'Microsoft' }] },
-                { sources: [{ type: 'Transvision' }] },
-                { sources: [{ type: 'Transvision' }] },
+                { sources: ['Translation memory'] },
+                { sources: ['Translation memory'] },
+                { sources: ['Microsoft'] },
+                { sources: ['Transvision'] },
+                { sources: ['Transvision'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);

frontend/public/static/locale/en-US/translate.ftl
@@ -358,19 +358,55 @@ interactivetour-InteractiveTour--end-footer =
 
 ## Machinery
 ## Shows a list of translations from machines.
-
 machinery-Machinery--search-placeholder =
     .placeholder = Search in Machinery
 
 
-## Machinery Translation
-## Shows a specific translation from machinery
+## Machinery Caighdean Translation
+## Shows the translation source from Caighdean Machine Translation.
+machinery-CaighdeanTranslation--visit-caighdean = Caighdean
+    .title = Visit Caighdean Machine Translation
 
+
+## Machinery Google Translation
+## Shows the translation source from Google Translate.
+machinery-GoogleTranslation--visit-google = Google Translate
+    .title = Visit Google Translate
+
+
+## Machinery Microsoft Terminology
+## Shows the translation source from Microsoft Terminology.
+machinery-MicrosoftTerminology--visit-microsoft = Microsoft
+    .title =
+        Visit Microsoft Terminology Service API.
+        © 2018 Microsoft Corporation. All rights reserved.
+
+
+## Machinery Microsoft Translation
+## Shows the translation source from Microsoft Translation.
+machinery-MicrosoftTranslation--visit-bing = Microsoft Translator
+    .title = Visit Bing Translate
+
+
+## Machinery Translation
+## Shows a specific translation from machinery.
 machinery-Translation--copy =
     .title = Copy Into Translation
 
-machinery-Translation--number-occurrences =
+
+## Machinery Translation Memory
+## Shows the translation source from Pontoon's memory.
+machinery-TranslationMemory--pontoon-homepage =
+    .title = Pontoon Homepage
+machinery-TranslationMemory--translation-memory = Translation memory
+machinery-TranslationMemory--number-occurrences =
     .title = Number of translation occurrences
+
+
+## Machinery Transvision Memory
+## Shows the translation source from Transvision.
+machinery-TransvisionMemory--visit-transvision = Mozilla
+    .title = Visit Transvision
 
 
 ## Notification Messages
@@ -403,7 +439,7 @@ notification--string-link-copied = Link copied to clipboard
 otherlocales-Translation--copy =
     .title = Copy Into Translation
 
-otherlocales-Translation--header-link = 
+otherlocales-Translation--header-link =
     .title = Open string in { $locale } ({ $code })
 
 

frontend/src/core/api/machinery.js
@@ -39,14 +39,10 @@ export default class MachineryAPI extends APIBase {
             return [];
         }
 
-        return results.map(item => {
+        return results.map((item): MachineryTranslation => {
             return {
-                sources: [{
-                    type: 'Translation memory',
-                    url: '/',
-                    title: 'Pontoon Homepage',
-                    count: item.count,
-                }],
+                sources: ['translation-memory'],
+                itemCount: item.count,
                 original: item.source,
                 translation: item.target,
                 quality: Math.round(item.quality),
@@ -71,11 +67,7 @@ export default class MachineryAPI extends APIBase {
         }
 
         return [{
-            sources: [{
-                type: 'Google Translate',
-                url: 'https://translate.google.com/',
-                title: 'Visit Google Translate',
-            }],
+            sources: ['google-translate'],
             original: source,
             translation: result.translation,
         }];
@@ -98,11 +90,7 @@ export default class MachineryAPI extends APIBase {
         }
 
         return [{
-            sources: [{
-                type: 'Microsoft Translator',
-                url: 'https://www.bing.com/translator',
-                title: 'Visit Bing Translator',
-            }],
+            sources: ['microsoft-translator'],
             original: source,
             translation: result.translation,
         }];
@@ -124,15 +112,9 @@ export default class MachineryAPI extends APIBase {
             return [];
         }
 
-        return results.translations.map(item => {
+        return results.translations.map((item): MachineryTranslation => {
             return {
-                sources: [{
-                    type: 'Microsoft',
-                    url: 'https://www.microsoft.com/Language/en-US/Search.aspx?sString=' +
-                        item.source + '&langID=' + locale.msTerminologyCode,
-                    title: 'Visit Microsoft Terminology Service API.\n' +
-                        '© 2018 Microsoft Corporation. All rights reserved.',
-                }],
+                sources: ['microsoft-terminology'],
                 original: item.source,
                 translation: item.target,
                 quality: Math.round(item.quality),
@@ -152,15 +134,9 @@ export default class MachineryAPI extends APIBase {
 
         const results = await this._get(url, params);
 
-        return results.map(item => {
+        return results.map((item): MachineryTranslation => {
             return {
-                sources: [{
-                    type: 'Mozilla',
-                    url: 'https://transvision.mozfr.org/?repo=global' +
-                        '&recherche=' + encodeURIComponent(source) +
-                        '&locale=' + locale.code,
-                    title: 'Visit Transvision',
-                }],
+                sources: ['transvision'],
                 original: item.source,
                 translation: item.target,
                 quality: Math.round(item.quality),
@@ -187,11 +163,7 @@ export default class MachineryAPI extends APIBase {
         }
 
         return [{
-            sources: [{
-                type: 'Caighdean',
-                url: 'https://github.com/kscanne/caighdean',
-                title: 'Visit Caighdean Machine Translation',
-            }],
+            sources: ['caighdean'],
             original: result.original,
             translation: result.translation,
         }];

frontend/src/core/api/types.js
@@ -57,13 +57,18 @@ export type OtherLocaleTranslation = {|
  * Translation that comes from a machine (Machine Translation,
  * Translation Memory... ).
  */
+type SourceType =
+    | 'translation-memory'
+    | 'google-translate'
+    | 'microsoft-translator'
+    | 'microsoft-terminology'
+    | 'transvision'
+    | 'caighdean'
+;
+
 export type MachineryTranslation = {|
-    sources: Array<{|
-        type: string,
-        url: string,
-        title: string,
-        count?: number,
-    |}>,
+    sources: Array<SourceType>,
+    itemCount?: number,
     original: string,
     translation: string,
     quality?: number,

frontend/src/modules/machinery/components/CaighdeanTranslation.js
@@ -1,0 +1,28 @@
+/* @flow */
+
+import React from 'react';
+import { Localized } from '@fluent/react';
+
+
+/**
+ * Show the translation source from Caighdean Machine Translation.
+ */
+export default function CaighdeanTranslation() {
+    return <li>
+        <Localized
+            id= "machinery-CaighdeanTranslation--visit-caighdean"
+            attrs={{ title: true }}
+        >
+            <a
+                className="translation-source"
+                href="https://github.com/kscanne/caighdean"
+                title="Visit Caighdean Machine Translation"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={ (e: SyntheticMouseEvent<>) => e.stopPropagation() }
+            >
+                <span>Caighdean</span>
+            </a>
+        </Localized>
+    </li>;
+}

frontend/src/modules/machinery/components/CaighdeanTranslation.test.js
@@ -1,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CaighdeanTranslation from './CaighdeanTranslation';
+
+
+describe('<CaighdeanTranslation>', () => {
+    it('renders the CaighdeanTranslation component properly', () => {
+        const wrapper = shallow(<CaighdeanTranslation/>);
+
+        expect(wrapper.find('li')).toHaveLength(1);
+        expect(wrapper.find('Localized').props().id).toEqual('machinery-CaighdeanTranslation--visit-caighdean');
+        expect(wrapper.find('li a').props().href).toEqual('https://github.com/kscanne/caighdean');
+        expect(wrapper.find('li a').props().title).toEqual('Visit Caighdean Machine Translation');
+        expect(wrapper.find('li a span').text()).toEqual('Caighdean');
+    });
+});

frontend/src/modules/machinery/components/GoogleTranslation.js
@@ -1,0 +1,28 @@
+/* @flow */
+
+import React from 'react';
+import { Localized } from '@fluent/react';
+
+
+/**
+ * Show the translation source from Google Translate.
+ */
+export default function GoogleTranslation() {
+    return <li>
+        <Localized
+            id= "machinery-GoogleTranslation--visit-google"
+            attrs={{ title: true }}
+        >
+            <a
+                className="translation-source"
+                href="https://translate.google.com/"
+                title="Visit Google Translate"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={ (e: SyntheticMouseEvent<>) => e.stopPropagation() }
+            >
+                <span>Google Translate</span>
+            </a>
+        </Localized>
+    </li>;
+}

commit 3bfed69499a3022b4b23d56eb1feaffd63b78af2

Fix bug 1594177: Visually indicate TM matches in Machinery count (#1482)
Fixes a regression introduced in 1c4c188.

frontend/src/modules/machinery/components/Count.js
@@ -17,7 +17,7 @@ export default class Count extends React.Component<Props> {
         const machineryCount = machinery.translations.length;
 
         const preferredCount = machinery.translations.reduce((count, item) => {
-            if (item.sources.find(source => source === 'Translation memory')) {
+            if (item.sources.find(source => source === 'translation-memory')) {
                 return count + 1;
             }
             return count;

frontend/src/modules/machinery/components/Count.test.js
@@ -8,8 +8,8 @@ describe('<Count>', () => {
     it('shows the correct number of preferred translations', () => {
         const machinery = {
             translations: [
-                { sources: ['Translation memory'] },
-                { sources: ['Translation memory'] },
+                { sources: ['translation-memory'] },
+                { sources: ['translation-memory'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);
@@ -26,9 +26,9 @@ describe('<Count>', () => {
     it('shows the correct number of remaining translations', () => {
         const machinery = {
             translations: [
-                { sources: ['Microsoft'] },
-                { sources: ['Transvision'] },
-                { sources: ['Transvision'] },
+                { sources: ['microsoft'] },
+                { sources: ['transvision'] },
+                { sources: ['transvision'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);
@@ -46,11 +46,11 @@ describe('<Count>', () => {
     it('shows the correct numbers of preferred and remaining translations', () => {
         const machinery = {
             translations: [
-                { sources: ['Translation memory'] },
-                { sources: ['Translation memory'] },
-                { sources: ['Microsoft'] },
-                { sources: ['Transvision'] },
-                { sources: ['Transvision'] },
+                { sources: ['translation-memory'] },
+                { sources: ['translation-memory'] },
+                { sources: ['microsoft'] },
+                { sources: ['transvision'] },
+                { sources: ['transvision'] },
             ],
         };
         const wrapper = shallow(<Count machinery={ machinery } />);