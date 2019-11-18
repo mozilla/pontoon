077c61a853dfbf5b83908a0ae4688ba5d680ca3c
Anand Thakker <github@anandthakker.net>
2019-11-18 10:14:19 -0500

    Move google auth settings per PR review

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -817,6 +817,10 @@ FXA_SCOPE = ['profile:uid', 'profile:display_name', 'profile:email']
 GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID', '')
 GITHUB_SECRET_KEY = os.environ.get('GITHUB_SECRET_KEY', '')
 
+# Google Accounts
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
+GOOGLE_SECRET_KEY = os.environ.get('GOOGLE_SECRET_KEY', '')
+
 # All settings related to the AllAuth
 SOCIALACCOUNT_PROVIDERS = {
     'fxa': {
@@ -825,10 +829,6 @@ SOCIALACCOUNT_PROVIDERS = {
         'PROFILE_ENDPOINT': FXA_PROFILE_ENDPOINT,
     }
 }
-
-# Google Accounts
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
-GOOGLE_SECRET_KEY = os.environ.get('GOOGLE_SECRET_KEY', '')
 
 # Defined all trusted origins that will be returned in pontoon.js file.
 if os.environ.get('JS_TRUSTED_ORIGINS'):
