40ecb7c13d8d556ae7a009eb3f649538a3a69d1a
Anand Thakker <anandthakker@users.noreply.github.com>
2019-11-18 10:53:00 -0500

    Fix bug 1597284: Add support for authenticating with Google (#1491)

--- pontoon/base/management/commands/update_auth_providers.py
+++ pontoon/base/management/commands/update_auth_providers.py
@@ -11,18 +11,12 @@ from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialApp
 from allauth.socialaccount.providers.fxa.provider import FirefoxAccountsProvider
 from allauth.socialaccount.providers.github.provider import GitHubProvider
-<<<<<<< HEAD
 from allauth.socialaccount.providers.google.provider import GoogleProvider
-=======
->>>>>>> Fix bug 1558484 - Add ability to log users in using GitHub (#1442)
 
 
 FXA_PROVIDER_ID = FirefoxAccountsProvider.id
 GITHUB_PROVIDER_ID = GitHubProvider.id
-<<<<<<< HEAD
 GOOGLE_PROVIDER_ID = GoogleProvider.id
-=======
->>>>>>> Fix bug 1558484 - Add ability to log users in using GitHub (#1442)
 
 
 class Command(BaseCommand):
@@ -72,6 +66,9 @@ class Command(BaseCommand):
 
             self.update_provider(github_data)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Fix bug 1597284: Add support for authenticating with Google (#1491)
 
         if settings.GOOGLE_CLIENT_ID is not None or settings.GOOGLE_SECRET_KEY is not None:
             google_data = dict(
@@ -82,5 +79,8 @@ class Command(BaseCommand):
             )
 
             self.update_provider(google_data)
+<<<<<<< HEAD
 =======
 >>>>>>> Fix bug 1558484 - Add ability to log users in using GitHub (#1442)
+=======
+>>>>>>> Fix bug 1597284: Add support for authenticating with Google (#1491)
