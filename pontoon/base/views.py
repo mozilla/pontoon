9152fbbce6bf2f9aabb82699b5cfac49eef9ef3c
abowler <abowler2@gmail.com>
2019-12-02 20:57:37 -0800

    Removed preferred translations from other_locales and concatenated lists on FE

--- pontoon/base/views.py
+++ pontoon/base/views.py
@@ -354,6 +354,10 @@ def get_translations_from_other_locales(request):
 
     other_translations = _serialize_translation_values(other)
 
+    preferred_locale_codes = [locale['locale__code'] for locale in preferred_locales]
+    for code in preferred_locale_codes:
+        other_locales = [l for l in other_locales if not (l['locale__code'] == code)]
+
     payload = {
         'preferred': preferred_translations,
         'other': other_translations,
