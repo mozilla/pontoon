1085962850bf4280e17958c81d8d4978ca52d825
Catherine Chepkurui <kathiekim95@gmail.com>
2019-12-09 16:41:28 +0300

    Fix bug 1395179 - Status numbers in progress menu should be clickable (#1498)
    This turns the numbers in the Resource progress panel into links to the same page but filtered by the associated status.

--- frontend/src/modules/resourceprogress/components/ResourceProgress.test.js
+++ frontend/src/modules/resourceprogress/components/ResourceProgress.test.js
@@ -14,16 +14,24 @@ describe('<ResourceProgress>', () => {
         missing: 1,
         total: 15,
     };
+    const PARAMETERS = {
+        entity: 0,
+        locale: "en-GB",
+        project: "tutorial",
+        resource: "all-resources",
+        status: "errors",
+        search: null
+    };
 
     it('shows only a selector by default', () => {
-        const wrapper = shallow(<ResourceProgressBase stats={ STATS } />);
+        const wrapper = shallow(<ResourceProgressBase stats={ STATS } parameters={ PARAMETERS } />);
 
         expect(wrapper.find('.selector').exists()).toBeTruthy();
         expect(wrapper.find('.menu').exists()).toBeFalsy();
     });
 
     it('shows the info menu after a click', () => {
-        const wrapper = shallow(<ResourceProgressBase stats={ STATS } />);
+        const wrapper = shallow(<ResourceProgressBase stats={ STATS } parameters={ PARAMETERS } />);
         wrapper.find('.selector').simulate('click');
 
         expect(wrapper.find('.menu').exists()).toBeTruthy();
