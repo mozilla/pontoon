7427ab00ae57d9d82f481e03ebd1c4aed8d34a8e
Fabien LOISON <flozz@users.noreply.github.com>
2019-12-11 16:17:07 +0100

    Fix bug 1602732: GitLab authentication method (#1516)
    Also: Avoid creating non-configured providers when running the update_auth_providers command.

--- pontoon/base/management/commands/update_auth_providers.py
+++ pontoon/base/management/commands/update_auth_providers.py
@@ -11,11 +11,13 @@ from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialApp
 from allauth.socialaccount.providers.fxa.provider import FirefoxAccountsProvider
 from allauth.socialaccount.providers.github.provider import GitHubProvider
+from allauth.socialaccount.providers.gitlab.provider import GitLabProvider
 from allauth.socialaccount.providers.google.provider import GoogleProvider
 
 
 FXA_PROVIDER_ID = FirefoxAccountsProvider.id
 GITHUB_PROVIDER_ID = GitHubProvider.id
+GITLAB_PROVIDER_ID = GitLabProvider.id
 GOOGLE_PROVIDER_ID = GoogleProvider.id
 
 
@@ -45,7 +47,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Check if FXA_* settings are configured
-        if settings.FXA_CLIENT_ID is not None or settings.FXA_SECRET_KEY is not None:
+        if settings.FXA_CLIENT_ID is not None and settings.FXA_SECRET_KEY is not None:
             fxa_data = dict(
                 name='FxA',
                 provider=FXA_PROVIDER_ID,
@@ -55,8 +57,8 @@ class Command(BaseCommand):
 
             self.update_provider(fxa_data)
 
-        # Check if GitHub_* settings are configured
-        if settings.GITHUB_CLIENT_ID is not None or settings.GITHUB_SECRET_KEY is not None:
+        # Check if GITHUB_* settings are configured
+        if settings.GITHUB_CLIENT_ID is not None and settings.GITHUB_SECRET_KEY is not None:
             github_data = dict(
                 name='GitHub',
                 provider=GITHUB_PROVIDER_ID,
@@ -66,7 +68,19 @@ class Command(BaseCommand):
 
             self.update_provider(github_data)
 
-        if settings.GOOGLE_CLIENT_ID is not None or settings.GOOGLE_SECRET_KEY is not None:
+        # Check if GITLAB_* settings are configured
+        if settings.GITLAB_CLIENT_ID is not None and settings.GITLAB_SECRET_KEY is not None:
+            gitlab_data = dict(
+                name='GitLab',
+                provider=GITLAB_PROVIDER_ID,
+                client_id=settings.GITLAB_CLIENT_ID,
+                secret=settings.GITLAB_SECRET_KEY
+            )
+
+            self.update_provider(gitlab_data)
+
+        # Check if GOOGLE_* settings are configured
+        if settings.GOOGLE_CLIENT_ID is not None and settings.GOOGLE_SECRET_KEY is not None:
             google_data = dict(
                 name='Google',
                 provider=GOOGLE_PROVIDER_ID,
