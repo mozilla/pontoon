fb233f4ec132061678861c610345af3a86f3175b
Matjaž Horvat <matjaz.horvat@gmail.com>
2019-12-18 16:51:48 +0100

    Fix bug 1604073: Properly handle approvals in tag latest activity

--- pontoon/tags/utils/translations.py
+++ pontoon/tags/utils/translations.py
@@ -13,7 +13,13 @@ class TagsLatestTranslationsTool(TagsTRTool):
     filter_methods = ('tag', 'projects', 'latest', 'locales', 'path')
 
     _default_annotations = (
-        ('last_change', Max('latest_translation__date')), )
+        (
+            'date', Max('latest_translation__date')
+        ),
+        (
+            'approved_date', Max('latest_translation__approved_date')
+        ),
+    )
 
     @property
     def groupby_prefix(self):
@@ -35,16 +41,31 @@ class TagsLatestTranslationsTool(TagsTRTool):
     def get_data(self):
         _translations = self.translation_manager.none()
         stats = super(TagsLatestTranslationsTool, self).get_data()
+
         for tr in stats.iterator():
+            if tr['approved_date'] is not None and tr['approved_date'] > tr['date']:
+                key = 'approved_date'
+            else:
+                key = 'date'
+
             # find translations with matching date and tag/locale
             _translations |= self.translation_manager.filter(
-                Q(**{'date': tr["last_change"],
-                     self.groupby_prefix: tr[self.get_groupby()[0]]}))
+                Q(
+                    **{
+                        key: tr[key],
+                        self.groupby_prefix: tr[self.get_groupby()[0]]
+                    }
+                )
+            )
+
         return _translations.values(
             *(
                 'string',
                 'date',
                 'approved_date',
+                'approved_user__email',
+                'approved_user__first_name',
+                'approved_user__username',
                 'user__email',
                 'user__first_name',
                 'user__username',
