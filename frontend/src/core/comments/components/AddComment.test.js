0260b78ff6e3d63784fa7b951983e912d4f151c8
abowler <abowler2@gmail.com>
2020-01-17 10:16:02 -0800

    Added new test for AddComment

--- frontend/src/core/comments/components/AddComment.test.js
+++ frontend/src/core/comments/components/AddComment.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import sinon from 'sinon';
 
 import AddComment from './AddComment';
 
+
+const DEFAULT_USER = {
+    user: 'RSwanson',
+    username: 'Ron_Swanson',
+    imageURL: '',
+}
+
 describe('<AddComment>', () => {
     it('calls submitComment function', () => {
         const submitCommentFn = sinon.spy();
-        const wrapper = shallow(<AddComment
+        const wrapper = mount(<AddComment
+            { ...DEFAULT_USER }
             submitComment={ submitCommentFn }
         />);
 
-        wrapper.prop('onSubmit') === submitCommentFn;
+        const event = {
+            preventDefault: sinon.spy(),
+        };
+
+        wrapper.find('form').simulate('submit', event);
+        expect(submitCommentFn.calledOnce).toBeTruthy;
     });
 });
