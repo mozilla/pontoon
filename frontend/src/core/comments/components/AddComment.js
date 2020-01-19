6d5ad275952ed4cc25cff94564ddfbad6725e19a
abowler <abowler2@gmail.com>
2020-01-19 13:17:24 -0800

    Changed input to textarea for AddComment

--- frontend/src/core/comments/components/AddComment.js
+++ frontend/src/core/comments/components/AddComment.js
@@ -31,6 +31,13 @@ export default function AddComments(props: Props) {
         return null;
     }
 
+    const onEnterSubmit = (event: SyntheticKeyboardEvent<HTMLTextAreaElement>) => {
+        if(event.keyCode === 13 && event.shiftKey === false) {
+            event.preventDefault();
+            submitComment(event);
+          }
+    }
+
     const submitComment = (event: SyntheticKeyboardEvent<>) => {
         event.preventDefault();
         const comment = commentInput.current.value;
@@ -50,18 +57,18 @@ export default function AddComments(props: Props) {
             title=''
             imageUrl={ imageURL }
         />
-        <form className='container' onSubmit={ submitComment }>
+        <form className='container'>
             <Localized
                 id='comments-AddComment--input'
                 attrs={{ placeholder: true }}
             >
-                <input
-                    type='text'
+                <textarea
                     id='comment-input'
                     name='comment'
                     dir='auto'
                     placeholder={ `Write a comment ${'\u2026'}` }
                     ref={ commentInput }
+                    onKeyDown={ onEnterSubmit }
                 />
             </Localized>
         </form>
