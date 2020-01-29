aa52a958922facdd86833798226ade244e34a6fa
Jarosław Śmiejczak <jot@evilb.it>
2020-01-29 16:11:38 +0100

    Fix bug 1603380 - Improve perfomance of combined status filters for smaller projects.
    The SQL query responsible for status filters is composed from many subqueries that are executed in the WHERE clause.
    Unfortunately, some of those sub-queries were too broad e.g. they were looking for strings for a whole locale without filtering them by the current project.
    
    This noticably improves the performance of multiple status filters for smaller projects (e.g. Firefox Monitor Website).
    It's still relatively slow on bigger ones, e.g. Firefox (it takes around 15s to get the list of strings when you use combined status filter).

--- pontoon/base/tests/managers/test_entity.py
+++ pontoon/base/tests/managers/test_entity.py
@@ -113,7 +113,7 @@ def test_mgr_entity_filter_translated(resource_a, locale_a):
     TranslationFactory.create(
         locale=locale_a, entity=entities[2], approved=True,
     )
-    assert set(Entity.objects.filter(Entity.objects.translated(locale_a))) == {
+    assert set(Entity.objects.filter(Entity.objects.translated(locale_a, resource_a.project))) == {
         entities[0],
         entities[2],
     }
@@ -146,7 +146,7 @@ def test_mgr_entity_filter_translated_plurals(resource_a, locale_a):
     TranslationFactory.create(
         locale=locale_a, entity=entities[2], plural_form=1, approved=True,
     )
-    assert set(Entity.objects.filter(Entity.objects.translated(locale_a))) == {
+    assert set(Entity.objects.filter(Entity.objects.translated(locale_a, resource_a.project))) == {
         entities[0],
         entities[2],
     }
