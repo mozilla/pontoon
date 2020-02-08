c66c4eca7b6f565a5d08c852a98ab96933039abf
abowler2 <abowler2@gmail.com>
2020-02-08 11:52:04 -0800

    Added comment count

--- frontend/src/core/comments/index.js
+++ frontend/src/core/comments/index.js
@@ -5,6 +5,7 @@ export { default as reducer } from './reducer';
 
 export { default as AddComment } from './components/AddComment';
 export { default as Comment } from './components/Comment';
+export { default as CommentCount } from './components/CommentCount';
 export { default as CommentsList } from './components/CommentsList';
 export { default as TeamComment } from './components/TeamComment';
 
