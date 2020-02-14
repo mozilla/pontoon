4fc2d583da463944233641d1b5c793484c02019e
abowler2 <abowler2@gmail.com>
2020-02-13 17:06:33 -0800

    Merged CommentsList and TeamComments

--- frontend/src/modules/commentslist/index.js
+++ frontend/src/modules/commentslist/index.js
@@ -3,10 +3,10 @@ export { default as reducer } from './reducer';
 
 
 export { default as CommentCount } from './components/CommentCount';
-export { default as TeamComment } from './components/TeamComment';
+export { default as CommentsList } from './components/CommentsList';
 
 export type { TeamCommentState } from './reducer';
 
 // Name of this module.
 // Used as the key to store this module's reducer.
-export const NAME: string = 'teamcomments';
+export const NAME: string = 'commentslist';
