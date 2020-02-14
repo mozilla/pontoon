a3ee70191132904607832d50f69bf38f9deec7c9
abowler2 <abowler2@gmail.com>
2020-02-14 11:56:07 -0800

    Unmerged CommentsList and TeamComments and refactored Comment save

--- frontend/src/modules/teamcomments/components/TeamComment.test.js
+++ frontend/src/modules/teamcomments/components/TeamComment.test.js
@@ -6,24 +6,6 @@ import TeamComment from './TeamComment';
 describe('<TeamComment>', () => {
     const DEFAULT_USER = 'AndyDwyer';
 
-    it('shows the correct number of comments', () => {
-        const teamComments = {
-            entity: 267,
-            comments: [
-                { id: 1 },
-                { id: 2 },
-                { id: 3 },
-            ],
-        };
-
-        const wrapper = shallow(<TeamComment
-            teamComments={ teamComments }
-            user={ DEFAULT_USER }
-        />);
-
-        expect(wrapper.find('ul > *')).toHaveLength(3);
-    });
-
     it('shows correct message when no comments', () => {
         const teamComments = {
             entity: 267,
