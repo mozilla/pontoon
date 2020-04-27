c4507546b71fc30461546d966bfd85fd1d393561
Jarosław Śmiejczak <poke@jotes.work>
2020-04-27 10:49:15 +0200

    fix black formatting

--- pontoon/projects/views.py
+++ pontoon/projects/views.py
@@ -24,7 +24,8 @@ from pontoon.tags.utils import TagsTool
 def projects(request):
     """List all active projects."""
     projects = (
-        Project.objects.visible().visible_for(request.user)
+        Project.objects.visible()
+        .visible_for(request.user)
         .prefetch_related("latest_translation__user")
         .order_by("name")
     )
