a21e96641847096c1c1aa0d5a746c761118dfbb8
Jarosław Śmiejczak <poke@jotes.work>
2020-05-19 21:09:04 +0200

    flake8

--- pontoon/projects/tests/test_views.py
+++ pontoon/projects/tests/test_views.py
@@ -17,7 +17,6 @@ from pontoon.base.tests import (
 )
 
 from pontoon.base.tests.test_views import ViewTestCase
-from pontoon.projects import views
 
 
 class ProjectTests(ViewTestCase):
@@ -31,7 +30,7 @@ class ProjectTests(ViewTestCase):
         """
         Checks if project page is returned properly.
         """
-        project = ProjectFactory.create(visibility='public')
+        project = ProjectFactory.create(visibility="public")
         ResourceFactory.create(project=project)
 
         with patch("pontoon.projects.views.render", wraps=render) as mock_render:
@@ -52,23 +51,23 @@ class ProjectContributorsTests(ViewTestCase):
         """
         Tests if view returns top contributors specific for given project.
         """
-        first_project = ProjectFactory.create(visibility='public')
+        first_project = ProjectFactory.create(visibility="public")
         ResourceFactory.create(project=first_project)
         first_project_contributor = TranslationFactory.create(
             entity__resource__project=first_project
         ).user
 
-        second_project = ProjectFactory.create(visibility='public')
+        second_project = ProjectFactory.create(visibility="public")
         ResourceFactory.create(project=second_project)
         second_project_contributor = TranslationFactory.create(
             entity__resource__project=second_project
         ).user
 
         with patch(
-            'pontoon.projects.views.ProjectContributorsView.render_to_response',
+            "pontoon.projects.views.ProjectContributorsView.render_to_response",
             return_value=HttpResponse(""),
         ) as mock_render:
-            response = self.client.get(
+            self.client.get(
                 "/projects/{}/ajax/contributors/".format(first_project.slug),
                 HTTP_X_REQUESTED_WITH="XMLHttpRequest",
             )
