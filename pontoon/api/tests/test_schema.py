f8c84a3bee18c621ec7ee9b1aebe73fe01caca88
Jarek <poke@jotes.work>
2020-10-11 19:22:39 +0200

    Bug 1417530 - Convert the api tests from Unittest to Pytest (#1701)

--- pontoon/api/tests/test_schema.py
+++ pontoon/api/tests/test_schema.py
@@ -2,147 +2,154 @@ from __future__ import absolute_import
 
 import sys
 
-from django.test import TestCase
+import pytest
 
 
-class TestCyclicQueries(TestCase):
-    def setUp(self):
-        # graphql-core's ExecutionContext.report_error uses sys.excepthook to
-        # print error stack traces. According to Python docs this hooks can be
-        # safely customized:
-        #
-        #     The handling of such top-level exceptions can be customized by
-        #     assigning another three-argument function to sys.excepthook.
-        #
-        # Cf. https://docs.python.org/2/library/sys.html#sys.excepthook
-        self.excepthook_orig = sys.excepthook
-        sys.excepthook = lambda *x: None
+@pytest.fixture
+def setup_excepthook():
+    # graphql-core's ExecutionContext.report_error uses sys.excepthook to
+    # print error stack traces. According to Python docs this hooks can be
+    # safely customized:
+    #
+    #     The handling of such top-level exceptions can be customized by
+    #     assigning another three-argument function to sys.excepthook.
+    #
+    # Cf. https://docs.python.org/2/library/sys.html#sys.excepthook
+    excepthook_orig = sys.excepthook
+    sys.excepthook = lambda *x: None
+    yield
+    sys.excepthook = excepthook_orig
 
-    def tearDown(self):
-        sys.excepthook = self.excepthook_orig
 
-    def test_projects(self):
-        body = {
-            "query": """{
-                projects(includeSystem: true) {
-                    name
-                }
-            }"""
+@pytest.mark.django_db
+def test_projects(client):
+    body = {
+        "query": """{
+            projects(includeSystem: true) {
+                name
+            }
+        }"""
+    }
+
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "data": {
+            "projects": [
+                {"name": "Pontoon Intro"},
+                {"name": "Terminology"},
+                {"name": "Tutorial"},
+            ]
         }
+    }
 
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(
-            response.content,
-            {
-                "data": {
-                    "projects": [
-                        {"name": "Pontoon Intro"},
-                        {"name": "Terminology"},
-                        {"name": "Tutorial"},
-                    ]
+@pytest.mark.django_db
+def test_project_localizations(client):
+    body = {
+        "query": """{
+            project(slug: "pontoon-intro") {
+                localizations {
+                    locale {
+                        name
+                    }
                 }
-            },
-        )
+            }
+        }"""
+    }
 
-    def test_project_localizations(self):
-        body = {
-            "query": """{
-                project(slug: "pontoon-intro") {
-                    localizations {
-                        locale {
-                            name
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "data": {"project": {"localizations": [{"locale": {"name": "English"}}]}}
+    }
+
+
+@pytest.mark.django_db
+def test_projects_localizations_cyclic(client):
+    body = {
+        "query": """{
+            projects {
+                localizations {
+                    locale {
+                        localizations {
+                            totalStrings
                         }
                     }
                 }
-            }"""
-        }
+            }
+        }"""
+    }
 
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(
-            response.content,
-            {"data": {"project": {"localizations": [{"locale": {"name": "English"}}]}}},
-        )
+    assert response.status_code == 200
+    assert b"Cyclic queries are forbidden" in response.content
 
-    def test_projects_localizations_cyclic(self):
-        body = {
-            "query": """{
-                projects {
-                    localizations {
-                        locale {
-                            localizations {
-                                totalStrings
-                            }
+
+@pytest.mark.django_db
+def test_project_localizations_cyclic(client):
+    body = {
+        "query": """{
+            project(slug: "pontoon-intro") {
+                localizations {
+                    locale {
+                        localizations {
+                            totalStrings
                         }
                     }
                 }
-            }"""
-        }
+            }
+        }"""
+    }
 
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Cyclic queries are forbidden")
+    assert response.status_code == 200
+    assert b"Cyclic queries are forbidden" in response.content
 
-    def test_project_localizations_cyclic(self):
-        body = {
-            "query": """{
-                project(slug: "pontoon-intro") {
-                    localizations {
-                        locale {
-                            localizations {
-                                totalStrings
-                            }
+
+@pytest.mark.django_db
+def test_locales_localizations_cyclic(client):
+    body = {
+        "query": """{
+            locales {
+                localizations {
+                    project {
+                        localizations {
+                            totalStrings
                         }
                     }
                 }
-            }"""
-        }
+            }
+        }"""
+    }
 
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Cyclic queries are forbidden")
+    assert response.status_code == 200
+    assert b"Cyclic queries are forbidden" in response.content
 
-    def test_locales_localizations_cyclic(self):
-        body = {
-            "query": """{
-                locales {
-                    localizations {
-                        project {
-                            localizations {
-                                totalStrings
-                            }
+
+@pytest.mark.django_db
+def test_locale_localizations_cyclic(client):
+    body = {
+        "query": """{
+            locale(code: "en-US") {
+                localizations {
+                    project {
+                        localizations {
+                            totalStrings
                         }
                     }
                 }
-            }"""
-        }
+            }
+        }"""
+    }
 
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
+    response = client.get("/graphql", body, HTTP_ACCEPT="application/json")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Cyclic queries are forbidden")
-
-    def test_locale_localizations_cyclic(self):
-        body = {
-            "query": """{
-                locale(code: "en-US") {
-                    localizations {
-                        project {
-                            localizations {
-                                totalStrings
-                            }
-                        }
-                    }
-                }
-            }"""
-        }
-
-        response = self.client.get("/graphql", body, HTTP_ACCEPT="application/json")
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Cyclic queries are forbidden")
+    assert response.status_code == 200
+    assert b"Cyclic queries are forbidden" in response.content
