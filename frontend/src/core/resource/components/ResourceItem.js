30a0f6e5341ea8004e990a5d36593409414de0a4
Matjaž Horvat <matjaz.horvat@gmail.com>
2020-12-30 21:37:49 +0100

    Fix bug 1684086: Prevent resource path overflow in resource menu (#1791)

--- frontend/src/core/resource/components/ResourceItem.js
+++ frontend/src/core/resource/components/ResourceItem.js
@@ -28,7 +28,9 @@ export default function ResourceItem(props: Props) {
                 href={`/${parameters.locale}/${parameters.project}/${resource.path}/`}
                 onClick={navigateToPath}
             >
-                <span>{resource.path}</span>
+                <span className='path' title={resource.path}>
+                    {resource.path}
+                </span>
                 <ResourcePercent resource={resource} />
             </a>
         </li>
