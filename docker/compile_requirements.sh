b80d7bc392ff2b385ac4388e487562f36e8b3501
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-01-08 22:02:29 +0100

    Fix bug 1685799: Compile python requirements in docker (#1806)

--- docker/compile_requirements.sh
+++ docker/compile_requirements.sh
@@ -3,7 +3,7 @@
 # This compiles all requirements files with pip-compile.
 # You should always use this script, because dev.txt and test.txt depend on default.txt.
 
-export CUSTOM_COMPILE_COMMAND="./requirements/compile_all.sh"
+export CUSTOM_COMPILE_COMMAND="./docker/compile_requirements.sh"
 
 pip-compile --generate-hashes requirements/default.in
 pip-compile --generate-hashes requirements/dev.in
