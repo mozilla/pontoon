06d17941dd5eb73792d02d24bbadb190c65deead
Adrian Gaudebert <adrian@gaudebert.fr>
2021-01-26 12:06:17 +0100

    Fix bug 1681688 - Correctly update generic editor content when the editor is reset.

--- frontend/src/modules/genericeditor/components/GenericEditor.js
+++ frontend/src/modules/genericeditor/components/GenericEditor.js
@@ -1,13 +1,50 @@
 /* @flow */
 
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import * as editor from 'core/editor';
 import * as entities from 'core/entities';
 import * as plural from 'core/plural';
 
 import GenericTranslationForm from './GenericTranslationForm';
+
+/**
+ * Hook to update the editor content whenever the entity changes.
+ */
+function useLoadTranslation() {
+    const dispatch = useDispatch();
+
+    const updateTranslation = editor.useUpdateTranslation();
+
+    const changeSource = useSelector((state) => state.editor.changeSource);
+    const entity = useSelector((state) =>
+        entities.selectors.getSelectedEntity(state),
+    );
+    const pluralForm = useSelector((state) =>
+        plural.selectors.getPluralForm(state),
+    );
+    const activeTranslationString = useSelector((state) =>
+        plural.selectors.getTranslationStringForSelectedEntity(state),
+    );
+
+    React.useLayoutEffect(() => {
+        // We want to run this only when the editor state has been reset.
+        if (changeSource === 'reset') {
+            dispatch(
+                editor.actions.setInitialTranslation(activeTranslationString),
+            );
+            updateTranslation(activeTranslationString, 'initial');
+        }
+    }, [
+        entity,
+        changeSource,
+        pluralForm,
+        activeTranslationString,
+        updateTranslation,
+        dispatch,
+    ]);
+}
 
 /**
  * Editor for regular translations.
@@ -17,7 +54,9 @@ import GenericTranslationForm from './GenericTranslationForm';
  * Shows a plural selector, a translation form and a menu.
  */
 export default function GenericEditor() {
-    editor.useLoadTranslation();
+    const dispatch = useDispatch();
+
+    useLoadTranslation();
     const updateTranslation = editor.useUpdateTranslation();
     const clearEditor = editor.useClearEditor();
     const copyOriginalIntoEditor = editor.useCopyOriginalIntoEditor();
@@ -35,11 +74,15 @@ export default function GenericEditor() {
         return null;
     }
 
+    function resetEditor() {
+        dispatch(editor.actions.reset());
+    }
+
     const original = pluralForm <= 0 ? entity.original : entity.original_plural;
 
     return (
         <>
-            <plural.PluralSelector />
+            <plural.PluralSelector resetEditor={resetEditor} />
             <GenericTranslationForm
                 sendTranslation={sendTranslation}
                 updateTranslation={updateTranslation}
