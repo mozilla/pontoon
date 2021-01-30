e6ad525c2ef055d36b25b0898dbcb4cc6baa3b10
Julen Ruiz Aizpuru <julenx@gmail.com>
2021-01-30 23:18:08 +0100

    Bug 1688571: fix negative performance side-effects of `UserNotificationsMenu`
    This now only listens for `click` events when the menu is visible by
    the means of leveraging the `useOnDiscard` hook.

--- frontend/src/core/user/index.js
+++ frontend/src/core/user/index.js
@@ -9,7 +9,7 @@ export { default as UserControls } from './components/UserControls';
 export { default as UserAvatar } from './components/UserAvatar';
 
 export type { Settings } from './actions';
-export type { SettingsState, UserState } from './reducer';
+export type { SettingsState, UserState, Notification } from './reducer';
 
 // Name of this module.
 // Used as the key to store this module's reducer.
