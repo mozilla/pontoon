545144606cc809aa5cfaf4f27d2d28bcc53d8350
Julen Ruiz Aizpuru <julenx@gmail.com>
2021-01-30 19:03:22 +0100

    Bug 1688571: fix negative performance side-effects of `UserMenu`
    This now only listens for `click` events when the menu is visible by
    the means of leveraging the `useOnDiscard` hook.

--- frontend/src/core/user/components/UserMenu.test.js
+++ frontend/src/core/user/components/UserMenu.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import FileUpload from './FileUpload';
 import SignOut from './SignOut';
-import { UserMenuBase } from './UserMenu';
+import UserMenuBase, { UserMenu } from './UserMenu';
 
 import { findLocalizedById } from 'test/utils';
 
@@ -17,36 +17,21 @@ function createShallowUserMenu({
     resource = 'myresource',
 } = {}) {
     return shallow(
-        <UserMenuBase
+        <UserMenu
             isReadOnly={isReadOnly}
             isTranslator={isTranslator}
-            user={{ isAuthenticated: isAuthenticated, isAdmin: isAdmin }}
+            user={{ isAuthenticated, isAdmin }}
             parameters={{ locale, project, resource }}
         />,
     );
 }
 
-describe('<UserMenuBase>', () => {
-    it('shows the user avatar when the user is logged in', () => {
-        const wrapper = createShallowUserMenu();
-
-        expect(wrapper.find('img')).toHaveLength(1);
-        expect(wrapper.find('.menu-icon')).toHaveLength(0);
-    });
-
-    it('shows the general menu icon when the user is logged out', () => {
-        const wrapper = createShallowUserMenu({ isAuthenticated: false });
-
-        expect(wrapper.find('img')).toHaveLength(0);
-        expect(wrapper.find('.menu-icon')).toHaveLength(1);
-    });
-
+describe('<UserMenu>', () => {
     it('shows the right menu items when the user is logged in', () => {
         const wrapper = createShallowUserMenu({
             locale: 'locale',
             project: 'project',
         });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find('.details')).toHaveLength(1);
         expect(wrapper.find('a[href="/settings/"]')).toHaveLength(1);
@@ -55,7 +40,6 @@ describe('<UserMenuBase>', () => {
 
     it('hides the right menu items when the user is logged out', () => {
         const wrapper = createShallowUserMenu({ isAuthenticated: false });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find('.details')).toHaveLength(0);
         expect(wrapper.find('a[href="/settings/"]')).toHaveLength(0);
@@ -64,7 +48,6 @@ describe('<UserMenuBase>', () => {
 
     it('shows upload & download menu items', () => {
         const wrapper = createShallowUserMenu();
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find(FileUpload)).toHaveLength(1);
         expect(
@@ -74,7 +57,6 @@ describe('<UserMenuBase>', () => {
 
     it('hides upload & download menu items when translating all projects', () => {
         const wrapper = createShallowUserMenu({ project: 'all-projects' });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
         expect(
@@ -84,7 +66,6 @@ describe('<UserMenuBase>', () => {
 
     it('hides upload & download menu items when translating all resources', () => {
         const wrapper = createShallowUserMenu({ resource: 'all-resources' });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
         expect(
@@ -94,25 +75,56 @@ describe('<UserMenuBase>', () => {
 
     it('hides upload menu item for users without permission to review translations', () => {
         const wrapper = createShallowUserMenu({ isTranslator: false });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
     });
 
     it('hides upload menu for read-only strings', () => {
         const wrapper = createShallowUserMenu({ isReadOnly: true });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find(FileUpload)).toHaveLength(0);
     });
 
     it('shows the admin menu items when the user is an admin', () => {
         const wrapper = createShallowUserMenu({ isAdmin: true });
-        wrapper.instance().setState({ visible: true });
 
         expect(wrapper.find('a[href="/admin/"]')).toHaveLength(1);
         expect(
             wrapper.find('a[href="/admin/projects/myproject/"]'),
         ).toHaveLength(1);
+    });
+});
+
+function createShallowUserMenuBase({
+    isAdmin = false,
+    isAuthenticated = true,
+} = {}) {
+    return shallow(<UserMenuBase user={{ isAuthenticated, isAdmin }} />);
+}
+
+describe('<UserMenuBase>', () => {
+    it('shows the user avatar when the user is logged in', () => {
+        const wrapper = createShallowUserMenuBase();
+
+        expect(wrapper.find('img')).toHaveLength(1);
+        expect(wrapper.find('.menu-icon')).toHaveLength(0);
+    });
+
+    it('shows the general menu icon when the user is logged out', () => {
+        const wrapper = createShallowUserMenuBase({ isAuthenticated: false });
+
+        expect(wrapper.find('img')).toHaveLength(0);
+        expect(wrapper.find('.menu-icon')).toHaveLength(1);
+    });
+
+    it('toggles the user menu when clicking the user avatar', () => {
+        const wrapper = createShallowUserMenuBase();
+        expect(wrapper.find('UserMenu')).toHaveLength(0);
+
+        wrapper.find('.selector').simulate('click');
+        expect(wrapper.find('UserMenu')).toHaveLength(1);
+
+        wrapper.find('.selector').simulate('click');
+        expect(wrapper.find('UserMenu')).toHaveLength(0);
     });
 });
