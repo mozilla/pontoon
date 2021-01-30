a596376e5b41574b2729a18dad2b852cb3c887da
Julen Ruiz Aizpuru <julenx@gmail.com>
2021-01-30 19:03:22 +0100

    Bug 1688571: fix negative performance side-effects of `UserMenu`
    This now only listens for `click` events when the menu is visible by
    the means of leveraging the `useOnDiscard` hook.

--- frontend/src/core/user/components/UserMenu.js
+++ frontend/src/core/user/components/UserMenu.js
@@ -1,8 +1,9 @@
 /* @flow */
 
 import * as React from 'react';
-import onClickOutside from 'react-onclickoutside';
 import { Localized } from '@fluent/react';
+
+import { useOnDiscard } from 'core/utils';
 
 import './UserMenu.css';
 import FileUpload from './FileUpload';
@@ -23,10 +24,221 @@ type State = {|
     visible: boolean,
 |};
 
+type UserMenuProps = Props & {
+    onDiscard: (e: SyntheticEvent<any>) => void,
+};
+
+export function UserMenu({
+    user,
+    parameters,
+    isTranslator,
+    isReadOnly,
+    signOut,
+    onDiscard,
+}: UserMenuProps) {
+    const { locale, project, resource } = parameters;
+
+    const canDownload =
+        project !== 'all-projects' && resource !== 'all-resources';
+
+    const canUpload =
+        /* TODO: Also disable for subpages (in-context l10n) when supported */
+        canDownload && isTranslator && !isReadOnly;
+
+    const ref = React.useRef(null);
+    useOnDiscard(ref, onDiscard);
+
+    return (
+        <ul ref={ref} className='menu'>
+            {user.isAuthenticated && (
+                <React.Fragment>
+                    <li className='details'>
+                        <a href={`/contributors/${user.username}/`}>
+                            <img
+                                src={user.gravatarURLBig}
+                                alt=''
+                                height='88'
+                                width='88'
+                            />
+                            <p className='name'>{user.nameOrEmail}</p>
+                            <p className='email'>{user.email}</p>
+                        </a>
+                    </li>
+                    <li className='horizontal-separator'></li>
+                </React.Fragment>
+            )}
+
+            <li>
+                <Localized
+                    id='user-UserMenu--download-terminology'
+                    elems={{
+                        glyph: <i className='fa fa-cloud-download-alt fa-fw' />,
+                    }}
+                >
+                    <a href={`/terminology/${locale}.tbx`}>
+                        {'<glyph></glyph>Download Terminology'}
+                    </a>
+                </Localized>
+            </li>
+
+            <li>
+                <Localized
+                    id='user-UserMenu--download-tm'
+                    elems={{
+                        glyph: <i className='fa fa-cloud-download-alt fa-fw' />,
+                    }}
+                >
+                    <a href={`/translation-memory/${locale}.${project}.tmx`}>
+                        {'<glyph></glyph>Download Translation Memory'}
+                    </a>
+                </Localized>
+            </li>
+
+            {canDownload && (
+                <li>
+                    <Localized
+                        id='user-UserMenu--download-translations'
+                        elems={{
+                            glyph: (
+                                <i className='fa fa-cloud-download-alt fa-fw' />
+                            ),
+                        }}
+                    >
+                        <a
+                            href={`/translations/?code=${locale}&slug=${project}&part=${resource}`}
+                        >
+                            {'<glyph></glyph>Download Translations'}
+                        </a>
+                    </Localized>
+                </li>
+            )}
+
+            {canUpload && (
+                <li>
+                    <FileUpload parameters={parameters} />
+                </li>
+            )}
+
+            <li className='horizontal-separator'></li>
+
+            <li>
+                <Localized
+                    id='user-UserMenu--terms'
+                    elems={{
+                        glyph: <i className='fa fa-gavel fa-fw' />,
+                    }}
+                >
+                    <a href='/terms/' rel='noopener noreferrer' target='_blank'>
+                        {'<glyph></glyph>Terms of Use'}
+                    </a>
+                </Localized>
+            </li>
+
+            <li>
+                <Localized
+                    id='user-UserMenu--github'
+                    elems={{
+                        glyph: <i className='fab fa-github fa-fw' />,
+                    }}
+                >
+                    <a
+                        href='https://github.com/mozilla/pontoon/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        {'<glyph></glyph>Hack it on GitHub'}
+                    </a>
+                </Localized>
+            </li>
+
+            <li>
+                <Localized
+                    id='user-UserMenu--feedback'
+                    elems={{
+                        glyph: <i className='fa fa-comment-dots fa-fw' />,
+                    }}
+                >
+                    <a
+                        href='https://discourse.mozilla.org/c/pontoon'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        {'<glyph></glyph>Give Feedback'}
+                    </a>
+                </Localized>
+            </li>
+
+            <li>
+                <Localized
+                    id='user-UserMenu--help'
+                    elems={{
+                        glyph: <i className='fa fa-life-ring fa-fw' />,
+                    }}
+                >
+                    <a
+                        href='https://mozilla-l10n.github.io/localizer-documentation/tools/pontoon/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        {'<glyph></glyph>Help'}
+                    </a>
+                </Localized>
+            </li>
+
+            {user.isAuthenticated && <li className='horizontal-separator'></li>}
+
+            {user.isAdmin && (
+                <React.Fragment>
+                    <li>
+                        <Localized
+                            id='user-UserMenu--admin'
+                            elems={{
+                                glyph: <i className='fa fa-wrench fa-fw' />,
+                            }}
+                        >
+                            <a href='/admin/'>{'<glyph></glyph>Admin'}</a>
+                        </Localized>
+                    </li>
+                    <li>
+                        <Localized
+                            id='user-UserMenu--admin-project'
+                            elems={{
+                                glyph: <i className='fa fa-wrench fa-fw' />,
+                            }}
+                        >
+                            <a href={`/admin/projects/${project}/`}>
+                                {'<glyph></glyph>Admin · Current Project'}
+                            </a>
+                        </Localized>
+                    </li>
+                </React.Fragment>
+            )}
+
+            {user.isAuthenticated && (
+                <React.Fragment>
+                    <li>
+                        <Localized
+                            id='user-UserMenu--settings'
+                            elems={{
+                                glyph: <i className='fa fa-cog fa-fw' />,
+                            }}
+                        >
+                            <a href='/settings/'>{'<glyph></glyph>Settings'}</a>
+                        </Localized>
+                    </li>
+                    <li>
+                        <SignOut signOut={signOut} />
+                    </li>
+                </React.Fragment>
+            )}
+        </ul>
+    );
+}
+
 /**
  * Renders user menu.
  */
-export class UserMenuBase extends React.Component<Props, State> {
+export default class UserMenuBase extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
@@ -40,9 +252,7 @@ export class UserMenuBase extends React.Component<Props, State> {
         });
     };
 
-    // This method is called by the Higher-Order Component `onClickOutside`
-    // when a user clicks outside the user menu.
-    handleClickOutside = () => {
+    handleDiscard = () => {
         this.setState({
             visible: false,
         });
@@ -56,17 +266,6 @@ export class UserMenuBase extends React.Component<Props, State> {
             signOut,
             user,
         } = this.props;
-
-        const locale = parameters.locale;
-        const project = parameters.project;
-        const resource = parameters.resource;
-
-        const canDownload =
-            project !== 'all-projects' && resource !== 'all-resources';
-
-        const canUpload =
-            /* TODO: Also disable for subpages (in-context l10n) when supported */
-            canDownload && isTranslator && !isReadOnly;
 
         return (
             <div className='user-menu'>
@@ -83,228 +282,17 @@ export class UserMenuBase extends React.Component<Props, State> {
                     )}
                 </div>
 
-                {!this.state.visible ? null : (
-                    <ul className='menu'>
-                        {!user.isAuthenticated ? null : (
-                            <React.Fragment>
-                                <li className='details'>
-                                    <a href={`/contributors/${user.username}/`}>
-                                        <img
-                                            src={user.gravatarURLBig}
-                                            alt=''
-                                            height='88'
-                                            width='88'
-                                        />
-                                        <p className='name'>
-                                            {user.nameOrEmail}
-                                        </p>
-                                        <p className='email'>{user.email}</p>
-                                    </a>
-                                </li>
-                                <li className='horizontal-separator'></li>
-                            </React.Fragment>
-                        )}
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--download-terminology'
-                                elems={{
-                                    glyph: (
-                                        <i className='fa fa-cloud-download-alt fa-fw' />
-                                    ),
-                                }}
-                            >
-                                <a href={`/terminology/${locale}.tbx`}>
-                                    {'<glyph></glyph>Download Terminology'}
-                                </a>
-                            </Localized>
-                        </li>
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--download-tm'
-                                elems={{
-                                    glyph: (
-                                        <i className='fa fa-cloud-download-alt fa-fw' />
-                                    ),
-                                }}
-                            >
-                                <a
-                                    href={`/translation-memory/${locale}.${project}.tmx`}
-                                >
-                                    {
-                                        '<glyph></glyph>Download Translation Memory'
-                                    }
-                                </a>
-                            </Localized>
-                        </li>
-
-                        {!canDownload ? null : (
-                            <li>
-                                <Localized
-                                    id='user-UserMenu--download-translations'
-                                    elems={{
-                                        glyph: (
-                                            <i className='fa fa-cloud-download-alt fa-fw' />
-                                        ),
-                                    }}
-                                >
-                                    <a
-                                        href={`/translations/?code=${locale}&slug=${project}&part=${resource}`}
-                                    >
-                                        {'<glyph></glyph>Download Translations'}
-                                    </a>
-                                </Localized>
-                            </li>
-                        )}
-
-                        {!canUpload ? null : (
-                            <li>
-                                <FileUpload parameters={parameters} />
-                            </li>
-                        )}
-
-                        <li className='horizontal-separator'></li>
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--terms'
-                                elems={{
-                                    glyph: <i className='fa fa-gavel fa-fw' />,
-                                }}
-                            >
-                                <a
-                                    href='/terms/'
-                                    rel='noopener noreferrer'
-                                    target='_blank'
-                                >
-                                    {'<glyph></glyph>Terms of Use'}
-                                </a>
-                            </Localized>
-                        </li>
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--github'
-                                elems={{
-                                    glyph: (
-                                        <i className='fab fa-github fa-fw' />
-                                    ),
-                                }}
-                            >
-                                <a
-                                    href='https://github.com/mozilla/pontoon/'
-                                    rel='noopener noreferrer'
-                                    target='_blank'
-                                >
-                                    {'<glyph></glyph>Hack it on GitHub'}
-                                </a>
-                            </Localized>
-                        </li>
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--feedback'
-                                elems={{
-                                    glyph: (
-                                        <i className='fa fa-comment-dots fa-fw' />
-                                    ),
-                                }}
-                            >
-                                <a
-                                    href='https://discourse.mozilla.org/c/pontoon'
-                                    rel='noopener noreferrer'
-                                    target='_blank'
-                                >
-                                    {'<glyph></glyph>Give Feedback'}
-                                </a>
-                            </Localized>
-                        </li>
-
-                        <li>
-                            <Localized
-                                id='user-UserMenu--help'
-                                elems={{
-                                    glyph: (
-                                        <i className='fa fa-life-ring fa-fw' />
-                                    ),
-                                }}
-                            >
-                                <a
-                                    href='https://mozilla-l10n.github.io/localizer-documentation/tools/pontoon/'
-                                    rel='noopener noreferrer'
-                                    target='_blank'
-                                >
-                                    {'<glyph></glyph>Help'}
-                                </a>
-                            </Localized>
-                        </li>
-
-                        {!user.isAuthenticated ? null : (
-                            <li className='horizontal-separator'></li>
-                        )}
-
-                        {!user.isAdmin ? null : (
-                            <React.Fragment>
-                                <li>
-                                    <Localized
-                                        id='user-UserMenu--admin'
-                                        elems={{
-                                            glyph: (
-                                                <i className='fa fa-wrench fa-fw' />
-                                            ),
-                                        }}
-                                    >
-                                        <a href='/admin/'>
-                                            {'<glyph></glyph>Admin'}
-                                        </a>
-                                    </Localized>
-                                </li>
-                                <li>
-                                    <Localized
-                                        id='user-UserMenu--admin-project'
-                                        elems={{
-                                            glyph: (
-                                                <i className='fa fa-wrench fa-fw' />
-                                            ),
-                                        }}
-                                    >
-                                        <a href={`/admin/projects/${project}/`}>
-                                            {
-                                                '<glyph></glyph>Admin · Current Project'
-                                            }
-                                        </a>
-                                    </Localized>
-                                </li>
-                            </React.Fragment>
-                        )}
-
-                        {!user.isAuthenticated ? null : (
-                            <React.Fragment>
-                                <li>
-                                    <Localized
-                                        id='user-UserMenu--settings'
-                                        elems={{
-                                            glyph: (
-                                                <i className='fa fa-cog fa-fw' />
-                                            ),
-                                        }}
-                                    >
-                                        <a href='/settings/'>
-                                            {'<glyph></glyph>Settings'}
-                                        </a>
-                                    </Localized>
-                                </li>
-                                <li>
-                                    <SignOut signOut={signOut} />
-                                </li>
-                            </React.Fragment>
-                        )}
-                    </ul>
+                {this.state.visible && (
+                    <UserMenu
+                        isReadOnly={isReadOnly}
+                        isTranslator={isTranslator}
+                        parameters={parameters}
+                        user={user}
+                        signOut={signOut}
+                        onDiscard={this.handleDiscard}
+                    />
                 )}
             </div>
         );
     }
 }
-
-export default onClickOutside(UserMenuBase);
