0089e9c0f4b244f2ae252d4e280085502e465dd5
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-02-03 18:16:23 +0100

    Actually, let's push that long function down

--- pontoon/machinery/static/js/machinery.js
+++ pontoon/machinery/static/js/machinery.js
@@ -1,6 +1,37 @@
 $(function () {
     var self = Pontoon;
 
+    // Trigger search with Enter
+    $('#search input')
+        .unbind('keydown.pontoon')
+        .bind('keydown.pontoon', function (e) {
+            var value = $(this).val();
+
+            if (e.which === 13 && value.length > 0) {
+                self.locale = $('.locale .selector .language').data();
+                getMachinery(value);
+                return false;
+            }
+        });
+
+    // Handle "Copy to clipboard" of search results on main Machinery page
+    var clipboard = new Clipboard('.machinery .machinery li');
+
+    clipboard.on('success', function (event) {
+        var successMessage = $(
+                '<span class="clipboard-success">Copied!</span>',
+            ),
+            $trigger = $(event.trigger);
+
+        $('.clipboard-success').remove();
+        $trigger.find('header').prepend(successMessage);
+        setTimeout(function () {
+            successMessage.fadeOut(500, function () {
+                successMessage.remove();
+            });
+        }, 1000);
+    });
+
     /*
      * Get suggestions from machine translation and translation memory
      *
@@ -406,35 +437,4 @@ $(function () {
 
         self.NProgressBind();
     }
-
-    // Trigger search with Enter
-    $('#search input')
-        .unbind('keydown.pontoon')
-        .bind('keydown.pontoon', function (e) {
-            var value = $(this).val();
-
-            if (e.which === 13 && value.length > 0) {
-                self.locale = $('.locale .selector .language').data();
-                getMachinery(value);
-                return false;
-            }
-        });
-
-    // Handle "Copy to clipboard" of search results on main Machinery page
-    var clipboard = new Clipboard('.machinery .machinery li');
-
-    clipboard.on('success', function (event) {
-        var successMessage = $(
-                '<span class="clipboard-success">Copied!</span>',
-            ),
-            $trigger = $(event.trigger);
-
-        $('.clipboard-success').remove();
-        $trigger.find('header').prepend(successMessage);
-        setTimeout(function () {
-            successMessage.fadeOut(500, function () {
-                successMessage.remove();
-            });
-        }, 1000);
-    });
 });
