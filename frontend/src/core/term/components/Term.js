2c82da37501ab67dfe3a57fbaa7e1dc063af73ce
Anuj Pandey <anujpandey785@gmail.com>
2021-02-11 03:07:47 +0530

    Ran prettier

--- frontend/src/core/term/components/Term.js
+++ frontend/src/core/term/components/Term.js
@@ -53,35 +53,35 @@ export default function Term(props: Props) {
 
     return (
         <Localized id='terms-Translation--copy' attrs={{ title: true }}>
-        <li
-            className={`term ${cannotCopy}`}
-            title='Copy Into Translation'
-            onClick={() => copyTermIntoEditor(term.translation)}
-        >
-            <header>
-                <span className='text'>{term.text}</span>
-                <span className='part-of-speech'>{term.partOfSpeech}</span>
-                <a
-                    href={`/${locale}/terminology/common/?string=${term.entityId}`}
-                    onClick={navigateToPath}
-                    className='translate'
-                >
-                    Translate
-                </a>
-            </header>
-            <p className='translation'>{term.translation}</p>
-            <div className='details'>
-                <p className='definition'>{term.definition}</p>
-                {!term.usage ? null : (
-                    <p className='usage'>
-                        <Localized id='term-Term--for-example'>
-                            <span className='title'>E.G.</span>
-                        </Localized>
-                        <span className='content'>{term.usage}</span>
-                    </p>
-                )}
-            </div>
-        </li>
+            <li
+                className={`term ${cannotCopy}`}
+                title='Copy Into Translation'
+                onClick={() => copyTermIntoEditor(term.translation)}
+            >
+                <header>
+                    <span className='text'>{term.text}</span>
+                    <span className='part-of-speech'>{term.partOfSpeech}</span>
+                    <a
+                        href={`/${locale}/terminology/common/?string=${term.entityId}`}
+                        onClick={navigateToPath}
+                        className='translate'
+                    >
+                        Translate
+                    </a>
+                </header>
+                <p className='translation'>{term.translation}</p>
+                <div className='details'>
+                    <p className='definition'>{term.definition}</p>
+                    {!term.usage ? null : (
+                        <p className='usage'>
+                            <Localized id='term-Term--for-example'>
+                                <span className='title'>E.G.</span>
+                            </Localized>
+                            <span className='content'>{term.usage}</span>
+                        </p>
+                    )}
+                </div>
+            </li>
         </Localized>
     );
 }
