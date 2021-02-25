da356dd29a3eb7f4776544bf3584d499c373d953
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-02-25 19:57:15 +0100

    Fix bug 1694504: Find & Replace fails for some strings (#1874)

--- pontoon/batch/utils.py
+++ pontoon/batch/utils.py
@@ -8,9 +8,9 @@ from pontoon.checks import DB_FORMATS
 from pontoon.checks.libraries import run_checks
 
 from fluent.syntax import (
-    ast,
     FluentParser,
     FluentSerializer,
+    visitor,
 )
 
 
@@ -52,14 +52,15 @@ def ftl_find_and_replace(string, find, replace):
 
     """
 
-    def replace_text_elements(node):
-        """Perform find and replace on text values only"""
-        if type(node) == ast.TextElement:
+    class FindAndReplaceTransformer(visitor.Transformer):
+        def visit_TextElement(self, node):
+            """Perform find and replace on text values only"""
             node.value = node.value.replace(find, replace)
-        return node
+            return node
 
+    transformer = FindAndReplaceTransformer()
     old_ast = parser.parse_entry(string)
-    new_ast = old_ast.traverse(replace_text_elements)
+    new_ast = transformer.visit(old_ast)
 
     return serializer.serialize_entry(new_ast)
 
