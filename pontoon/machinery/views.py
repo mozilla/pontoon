d7d0934853bd9ff997097ba6322d52ece38855e0
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-03-03 18:31:23 +0100

    Fix bug 1695226: Sort projects in Concordance search results by priority and disabled status (#1880)
    ArrayAgg does not support using distinct=True in combination with ordering,
    so we need to order manually (in python). We do that after pagination
    to reduce the number of processed rows.
    
    Also included: Always show project names on hover, even if user logged out or project read-only.

--- pontoon/machinery/views.py
+++ pontoon/machinery/views.py
@@ -15,7 +15,7 @@ from django.template.loader import get_template
 from django.utils.datastructures import MultiValueDictKeyError
 
 from pontoon.base import utils
-from pontoon.base.models import Entity, Locale, Translation
+from pontoon.base.models import Entity, Locale, Project, Translation
 from pontoon.machinery.utils import (
     get_concordance_search_data,
     get_google_translate_data,
@@ -81,6 +81,15 @@ def concordance_search(request):
         data = paginator.page(page)
     except EmptyPage:
         return JsonResponse({"results": [], "has_next": False})
+
+    # ArrayAgg (used in get_concordance_search_data()) does not support using
+    # distinct=True in combination with ordering, so we need to do one of them
+    # manually - after pagination, to reduce the number of rows processed.
+    projects = Project.objects.order_by("disabled", "-priority").values_list(
+        "name", flat=True
+    )
+    for r in data.object_list:
+        r["project_names"] = [p for p in projects if p in r["project_names"]]
 
     return JsonResponse(
         {"results": data.object_list, "has_next": data.has_next()}, safe=False
