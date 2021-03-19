6560c929b043ac5ab47f78a7cf741bccb0332bc2
Fabien LOISON <flozz@users.noreply.github.com>
2021-03-19 19:39:54 +0100

    Fix bug 1699381: make the media folder configurable (#1901)
    Also remove media/ subfolders.

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -511,7 +511,7 @@ SITE_ID = 1
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = path("media")
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", path("media"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
