3c8f39369f96858949605a4eb3f631a13fd60fe9
Fabien LOISON <flo@flogisoft.com>
2021-03-19 10:29:35 +0100

    Fix bug 1699455: suppress warning when the dotenv file is missing

--- manage.py
+++ manage.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 import os
 import sys
-import warnings
 
 import dotenv
 
 
-if __name__ == '__main__':
-    # Filter out missing .env warning, it's fine if we don't have one.
-    warnings.filterwarnings('ignore', module='dotenv')
+if __name__ == "__main__":
+    # Read dotenv file and inject it's values into the environment
+    root_path = os.path.dirname(__file__)
+    if (
+        "DOTENV_PATH" in os.environ
+        or os.path.isfile(os.path.join(root_path, ".env"))
+        or os.path.isfile(os.path.join(root_path, ".env", ".env"))
+    ):
+        dotenv.read_dotenv(os.environ.get("DOTENV_PATH"))
 
-    # Read .env file and inject it's values into the environment
-    dotenv.read_dotenv(os.environ.get("DOTENV_PATH"))
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontoon.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pontoon.settings")
 
     from django.core.management import execute_from_command_line
 
