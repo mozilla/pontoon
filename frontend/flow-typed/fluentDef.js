e8cbdfa5b0f2108dbe1d88e4eb01679084c35657
Axel Hecht <axel@pike.org>
2021-04-08 17:42:35 +0200

    Move @fluent/react typedef into fluentDef

--- frontend/flow-typed/fluentDef.js
+++ frontend/flow-typed/fluentDef.js
@@ -34,6 +34,27 @@ declare module '@fluent/langneg' {
     ): Array<string>;
 }
 
+declare module '@fluent/react' {
+    declare export class ReactLocalization {
+        constructor(Array<any>): ReactLocalization;
+    }
+
+    declare export function LocalizationProvider(
+        props: any,
+    ): React$Element<any>;
+
+    declare export interface LocalizedProps {
+        id: string;
+        attrs?: { [string]: boolean };
+        children?: React$Node;
+        vars?: { [string]: any };
+        elems?: { [string]: ?React$Element<any> };
+    }
+    declare export function Localized(
+        props: LocalizedProps,
+    ): React$Element<any>;
+}
+
 declare module '@fluent/syntax' {
     declare export class BaseNode {
         [name: string]: mixed;
--- frontend/flow-typed/myLibDef.js
+++ frontend/flow-typed/myLibDef.js
@@ -1,24 +1,3 @@
-declare module '@fluent/react' {
-    declare export class ReactLocalization {
-        constructor(Array<any>): ReactLocalization;
-    }
-
-    declare export function LocalizationProvider(
-        props: any,
-    ): React$Element<any>;
-
-    declare export interface LocalizedProps {
-        id: string;
-        attrs?: { [string]: boolean };
-        children?: React$Node;
-        vars?: { [string]: any };
-        elems?: { [string]: ?React$Element<any> };
-    }
-    declare export function Localized(
-        props: LocalizedProps,
-    ): React$Element<any>;
-}
-
 declare module 'react-content-marker' {
     declare export type TagFunction = (input: string) => React$Element<any>;
     declare export interface Parser {
