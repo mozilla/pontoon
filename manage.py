af0c47777adf442283d9fd34e5bfdd3eda8353eb
Fabien LOISON <flo@flogisoft.com>
2021-04-09 10:59:08 +0200

    Replaces django-dotenv by python-dotenv

--- manage.py
+++ manage.py
@@ -5,21 +5,9 @@ import sys
 import dotenv
 
 
-ROOT = os.path.dirname(__file__)
-
-
-def path(*args):
-    return os.path.join(ROOT, *args)
-
-
 if __name__ == "__main__":
     # Read dotenv file and inject it's values into the environment
-    if "DOTENV_PATH" in os.environ:
-        dotenv.read_dotenv(os.environ.get("DOTENV_PATH"))
-    elif os.path.isfile(path(".env")):
-        dotenv.read_dotenv(path(".env"))
-    elif os.path.isfile(path(".env", ".env")):
-        dotenv.read_dotenv(path(".env", ".env"))
+    dotenv.load_dotenv(dotenv_path=os.environ.get("DOTENV_PATH"))
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pontoon.settings")
 
--- pontoon/base/celeryapp.py
+++ pontoon/base/celeryapp.py
@@ -4,21 +4,8 @@ import dotenv
 from celery import Celery
 
 
-_dirname = os.path.dirname
-ROOT = _dirname(_dirname(_dirname(os.path.abspath(__file__))))
-
-
-def path(*args):
-    return os.path.join(ROOT, *args)
-
-
-# Read .env file and inject it's values into the environment
-if "DOTENV_PATH" in os.environ:
-    dotenv.read_dotenv(os.environ.get("DOTENV_PATH"))
-elif os.path.isfile(path(".env")):
-    dotenv.read_dotenv(path(".env"))
-elif os.path.isfile(path(".env", ".env")):
-    dotenv.read_dotenv(path(".env", ".env"))
+# Read dotenv file and inject it's values into the environment
+dotenv.load_dotenv(dotenv_path=os.environ.get("DOTENV_PATH"))
 
 # Set the default Django settings module for `celery`.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pontoon.settings")
