86ecf7c970876c9a843f20c1fabb5f4bb401f89d
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-04-28 00:10:40 +0200

    Add unit tests

--- frontend/src/core/user/components/UserNotificationsMenu.test.js
+++ frontend/src/core/user/components/UserNotificationsMenu.test.js
@@ -48,6 +48,20 @@ describe('<UserNotificationsMenu>', () => {
         expect(wrapper.find('.notification-list .no')).toHaveLength(0);
         expect(wrapper.find(UserNotification)).toHaveLength(1);
     });
+
+    it('calls the logUxAction function on click on .see-all', () => {
+        const logUxAction = sinon.spy();
+        const notifications = [];
+        const wrapper = shallow(
+            <UserNotificationsMenu
+                logUxAction={logUxAction}
+                notifications={notifications}
+            />,
+        );
+
+        wrapper.find('.see-all').simulate('click');
+        expect(logUxAction.called).toEqual(true);
+    });
 });
 
 describe('<UserNotificationsMenuBase>', () => {
@@ -76,7 +90,8 @@ describe('<UserNotificationsMenuBase>', () => {
         expect(wrapper.find('.user-notifications-menu')).toHaveLength(1);
     });
 
-    it('highlights the notifications icon when the user has unread notifications', () => {
+    it('highlights the notifications icon when the user has unread notifications and call logUxAction', () => {
+        const logUxAction = sinon.spy();
         const user = {
             isAuthenticated: true,
             notifications: {
@@ -85,9 +100,35 @@ describe('<UserNotificationsMenuBase>', () => {
             },
         };
         const wrapper = shallow(
-            <UserNotificationsMenuBase user={user} logUxAction={sinon.spy()} />,
+            <UserNotificationsMenuBase user={user} logUxAction={logUxAction} />,
         );
 
         expect(wrapper.find('.user-notifications-menu.unread')).toHaveLength(1);
+        expect(logUxAction.called).toEqual(true);
+    });
+
+    it('calls the logUxAction function on click on the icon if menu not visible', () => {
+        const logUxAction = sinon.spy();
+        const markAllNotificationsAsRead = sinon.spy();
+        const user = {
+            isAuthenticated: true,
+            notifications: {
+                has_unread: true,
+                notifications: [],
+            },
+        };
+        const wrapper = shallow(
+            <UserNotificationsMenuBase
+                logUxAction={logUxAction}
+                markAllNotificationsAsRead={markAllNotificationsAsRead}
+                user={user}
+            />,
+        );
+
+        wrapper.setState({
+            visible: false,
+        });
+        wrapper.find('.selector').simulate('click');
+        expect(logUxAction.called).toEqual(true);
     });
 });
