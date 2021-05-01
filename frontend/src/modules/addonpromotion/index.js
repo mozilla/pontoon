562c61a97aaff60f0deeb19df7554568fad6c065
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-05-01 11:54:15 +0200

    Typo

--- frontend/src/modules/addonpromotion/index.js
+++ frontend/src/modules/addonpromotion/index.js
@@ -4,4 +4,4 @@ export { default as AddonPromotion } from './components/AddonPromotion';
 
 // Name of this module.
 // Used as the key to store this module's reducer.
-export const NAME: string = 'addonpromotiom';
+export const NAME: string = 'addonpromotion';
