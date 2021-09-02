30fd32c82f001f6ed5408d16eb527ddf32f034ac
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-09-02 13:45:37 +0200

    Update CLDR plurals help text and link (#2000)

--- pontoon/base/models.py
+++ pontoon/base/models.py
@@ -737,8 +737,8 @@ class Locale(AggregatedStats):
         validators=[validate_cldr],
         help_text="""
         A comma separated list of
-        <a href="http://www.unicode.org/cldr/charts/dev/supplemental/language_plural_rules.html">
-        CLDR plural rules</a>, where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.
+        <a href="http://www.unicode.org/cldr/charts/latest/supplemental/language_plural_rules.html">
+        CLDR plural categories</a>, where 0 represents zero, 1 one, 2 two, 3 few, 4 many, and 5 other.
         E.g. 1,5
         """,
     )
