623d7bbb39934fe2152f78449efb25f3b273529e
Eemeli Aro <eemeli@gmail.com>
2021-09-30 09:02:01 -0400

    Add a separate frontend container (#1996)
    * refactor: Rename build-frontend make task as build-tagadmin
    
    * feat: Split frontend app into its own container
    
    * chore: Include static frontend build in webapp for compatibility
    
    * chore: Run all tests on `make test`; add `make test-webapp`
    
    * refactor: Rename "webapp" as "server"
    
    * refactor: Use fronted build stage rather than duplicated Dockerfile
    
    * chore: Rename run_server -> server_run also in docker/k8s-first-steps
    
    * chore: Run ESLint & Prettier tasks also in frontend
    
    * chore: Use new `make ci` to test & lint both frontend & server
    
    Based on `make test-server`, that now only runs server tests.

--- docker/server_run.sh
+++ docker/server_run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Prepares then runs the webapp.
+# Prepares then runs the server
 
 echo ">>> Setting up the db for Django"
 python manage.py migrate
-
-echo ">>> Starting frontend build process in the background"
-cd frontend && yarn start &
 
 echo ">>> Starting local server"
 python manage.py runserver 0.0.0.0:8000
