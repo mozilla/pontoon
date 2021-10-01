02e5faaf2db14894c684f307320b9400a71750bd
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-10-01 17:34:35 +0200

    Closes #2325: Update to latest javascript-time-ago (#2327)
    Replace old addLocale() alias, which is not supported by TypeScript.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { ConnectedRouter } from 'connected-react-router';
-import JavascriptTimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
 
 import './index.css';
 
@@ -17,7 +17,7 @@ import App from './App';
 // TODO: Once we have support for more locales in Pontoon, we should
 // make TimeAgo internationalized and initialize all locales here.
 // See: https://www.npmjs.com/package/react-time-ago
-JavascriptTimeAgo.locale(en);
+TimeAgo.addLocale(en);
 
 ReactDOM.render(
     <Provider store={store}>
