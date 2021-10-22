db5145f05f2a1b0e97a64633e66fcefbe63c3f72
Eemeli Aro <eemeli@gmail.com>
2021-10-22 15:12:09 +0300

    Use CSS & measured size to control resource comment overflow (#2336)
    * refactor: From Metadata.renderResourceComment to <ResourceComment>
    
    Uses key prop to ensure that switching entities resets see-more state
    
    * fix: Use CSS & measured size to control resource comment overflow
    
    * chore: Address code review comments
    
    * chore: Partial revert of previous, to return explicit null

--- frontend/src/modules/entitydetails/components/Metadata.tsx
+++ frontend/src/modules/entitydetails/components/Metadata.tsx
@@ -36,8 +36,46 @@ type Props = {
 
 type State = {
     popupTerms: Array<TermType>;
-    seeMore: boolean;
 };
+
+function ResourceComment({ comment }: { comment: string }) {
+    const ref = React.useRef<HTMLDivElement>(null);
+    const [overflow, setOverflow] = React.useState(false);
+    const [expand, setExpand] = React.useState(false);
+
+    React.useLayoutEffect(() => {
+        const body = ref.current?.querySelector<HTMLDivElement>('.comment');
+        if (body && body.scrollWidth > body.offsetWidth) setOverflow(true);
+    }, []);
+
+    return !comment ? null : (
+        <div className='resource-comment' ref={ref}>
+            <Localized
+                id='entitydetails-Metadata--resource-comment'
+                attrs={{ title: true }}
+            >
+                <Property
+                    className={expand ? 'comment expanded' : 'comment'}
+                    title='RESOURCE COMMENT'
+                >
+                    <Linkify
+                        properties={{
+                            target: '_blank',
+                            rel: 'noopener noreferrer',
+                        }}
+                    >
+                        {comment}
+                    </Linkify>
+                </Property>
+            </Localized>
+            {overflow && !expand && (
+                <Localized id='entitydetails-Metadata--see-more'>
+                    <button onClick={() => setExpand(true)}>See More</button>
+                </Localized>
+            )}
+        </div>
+    );
+}
 
 /**
  * Component showing metadata of an entity.
@@ -57,7 +95,6 @@ export default class Metadata extends React.Component<Props, State> {
         super(props);
         this.state = {
             popupTerms: [],
-            seeMore: false,
         };
     }
 
@@ -65,14 +102,9 @@ export default class Metadata extends React.Component<Props, State> {
         if (this.props.entity !== prevProps.entity) {
             this.setState({
                 popupTerms: [],
-                seeMore: false,
             });
         }
     }
-
-    handleClickOnSeeMore: () => void = () => {
-        this.setState({ seeMore: true });
-    };
 
     handleClickOnPlaceable: (
         e: React.MouseEvent<HTMLParagraphElement>,
@@ -208,44 +240,6 @@ export default class Metadata extends React.Component<Props, State> {
                           );
                       })}
             </>
-        );
-    }
-
-    renderResourceComment(entity: Entity): React.ReactNode {
-        const { seeMore } = this.state;
-        const MAX_LENGTH = 85;
-
-        if (!entity.resource_comment) {
-            return null;
-        }
-
-        let comment = entity.resource_comment;
-
-        return (
-            <Localized
-                id='entitydetails-Metadata--resource-comment'
-                attrs={{ title: true }}
-            >
-                <Property title='RESOURCE COMMENT' className='comment'>
-                    <Linkify
-                        properties={{
-                            target: '_blank',
-                            rel: 'noopener noreferrer',
-                        }}
-                    >
-                        {comment.length < MAX_LENGTH || seeMore
-                            ? comment
-                            : comment.slice(0, MAX_LENGTH) + '\u2026'}
-                    </Linkify>
-                    {comment.length < MAX_LENGTH || seeMore ? null : (
-                        <Localized id='entitydetails-Metadata--see-more'>
-                            <button onClick={this.handleClickOnSeeMore}>
-                                {'See More'}
-                            </button>
-                        </Localized>
-                    )}
-                </Property>
-            </Localized>
         );
     }
 
@@ -394,7 +388,10 @@ export default class Metadata extends React.Component<Props, State> {
                 {this.renderPinnedComments(teamComments)}
                 {this.renderComment(entity)}
                 {this.renderGroupComment(entity)}
-                {this.renderResourceComment(entity)}
+                <ResourceComment
+                    comment={entity.resource_comment}
+                    key={entity.pk}
+                />
                 <FluentAttribute entity={entity} />
                 {this.renderContext(entity)}
                 {this.renderSources(entity)}
