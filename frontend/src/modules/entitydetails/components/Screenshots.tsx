7eddb8f47b43193033105f17813ec824be4bed75
Axel Hecht <axel@pike.org>
2022-01-30 08:04:01 +0100

    Convert Screenshots to functional component

--- frontend/src/modules/entitydetails/components/Screenshots.tsx
+++ frontend/src/modules/entitydetails/components/Screenshots.tsx
@@ -15,37 +15,25 @@ type Props = {
  * This component looks at all URLs to an image (either .png or .jpg) in a
  * source string and then shows a miniature of those images.
  */
-export default class Screenshots extends React.Component<Props> {
-    openLightbox: (image: string) => () => any = (image: string) => {
-        return () => this.props.openLightbox(image);
-    };
+export default function Screenshots(props: Props) {
+    const { locale, openLightbox, source } = props;
 
-    getImages(): Array<React.ReactNode> {
-        const { locale, source } = this.props;
+    const images = getImageURLs(source, locale);
 
-        const images = getImageURLs(source, locale);
-        return images.map((urlWithLocale, i) => {
-            return (
+    if (images.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className='screenshots'>
+            {images.map((urlWithLocale, i) => (
                 <img
                     src={urlWithLocale}
                     alt=''
                     key={i}
-                    onClick={this.openLightbox(urlWithLocale)}
+                    onClick={() => openLightbox(urlWithLocale)}
                 />
-            );
-        });
-    }
-
-    render(): React.ReactNode {
-        const images = this.getImages();
-        if (images.length === 0) {
-            return null;
-        }
-
-        return (
-            <React.Fragment>
-                <div className='screenshots'>{images}</div>
-            </React.Fragment>
-        );
-    }
+            ))}
+        </div>
+    );
 }
