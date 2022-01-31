2b9294f52ce14615e53c8d5121ccc444a979a1bb
Axel Hecht <axel@pike.org>
2022-01-31 17:07:02 +0100

    Fix test

--- frontend/src/modules/entitydetails/components/Screenshots.test.js
+++ frontend/src/modules/entitydetails/components/Screenshots.test.js
@@ -2,19 +2,23 @@ import * as lightbox from 'core/lightbox';
 
 import Screenshots from './Screenshots';
 import { createReduxStore, mountComponentWithStore } from 'test/store';
+import sinon from 'sinon';
 
 describe('<Screenshots>', () => {
     it('shows a Lightbox on image click', () => {
         const store = createReduxStore();
+        const stub = sinon.stub(store, 'dispatch');
         const source = 'That is an image URL: http://link.to/image.png';
         const wrapper = mountComponentWithStore(Screenshots, store, {
             locale: 'kg',
             source,
         });
         wrapper.find('img').simulate('click');
-
-        const newState = store.getState()[lightbox.NAME];
-
-        expect(newState.image).toEqual('http://link.to/image.png');
+        expect(
+            stub.calledOnceWith(
+                lightbox.actions.open('http://link.to/image.png'),
+            ),
+        ).toBeTruthy();
+        store.dispatch.restore();
     });
 });
