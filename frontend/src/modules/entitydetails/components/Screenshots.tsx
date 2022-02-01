737e81d6cb9fa30402e4dd9057c5a1890416ba7d
Axel Hecht <axel@pike.org>
2022-02-01 19:39:29 +0100

    Make Screenshots deal with state directly (#2415)
    * Convert Screenshots to functional component
    * Make Screenshots interact with state, remove the props that used to do that
    * Update key to be the url
    
    Co-authored-by: Eemeli Aro <eemeli@gmail.com>

--- frontend/src/modules/entitydetails/components/Screenshots.tsx
+++ frontend/src/modules/entitydetails/components/Screenshots.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 
 import './Screenshots.css';
 import { getImageURLs } from 'core/linkify';
+import { actions } from 'core/lightbox';
+import { useAppDispatch } from 'hooks';
 
 type Props = {
     locale: string;
     source: string;
-    openLightbox: (image: string) => void;
 };
 
 /**
@@ -15,37 +16,26 @@ type Props = {
  * This component looks at all URLs to an image (either .png or .jpg) in a
  * source string and then shows a miniature of those images.
  */
-export default class Screenshots extends React.Component<Props> {
-    openLightbox: (image: string) => () => any = (image: string) => {
-        return () => this.props.openLightbox(image);
-    };
+export default function Screenshots(props: Props) {
+    const { locale, source } = props;
+    const dispatch = useAppDispatch();
 
-    getImages(): Array<React.ReactNode> {
-        const { locale, source } = this.props;
+    const images = getImageURLs(source, locale);
 
-        const images = getImageURLs(source, locale);
-        return images.map((urlWithLocale, i) => {
-            return (
+    if (images.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className='screenshots'>
+            {images.map((urlWithLocale) => (
                 <img
                     src={urlWithLocale}
                     alt=''
-                    key={i}
-                    onClick={this.openLightbox(urlWithLocale)}
+                    key={urlWithLocale}
+                    onClick={() => dispatch(actions.open(urlWithLocale))}
                 />
-            );
-        });
-    }
-
-    render(): React.ReactNode {
-        const images = this.getImages();
-        if (images.length === 0) {
-            return null;
-        }
-
-        return (
-            <React.Fragment>
-                <div className='screenshots'>{images}</div>
-            </React.Fragment>
-        );
-    }
+            ))}
+        </div>
+    );
 }
