3511641f3775d365e0b3a98944b3b9120f31faa2
Eemeli Aro <eemeli@gmail.com>
2022-02-14 11:39:19 +0200

    chore(frontend): Add & configure jest & ts-jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,37 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+    verbose: true,
+    roots: ['<rootDir>/src'],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+    coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    ],
+    testEnvironment: 'jsdom',
+    preset: 'ts-jest',
+    globals: {
+        'ts-jest': {
+            isolatedModules: true,
+        },
+    },
+    transform: {
+        '\\.(ts|js)x?$': 'ts-jest',
+    },
+    transformIgnorePatterns: [
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    ],
+    resetMocks: true,
+    moduleNameMapper: {
+        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+            'identity-obj-proxy',
+        '\\.svg$': '<rootDir>/__mocks__/svg.js',
+        '~(.*)$': '<rootDir>/src/$1',
+    },
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+    ],
+    testTimeout: 10000, // optional
+};
