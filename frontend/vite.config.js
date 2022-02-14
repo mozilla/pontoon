74b95c1dc3205513bde418bda276006a7522fefa
Eemeli Aro <eemeli@gmail.com>
2022-02-14 14:52:49 +0200

    Use /static/ as base URL for frontend, to match previous

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/static/',
     plugins: [react()],
     resolve: {
         alias: {
@@ -18,9 +19,9 @@ export default defineConfig(({ mode }) => ({
         outDir: 'build',
         rollupOptions: {
             external: [
-                '/static/css/boilerplate.css',
-                '/static/css/fonts.css',
-                '/static/css/fontawesome-all.css',
+                '/css/boilerplate.css',
+                '/css/fonts.css',
+                '/css/fontawesome-all.css',
             ],
         },
     },
