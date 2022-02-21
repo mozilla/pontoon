e2f514de3a9954409d5e00f19b103a79fa929649
Eemeli Aro <eemeli@gmail.com>
2022-02-21 13:27:20 +0200

    chore(tag-admin): Update from rollup-plugin-replace to @rollup/plugin-replace

--- tag-admin/rollup.config.js
+++ tag-admin/rollup.config.js
@@ -2,8 +2,8 @@ import path from 'path';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import css from 'rollup-plugin-css-only';
-import replace from 'rollup-plugin-replace';
 
 /** @type {import('rollup').RollupOptions} */
 export default {
@@ -11,6 +11,7 @@ export default {
     output: { dir: 'dist/' },
     plugins: [
         replace({
+            preventAssignment: true,
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         resolve(),
