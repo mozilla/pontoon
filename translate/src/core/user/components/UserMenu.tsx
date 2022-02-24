907f573f8e81e2945d08ae6ec576c98660f039df
Eemeli Aro <eemeli@gmail.com>
2022-02-24 17:15:44 +0200

    refactor: UserControls and UserMenu as functional components

--- translate/src/core/user/components/UserMenu.tsx
+++ translate/src/core/user/components/UserMenu.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
 import { Localized } from '@fluent/react';
-
-import { useOnDiscard } from '~/core/utils';
-
-import './UserMenu.css';
-import FileUpload from './FileUpload';
-import SignOut from './SignOut';
+import React, { useRef, useState } from 'react';
 
 import type { NavigationParams } from '~/core/navigation';
 import type { UserState } from '~/core/user';
+import { useOnDiscard } from '~/core/utils';
+
+import FileUpload from './FileUpload';
+import SignOut from './SignOut';
+
+import './UserMenu.css';
 
 type Props = {
   isReadOnly: boolean;
@@ -16,10 +16,6 @@ type Props = {
   parameters: NavigationParams;
   signOut: () => void;
   user: UserState;
-};
-
-type State = {
-  visible: boolean;
 };
 
 type UserMenuProps = Props & {
@@ -39,17 +35,16 @@ export function UserMenu({
   const canDownload =
     project !== 'all-projects' && resource !== 'all-resources';
 
-  const canUpload =
-    /* TODO: Also disable for subpages (in-context l10n) when supported */
-    canDownload && isTranslator && !isReadOnly;
+  /* TODO: Also disable for subpages (in-context l10n) when supported */
+  const canUpload = canDownload && isTranslator && !isReadOnly;
 
-  const ref = React.useRef(null);
+  const ref = useRef<HTMLUListElement>(null);
   useOnDiscard(ref, onDiscard);
 
   return (
     <ul ref={ref} className='menu'>
       {user.isAuthenticated && (
-        <React.Fragment>
+        <>
           <li className='details'>
             <a href={`/contributors/${user.username}/`}>
               <img src={user.gravatarURLBig} alt='' height='88' width='88' />
@@ -58,15 +53,13 @@ export function UserMenu({
             </a>
           </li>
           <li className='horizontal-separator'></li>
-        </React.Fragment>
+        </>
       )}
 
       <li>
         <Localized
           id='user-UserMenu--download-terminology'
-          elems={{
-            glyph: <i className='fa fa-cloud-download-alt fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fa fa-cloud-download-alt fa-fw' /> }}
         >
           <a href={`/terminology/${locale}.tbx`}>
             {'<glyph></glyph>Download Terminology'}
@@ -77,9 +70,7 @@ export function UserMenu({
       <li>
         <Localized
           id='user-UserMenu--download-tm'
-          elems={{
-            glyph: <i className='fa fa-cloud-download-alt fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fa fa-cloud-download-alt fa-fw' /> }}
         >
           <a href={`/translation-memory/${locale}.${project}.tmx`}>
             {'<glyph></glyph>Download Translation Memory'}
@@ -91,9 +82,7 @@ export function UserMenu({
         <li>
           <Localized
             id='user-UserMenu--download-translations'
-            elems={{
-              glyph: <i className='fa fa-cloud-download-alt fa-fw' />,
-            }}
+            elems={{ glyph: <i className='fa fa-cloud-download-alt fa-fw' /> }}
           >
             <a
               href={`/translations/?code=${locale}&slug=${project}&part=${resource}`}
@@ -115,9 +104,7 @@ export function UserMenu({
       <li>
         <Localized
           id='user-UserMenu--terms'
-          elems={{
-            glyph: <i className='fa fa-gavel fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fa fa-gavel fa-fw' /> }}
         >
           <a href='/terms/' rel='noopener noreferrer' target='_blank'>
             {'<glyph></glyph>Terms of Use'}
@@ -128,9 +115,7 @@ export function UserMenu({
       <li>
         <Localized
           id='user-UserMenu--github'
-          elems={{
-            glyph: <i className='fab fa-github fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fab fa-github fa-fw' /> }}
         >
           <a
             href='https://github.com/mozilla/pontoon/'
@@ -145,9 +130,7 @@ export function UserMenu({
       <li>
         <Localized
           id='user-UserMenu--feedback'
-          elems={{
-            glyph: <i className='fa fa-comment-dots fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fa fa-comment-dots fa-fw' /> }}
         >
           <a
             href='https://discourse.mozilla.org/c/pontoon'
@@ -162,9 +145,7 @@ export function UserMenu({
       <li>
         <Localized
           id='user-UserMenu--help'
-          elems={{
-            glyph: <i className='fa fa-life-ring fa-fw' />,
-          }}
+          elems={{ glyph: <i className='fa fa-life-ring fa-fw' /> }}
         >
           <a
             href='https://mozilla-l10n.github.io/localizer-documentation/tools/pontoon/'
@@ -179,13 +160,11 @@ export function UserMenu({
       {user.isAuthenticated && <li className='horizontal-separator'></li>}
 
       {user.isAdmin && (
-        <React.Fragment>
+        <>
           <li>
             <Localized
               id='user-UserMenu--admin'
-              elems={{
-                glyph: <i className='fa fa-wrench fa-fw' />,
-              }}
+              elems={{ glyph: <i className='fa fa-wrench fa-fw' /> }}
             >
               <a href='/admin/'>{'<glyph></glyph>Admin'}</a>
             </Localized>
@@ -193,26 +172,22 @@ export function UserMenu({
           <li>
             <Localized
               id='user-UserMenu--admin-project'
-              elems={{
-                glyph: <i className='fa fa-wrench fa-fw' />,
-              }}
+              elems={{ glyph: <i className='fa fa-wrench fa-fw' /> }}
             >
               <a href={`/admin/projects/${project}/`}>
                 {'<glyph></glyph>Admin · Current Project'}
               </a>
             </Localized>
           </li>
-        </React.Fragment>
+        </>
       )}
 
       {user.isAuthenticated && (
-        <React.Fragment>
+        <>
           <li>
             <Localized
               id='user-UserMenu--settings'
-              elems={{
-                glyph: <i className='fa fa-cog fa-fw' />,
-              }}
+              elems={{ glyph: <i className='fa fa-cog fa-fw' /> }}
             >
               <a href='/settings/'>{'<glyph></glyph>Settings'}</a>
             </Localized>
@@ -220,59 +195,33 @@ export function UserMenu({
           <li>
             <SignOut signOut={signOut} />
           </li>
-        </React.Fragment>
+        </>
       )}
     </ul>
   );
 }
 
-/**
- * Renders user menu.
- */
-export default class UserMenuBase extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      visible: false,
-    };
-  }
-
-  toggleVisibility: () => void = () => {
-    this.setState((state) => {
-      return { visible: !state.visible };
-    });
-  };
-
-  handleDiscard: () => void = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  render(): React.ReactElement<'div'> {
-    const { isReadOnly, isTranslator, parameters, signOut, user } = this.props;
-
-    return (
-      <div className='user-menu'>
-        <div className='selector' onClick={this.toggleVisibility}>
-          {user.isAuthenticated ? (
-            <img src={user.gravatarURLSmall} alt='' height='44' width='44' />
-          ) : (
-            <div className='menu-icon fa fa-bars' />
-          )}
-        </div>
-
-        {this.state.visible && (
-          <UserMenu
-            isReadOnly={isReadOnly}
-            isTranslator={isTranslator}
-            parameters={parameters}
-            user={user}
-            signOut={signOut}
-            onDiscard={this.handleDiscard}
+export default function UserMenuBase(props: Props): React.ReactElement<'div'> {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className='user-menu'>
+      <div
+        className='selector'
+        onClick={() => setVisible((visible) => !visible)}
+      >
+        {props.user.isAuthenticated ? (
+          <img
+            src={props.user.gravatarURLSmall}
+            alt=''
+            height='44'
+            width='44'
           />
+        ) : (
+          <div className='menu-icon fa fa-bars' />
         )}
       </div>
-    );
-  }
+
+      {visible && <UserMenu {...props} onDiscard={() => setVisible(false)} />}
+    </div>
+  );
 }
