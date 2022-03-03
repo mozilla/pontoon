ba38a288cd357416d3e8524d6216f3e0eec425d3
Eemeli Aro <eemeli@gmail.com>
2022-03-03 16:49:31 -0600

    refactor: Add new Location context & custom Link component

--- translate/src/index.tsx
+++ translate/src/index.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
 import { ConnectedRouter } from 'connected-react-router';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en.json';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-import './index.css';
-
-import { AppLocalizationProvider } from '~/core/l10n';
-
-import history from './historyInstance';
-import store from './store';
 import App from './App';
+import { LocationProvider } from './context/location';
+import { AppLocalizationProvider } from './core/l10n';
+import history from './historyInstance';
+import './index.css';
+import store from './store';
 
 // TODO: Once we have support for more locales in Pontoon, we should
 // make TimeAgo internationalized and initialize all locales here.
 // See: https://www.npmjs.com/package/react-time-ago
 TimeAgo.addLocale(en);
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <AppLocalizationProvider>
-        <App />
-      </AppLocalizationProvider>
+      <LocationProvider history={history}>
+        <AppLocalizationProvider>
+          <App />
+        </AppLocalizationProvider>
+      </LocationProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
