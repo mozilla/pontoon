ac19a58eff4a7e8234032169e0cc510c0a74f007
Eemeli Aro <eemeli@gmail.com>
2022-03-04 11:51:40 -0600

    chore: Remove unused files & dependencies, including react-router

--- translate/src/core/navigation/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Location } from '~/hooks/useLocation';
-export type NavigationParams = Location;
-
-export { default as actions } from './actions';
-
-// Name of this module.
-// Used as the key to store this module's reducer.
-export const NAME = 'navigation';
