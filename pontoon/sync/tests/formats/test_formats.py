e17b2b4a5917c611413824b729644eae117b0be6
Quentin G <qgalliano@gmail.com>
2022-03-15 14:16:23 +0300

    Add support for key-value JSON #2322  (#2352)
    * WIP
    
    * Fix file update
    
    * Improve behavior on empty objetcs
    
    * Add unit tests
    
    * Remove debug
    
    * Fix lint
    
    * Fix header, fix tests, add nested test
    
    * Fix file format check test
    
    * Add support for dots in key name by using different internal separator
    
    * Defina a short specification
    
    * Fix linting
    
    * Update pontoon/sync/formats/__init__.py
    
    Co-authored-by: Eemeli Aro <eemeli@gmail.com>
    
    * Update pontoon/sync/formats/json_keyvalue.py
    
    Co-authored-by: Eemeli Aro <eemeli@gmail.com>
    
    * Use a JSON array as an internal key for key-value translations
    
    * Deduplicate common JSON code
    
    * Add ambiguous keys test & make use of order
    
    * Add key-value json to the doc, fix a crash and a typo
    
    Co-authored-by: Eemeli Aro <eemeli@gmail.com>

--- pontoon/sync/tests/formats/test_formats.py
+++ pontoon/sync/tests/formats/test_formats.py
@@ -1,22 +1,23 @@
 from pontoon.base.tests import TestCase
-from pontoon.sync.formats import are_compatible_formats
+from pontoon.sync.formats import are_compatible_files
 
 
 class CompareLocalesResourceTests(TestCase):
-    def test_are_compatible_formats(self):
+    def test_are_compatible_files(self):
         """
         Return True if both extensions use the same file parser.
         """
         # Same extension
-        assert are_compatible_formats(".dtd", ".dtd")
-        assert are_compatible_formats(".po", ".po")
+        assert are_compatible_files("file_a.dtd", "file_b.dtd")
+        assert are_compatible_files("file_a.po", "file_b.po")
 
         # Same parser
-        assert are_compatible_formats(".po", ".pot")
-        assert are_compatible_formats(".xlf", ".xliff")
+        assert are_compatible_files("file_a.po", "file_b.pot")
+        assert are_compatible_files("file_a.xlf", "file_b.xliff")
 
         # Different parser
-        assert not are_compatible_formats(".ftl", ".json")
+        assert not are_compatible_files("file_a.ftl", "file_b.json")
+        assert not are_compatible_files("file_a.json", "messages.json")
 
         # Not supported file format
-        assert not are_compatible_formats(".something", ".else")
+        assert not are_compatible_files("file_a.something", "file_b.else")
