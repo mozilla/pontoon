3872c3341f96c7a07f16171c283c68ace012b3ef
Eemeli Aro <eemeli@gmail.com>
2022-08-03 10:16:13 +0200

    Only call /get-users/ when adding a comment (#2588)

--- translate/src/core/comments/components/AddComment.test.js
+++ translate/src/core/comments/components/AddComment.test.js
@@ -1,29 +1,23 @@
-import React from 'react';
 import { shallow } from 'enzyme';
+import React from 'react';
 import sinon from 'sinon';
+
+import { MentionUsers } from '~/context/MentionUsers';
+import { createReduxStore, mountComponentWithStore } from '~/test/store';
 
 import { AddComment } from './AddComment';
 
 const USER = {
-  user: {
-    user: 'RSwanson',
-    username: 'Ron_Swanson',
-    imageURL: '',
-    users: [
-      {
-        name: 'April Ludwig',
-        url: 'aprilL@parksdept.com',
-        display: 'April',
-      },
-    ],
-  },
+  user: 'RSwanson',
+  username: 'Ron_Swanson',
+  imageURL: '',
 };
 
 describe('<AddComment>', () => {
   it('calls submitComment function', () => {
     const submitCommentFn = sinon.spy();
     const wrapper = shallow(
-      <AddComment {...USER} submitComment={submitCommentFn} />,
+      <AddComment submitComment={submitCommentFn} user={USER} />,
     );
 
     const event = {
@@ -32,5 +26,18 @@ describe('<AddComment>', () => {
 
     wrapper.find('button').simulate('onClick', event);
     expect(submitCommentFn.calledOnce).toBeTruthy;
+  });
+
+  it('fetches mentionable users on render', () => {
+    const initMentions = sinon.spy();
+    const store = createReduxStore();
+    const Wrapper = () => (
+      <MentionUsers.Provider value={{ initMentions, mentionUsers: [] }}>
+        <AddComment user={USER} />
+      </MentionUsers.Provider>
+    );
+    mountComponentWithStore(Wrapper, store);
+
+    expect(initMentions.calledOnce).toBeTruthy;
   });
 });
