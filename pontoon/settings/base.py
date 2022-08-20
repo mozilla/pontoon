ab6fa0f26e5cfa7bdb99e97aaad529fed1f289a2
Matjaž Horvat <matjaz.horvat@gmail.com>
2022-08-21 01:53:39 +0200

    Rename contributor.js -> profile.js

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -513,7 +513,7 @@ PIPELINE_JS = {
         "source_filenames": (
             "js/lib/Chart.bundle.js",
             "js/insights_charts.js",
-            "js/contributor.js",
+            "js/profile.js",
         ),
         "output_filename": "js/profile.min.js",
     },
