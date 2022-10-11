59a5597ffddc9f6f34f1148e6bd952adaf9bc2a4
Eemeli Aro <eemeli@gmail.com>
2022-10-11 15:17:08 -0400

    Refactor accesskey extraction to include more cases (#2636)

--- translate/src/core/utils/fluent/extractAccessKeyCandidates.test.js
+++ translate/src/core/utils/fluent/extractAccessKeyCandidates.test.js
@@ -1,69 +1,108 @@
+import ftl from '@fluent/dedent';
 import { extractAccessKeyCandidates } from './extractAccessKeyCandidates';
 import { parseEntry } from './parser';
 
 describe('extractAccessKeyCandidates', () => {
-  it('returns null if the message has no attributes', () => {
-    const message = parseEntry('title = Title');
-    const res = extractAccessKeyCandidates(message);
-
-    expect(res).toEqual(null);
-  });
-
-  it('returns null if the message has no accesskey attribute', () => {
-    const input = 'title =' + '\n    .foo = Bar';
+  it('returns an empty list if the message has no label attribute or value', () => {
+    const input = ftl`
+      title =
+        .foo = Bar
+        .accesskey = B
+      `;
     const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
 
-    expect(res).toEqual(null);
-  });
-
-  it('returns null if the message has no label attribute or value', () => {
-    const input = 'title =' + '\n    .foo = Bar' + '\n    .accesskey = B';
-    const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
-
-    expect(res).toEqual(null);
+    expect(res).toEqual([]);
   });
 
   it('returns a list of access keys from the message value', () => {
-    const input = 'title = Candidates' + '\n    .accesskey = B';
+    const input = ftl`
+      title = Candidates
+        .accesskey = B
+      `;
     const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
 
     expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
   });
 
   it('returns a list of access keys from the label attribute', () => {
-    const input =
-      'title = Title' + '\n    .label = Candidates' + '\n    .accesskey = B';
+    const input = ftl`
+      title = Title
+        .label = Candidates
+        .accesskey = B
+        .aria-label = Ignore this
+        .value = Ignore this
+      `;
     const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
+
+    expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
+  });
+
+  it('returns a list of access keys from the value attribute', () => {
+    const input = ftl`
+      title =
+        .accesskey = B
+        .aria-label = Ignore this
+        .value = Candidates
+      `;
+    const message = parseEntry(input);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
+
+    expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
+  });
+
+  it('returns a list of access keys from the aria-label attribute', () => {
+    const input = ftl`
+      title =
+        .accesskey = B
+        .aria-label = Candidates
+      `;
+    const message = parseEntry(input);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
+
+    expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
+  });
+
+  it('returns a list of access keys from the prefixed label attribute', () => {
+    const input = ftl`
+      title = Title
+        .buttonlabel = Candidates
+        .buttonaccesskey = B
+        .label = Ignore this
+        .value = Ignore this
+      `;
+    const message = parseEntry(input);
+    const res = extractAccessKeyCandidates(message, 'buttonaccesskey');
 
     expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
   });
 
   it('Does not take Placeables into account when generating candidates', () => {
-    const input =
-      'title = Title' +
-      '\n    .label = Candidates { brand }' +
-      '\n    .accesskey = B';
+    const input = ftl`
+      title = Title
+        .label = Candidates { brand }
+        .accesskey = B
+      `;
     const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
 
     expect(res).toEqual(['C', 'a', 'n', 'd', 'i', 't', 'e', 's']);
   });
 
   it('returns a list of access keys from all label attribute variants', () => {
-    const input =
-      'title = Title' +
-      '\n    .label =' +
-      '\n        { PLATFORM() ->' +
-      '\n            [windows] Ctrl' +
-      '\n           *[other] Cmd' +
-      '\n        }' +
-      '\n    .accesskey = C';
+    const input = ftl`
+      title = Title' +
+        .label =
+            { PLATFORM() ->
+                [windows] Ctrl
+               *[other] Cmd
+            }
+        .accesskey = C
+      `;
     const message = parseEntry(input);
-    const res = extractAccessKeyCandidates(message);
+    const res = extractAccessKeyCandidates(message, 'accesskey');
 
     expect(res).toEqual(['C', 't', 'r', 'l', 'm', 'd']);
   });
