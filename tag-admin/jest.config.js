610b2f74120d747fa0baeea61019b71082a4ed40
Eemeli Aro <eemeli@gmail.com>
2022-12-07 15:19:31 +0200

    Update to Jest 29

--- tag-admin/jest.config.js
+++ tag-admin/jest.config.js
@@ -3,7 +3,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testEnvironment: 'jsdom',
-  testURL: 'https://nowhere.com/at/all',
+  testEnvironmentOptions: { url: 'https://nowhere.com/at/all' },
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
   },
