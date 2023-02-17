1f74eec41cf4bb156422272890b0dcb49d82e1ba
Willian Gustavo Veiga <beberveiga@gmail.com>
2023-02-17 06:56:16 -0300

    Consider contribution graph as well

--- pontoon/contributors/views.py
+++ pontoon/contributors/views.py
@@ -51,7 +51,9 @@ def contributor_username(request, username):
 
 def contributor(request, user):
     """Contributor profile."""
-    graph_data, graph_title = utils.get_contribution_graph_data(user)
+    graph_data, graph_title = utils.get_contribution_graph_data(
+        user, "all_contributions"
+    )
     timeline_data, timeline_title = utils.get_contribution_timeline_data(
         user, "all_contributions"
     )
