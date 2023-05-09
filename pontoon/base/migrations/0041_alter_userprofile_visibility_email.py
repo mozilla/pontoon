bb6d6b9ff153f9bdeb863ce26e02321d9180f82c
Ayanaa Rahman <arahman@Ayanaas-MacBook-Pro.local>
2023-05-09 16:09:35 -0400

    #2824 adding new migration file

--- pontoon/base/migrations/0041_alter_userprofile_visibility_email.py
+++ pontoon/base/migrations/0041_alter_userprofile_visibility_email.py
@@ -6,13 +6,21 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0040_remove_changed_entity_locale_entries_for_repository_projects'),
+        ("base", "0040_remove_changed_entity_locale_entries_for_repository_projects"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='userprofile',
-            name='visibility_email',
-            field=models.CharField(choices=[('Logged-in users', 'Logged-in users'), ('Translators', 'Users with translator rights')], default='Translators', max_length=20, verbose_name='Email address'),
+            model_name="userprofile",
+            name="visibility_email",
+            field=models.CharField(
+                choices=[
+                    ("Logged-in users", "Logged-in users"),
+                    ("Translators", "Users with translator rights"),
+                ],
+                default="Translators",
+                max_length=20,
+                verbose_name="Email address",
+            ),
         ),
     ]
