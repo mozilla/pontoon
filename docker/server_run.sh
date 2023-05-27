b2d29c2ccd5235aa7f8850938bcb69aa3ffbcdc2
Eemeli Aro <eemeli@mozilla.com>
2023-05-27 15:04:03 +0300

    Speed up server stop in development

--- docker/server_run.sh
+++ docker/server_run.sh
@@ -6,4 +6,4 @@ echo ">>> Setting up the db for Django"
 python manage.py migrate
 
 echo ">>> Starting local server"
-python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
