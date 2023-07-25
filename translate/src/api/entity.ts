mozilla/pontoon

commit 1e930cf08466f5c3947fbefbcd4f85e36a4c30a4

Disable Microsoft Terminology service in Machinery (#2900)

translate/src/context/MachineryTranslations.tsx
@@ -113,10 +113,6 @@ export function MachineryProvider({
         }
       }
 
-      if (locale.msTerminologyCode) {
-        fetchMicrosoftTerminology(plain, locale).then(addResults);
-      }
-
       if (locale.code === 'ga-IE' && pk) {
         fetchCaighdeanTranslation(pk).then(addResults);
       }

pontoon/machinery/static/js/machinery.js
@@ -133,10 +133,9 @@ $(function () {
       var listitems = ul.children('li'),
         sourceMap = {
           'Translation memory': 1,
-          Microsoft: 2,
-          'Google Translate': 3,
-          'Systran Translate': 4,
-          'Microsoft Translator': 5,
+          'Google Translate': 2,
+          'Systran Translate': 3,
+          'Microsoft Translator': 4,
         };
 
       function getTranslationSource(el) {
@@ -363,44 +362,6 @@ $(function () {
               source: 'Systran Translate',
               original: original,
               translation: data.translation,
-            });
-          }
-        },
-        error: error,
-        complete: complete,
-      });
-    }
-
-    // Microsoft Terminology
-    if (self.locale.ms_terminology_code.length) {
-      requests++;
-
-      if (self.XHRmicrosoftTerminology) {
-        self.XHRmicrosoftTerminology.abort();
-      }
-
-      self.XHRmicrosoftTerminology = $.ajax({
-        url: '/microsoft-terminology/',
-        data: {
-          text: original,
-          locale: self.locale.ms_terminology_code,
-        },
-        success: function (data) {
-          if (data.translations) {
-            $.each(data.translations, function () {
-              append({
-                url:
-                  'https://www.microsoft.com/Language/en-US/Search.aspx?sString=' +
-                  this.source +
-                  '&langID=' +
-                  self.locale.ms_terminology_code,
-                title:
-                  'Visit Microsoft Terminology Service API.\n' +
-                  '© 2018 Microsoft Corporation. All rights reserved.',
-                source: 'Microsoft',
-                original: this.source,
-                translation: this.target,
-              });
             });
           }
         },

commit 93c7179981fe607c449da8f0402c229588574670

Do not enforce camel case on sibling entity object keys (#2919)
* Do not enforce camel case on sibling entity object keys

* Fix linter issue introduced in #2900

translate/src/api/entity.ts
@@ -148,8 +148,7 @@ export async function fetchSiblingEntities(
   locale: string,
 ): Promise<EntitySiblings> {
   const search = new URLSearchParams({ entity: String(entity), locale });
-  const results = await GET('/get-sibling-entities/', search);
-  return keysToCamelCase(results);
+  return GET('/get-sibling-entities/', search);
 }
 
 export async function fetchEntityHistory(

translate/src/context/MachineryTranslations.tsx
@@ -4,7 +4,6 @@ import {
   abortMachineryRequests,
   fetchCaighdeanTranslation,
   fetchGoogleTranslation,
-  fetchMicrosoftTerminology,
   fetchMicrosoftTranslation,
   fetchSystranTranslation,
   fetchTranslationMemory,
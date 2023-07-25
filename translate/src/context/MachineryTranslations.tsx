93c7179981fe607c449da8f0402c229588574670
Eemeli Aro <eemeli@mozilla.com>
2023-07-26 00:24:26 +0300

    Do not enforce camel case on sibling entity object keys (#2919)
    * Do not enforce camel case on sibling entity object keys
    
    * Fix linter issue introduced in #2900

--- translate/src/context/MachineryTranslations.tsx
+++ translate/src/context/MachineryTranslations.tsx
@@ -4,7 +4,6 @@ import {
   abortMachineryRequests,
   fetchCaighdeanTranslation,
   fetchGoogleTranslation,
-  fetchMicrosoftTerminology,
   fetchMicrosoftTranslation,
   fetchSystranTranslation,
   fetchTranslationMemory,
