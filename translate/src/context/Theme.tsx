dbe5f1920fda890d98250e2eebf2acbece93d1fb
Ayanaa Rahman <ayanaa.rahman@mail.utoronto.ca>
2023-10-16 13:09:41 -0400

    Add system theme switching in translate view

---
+++ translate/src/context/Theme.tsx
@@ -0,0 +1,59 @@
+import { createContext, useEffect, useState } from 'react';
+
+export const ThemeContext = createContext({
+  theme: 'system', 
+  setTheme: (theme: string) => {}, 
+});
+
+function getSystemTheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
+
+export function ThemeProvider({ children }: { children: React.ReactElement }) {
+    console.log("ThemeProvider running.");
+    const [theme, setTheme] = useState(() => {
+      const currentTheme = document.body.getAttribute('data-theme') || 'system';
+      if (currentTheme === 'system') {
+        return getSystemTheme();
+      }
+      return currentTheme;
+    });
+  
+    useEffect(() => {
+      function applyTheme(newTheme: string) {
+        if (newTheme === 'system') {
+          newTheme = getSystemTheme();
+        }
+        document.body.classList.remove('dark-theme', 'light-theme');
+        document.body.classList.add(`${newTheme}-theme`);
+      }
+  
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      function handleThemeChange(e: MediaQueryListEvent) {
+        let userThemeSetting = document.body.getAttribute('data-theme') || 'system';
+  
+        if (userThemeSetting === 'system') {
+          applyTheme(e.matches ? 'dark' : 'light');
+        }
+      }
+  
+      mediaQuery.addEventListener('change', handleThemeChange);
+  
+      applyTheme(theme);
+  
+      return () => {
+        mediaQuery.removeEventListener('change', handleThemeChange);
+      };
+    }, [theme]);
+  
+    return (
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        {children}
+      </ThemeContext.Provider>
+    );
+  }
+  
