e49a349e64c1f50f2cfa0170224ab5d97c2d0df4
Eemeli Aro <eemeli@mozilla.com>
2023-10-23 15:59:00 +0300

    Port migration from SQL to Python

---
+++ pontoon/base/migrations/0049_fix_pluralform_data.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+
+
+def fix_gettext_plural_forms(apps, schema_editor):
+    Translation = apps.get_model("base", "Translation")
+    broken = Translation.objects.filter(plural_form__isnull=True).exclude(
+        entity__string_plural=""
+    )
+    broken.update(plural_form=0)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("base", "0048_userprofile_theme"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=fix_gettext_plural_forms,
+            reverse_code=migrations.RunPython.noop,
+        ),
+    ]
