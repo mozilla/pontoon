7db892a331c060edd7eef48f07a72bae785d7942
Matjaž Horvat <matjaz.horvat@gmail.com>
2023-10-26 13:46:06 +0200

    Restore system theme change event handler

--- translate/src/context/Theme.tsx
+++ translate/src/context/Theme.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { useTheme } from '~/hooks/useTheme';
 
 export const ThemeContext = createContext({
@@ -9,9 +9,26 @@ export function ThemeProvider({ children }: { children: React.ReactElement }) {
   const [theme] = useState(
     () => document.body.getAttribute('data-theme') || 'dark',
   );
+
   const applyTheme = useTheme();
 
-  applyTheme(theme);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    function handleThemeChange(e: MediaQueryListEvent) {
+      let userThemeSetting = document.body.getAttribute('data-theme') || 'dark';
+
+      if (userThemeSetting === 'system') {
+        applyTheme(e.matches ? 'dark' : 'light');
+      }
+    }
+
+    mediaQuery.addEventListener('change', handleThemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleThemeChange);
+    };
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
