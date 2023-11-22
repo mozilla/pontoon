861cbaf7a3e621dfa19d6501c2cf0774e3525a5f
Ayanaa Rahman <ayanaa.rahman@mail.utoronto.ca>
2023-11-22 01:56:12 -0500

    Satisfy python linter

--- pontoon/base/migrations/0051_localecodehistory.py
+++ pontoon/base/migrations/0051_localecodehistory.py
@@ -7,17 +7,30 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0050_strings_xml_apostrophes'),
+        ("base", "0050_strings_xml_apostrophes"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='LocaleCodeHistory',
+            name="LocaleCodeHistory",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('old_code', models.CharField(max_length=20)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('locale', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='base.locale')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("old_code", models.CharField(max_length=20)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "locale",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="base.locale"
+                    ),
+                ),
             ],
         ),
     ]
