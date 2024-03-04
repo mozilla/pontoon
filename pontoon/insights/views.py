e4ab1a60d2cb7021fb172f092481a5e0123c0fe3
Matjaž Horvat <matjaz.horvat@gmail.com>
2024-03-04 13:43:44 +0100

    Drop cache.get_or_set() in pontoon.insights

--- pontoon/insights/views.py
+++ pontoon/insights/views.py
@@ -17,17 +17,27 @@ def insights(request):
     if not settings.ENABLE_INSIGHTS:
         raise ImproperlyConfigured("ENABLE_INSIGHTS variable not set in settings.")
 
-    team_pretranslation_quality = cache.get_or_set(
-        f"/{__name__}/team_pretranslation_quality",
-        get_global_pretranslation_quality("locale", "code"),
-        settings.VIEW_CACHE_TIMEOUT,
-    )
+    # Cannot use cache.get_or_set(), because it always calls the slow function
+    # get_global_pretranslation_quality(). The reason we use cache in first place is to
+    # avoid that.
 
-    project_pretranslation_quality = cache.get_or_set(
-        f"/{__name__}/project_pretranslation_quality",
-        get_global_pretranslation_quality("entity__resource__project", "slug"),
-        settings.VIEW_CACHE_TIMEOUT,
-    )
+    team_pt_key = f"/{__name__}/team_pretranslation_quality"
+    team_pretranslation_quality = cache.get(team_pt_key)
+    if not team_pretranslation_quality:
+        team_pretranslation_quality = get_global_pretranslation_quality(
+            "locale", "code"
+        )
+        cache.set(team_pt_key, team_pretranslation_quality, settings.VIEW_CACHE_TIMEOUT)
+
+    project_pt_key = f"/{__name__}/project_pretranslation_quality"
+    project_pretranslation_quality = cache.get(project_pt_key)
+    if not project_pretranslation_quality:
+        project_pretranslation_quality = get_global_pretranslation_quality(
+            "entity__resource__project", "slug"
+        )
+        cache.set(
+            project_pt_key, project_pretranslation_quality, settings.VIEW_CACHE_TIMEOUT
+        )
 
     return render(
         request,
