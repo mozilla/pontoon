4a506843f9da734bf0b37d556e3a6a46e5cf6883
Ayanaa Rahman <ayanaa.rahman@mail.utoronto.ca>
2024-03-28 17:10:21 -0400

    Adding LLM field to model

--- pontoon/base/models.py
+++ pontoon/base/models.py
@@ -3262,6 +3262,7 @@ class Translation(DirtyFieldsMixin, models.Model):
         SYSTRAN_TRANSLATE = "systran-translate", "Systran Translate"
         MICROSOFT_TERMINOLOGY = "microsoft-terminology", "Microsoft"
         CAIGHDEAN = "caighdean", "Caighdean"
+        LLM = "gpt-transform", "GPT Transform"
 
     machinery_sources = ArrayField(
         models.CharField(max_length=30, choices=MachinerySource.choices),
