b5f6692ff4a49c749d5d24d44145e9d98a353f91
Matjaž Horvat <matjaz.horvat@gmail.com>
2024-04-09 18:48:54 +0200

    Fix jinja after update

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -323,9 +323,7 @@ TEMPLATES = [
             "extensions": [
                 "jinja2.ext.do",
                 "jinja2.ext.loopcontrols",
-                "jinja2.ext.with_",
                 "jinja2.ext.i18n",
-                "jinja2.ext.autoescape",
                 "django_jinja.builtins.extensions.CsrfExtension",
                 "django_jinja.builtins.extensions.CacheExtension",
                 "django_jinja.builtins.extensions.TimezoneExtension",
