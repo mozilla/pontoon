ae8a742f81fb43e97bf17be209eb1298ce562ba5
Harmit Goswami <hgoswami@macbook-pro.home>
2024-06-24 17:55:49 -0400

    Modified test case for contribution_timeline_data

--- pontoon/contributors/tests/test_utils.py
+++ pontoon/contributors/tests/test_utils.py
@@ -300,7 +300,7 @@ def test_get_contribution_timeline_data_with_actions(
                         },
                         "action_dates": ["1 approved"],
                         "count": 1,
-                        "actions_month": {current_month: ["1 approved"]},
+                        "actions_month": {},
                         "url": f"/kg/project_a/all-resources/?{urlencode(params)}",
                     },
                 },
