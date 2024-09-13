4f11d0b05794aaa69ea23a8272acd74b5738cd83
Felix Staud <felix.staud@headwire.com>
2024-09-13 14:28:14 -0700

    add i18next format to commonMode
    in editFieldModes.ts

--- translate/src/modules/translationform/utils/editFieldModes.ts
+++ translate/src/modules/translationform/utils/editFieldModes.ts
@@ -88,12 +88,15 @@ const printf =
 
 const pythonFormat = /^{[\w.[\]]*(![rsa])?(:.*?)?}/;
 
+// https://www.i18next.com/translation-function/interpolation
+const i18nextFormat = /{{\s*[\w.[\]]*\s*(?:,\s*\w+(?:\([^)]*\))?)*\s*(?:![rsa])?(?::.*?)?\s*}}/;
+
 export const commonMode: StreamParser<Array<'literal' | 'tag'>> = {
   name: 'common',
   languageData: { closeBrackets: { brackets: ['(', '[', '{', '"', '<'] } },
   startState: () => [],
   token(stream, state) {
-    if (stream.match(printf) || stream.match(pythonFormat)) {
+    if (stream.match(printf) || stream.match(pythonFormat) || stream.match(i18nextFormat)) {
       return 'keyword';
     }
 
