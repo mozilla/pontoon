db8f252386a4175094c3423b8cadd69d19165bff
Harmit Goswami <hgoswami@harmits-mbp.home>
2024-09-19 14:09:23 -0400

    Fixed discard bugs

--- translate/src/modules/search/components/SearchBox.tsx
+++ translate/src/modules/search/components/SearchBox.tsx
@@ -275,6 +275,7 @@ export function SearchBoxBase({
         dispatch(resetEntities());
         parameters.push({
           ...parameters, // Persist all other variables to next state
+          search,
           search_identifiers: search_identifiers,
           search_exclude_source_strings: search_exclude_source_strings,
           search_rejected_translations: search_rejected_translations,
@@ -386,7 +387,6 @@ export function SearchBoxBase({
         searchOptions={searchOptions}
         applyOptions={applyOptions}
         toggleOption={toggleOption}
-        updateOptionsFromURL={updateOptionsFromURL}
       />
     </div>
   );
