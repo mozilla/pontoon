7491223ec1edaaa689eacbb680a42fd4758ccfe4
Francesco Lodolo <flod@lodolo.net>
2024-09-27 15:14:48 +0200

    Update configuration

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ release = '1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -77,7 +77,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'venv']
+exclude_patterns = ['_build', '.venv']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
