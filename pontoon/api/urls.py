9f4cc6f9b626c20dfacdf0e78f1fcaa8e028261f
Matjaž Horvat <matjaz.horvat@gmail.com>
2024-09-30 16:02:28 +0200

    Explicitly support URLs with or without trailing slash in order to support curl requests

--- pontoon/api/urls.py
+++ pontoon/api/urls.py
@@ -1,15 +1,16 @@
 from graphene_django.views import GraphQLView
 
-from django.urls import path
+from django.urls import re_path
 
 from pontoon.api.schema import schema
 from pontoon.settings import DEV
 
 
 urlpatterns = [
-    # GraphQL endpoint. In DEV mode it serves the GraphiQL IDE if accessed with Accept: text/html
-    path(
-        "graphql/",
+    # GraphQL endpoint. In DEV mode it serves the GraphiQL IDE if accessed with Accept: text/html.
+    # Explicitly support URLs with or without trailing slash in order to support curl requests.
+    re_path(
+        r"^graphql/?$",
         GraphQLView.as_view(schema=schema, graphiql=DEV),
     ),
 ]
