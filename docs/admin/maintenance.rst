8ed6f69dfeaed65c5d33e3fb6adf7aace412cf39
Matjaž Horvat <matjaz.horvat@gmail.com>
2024-10-13 23:48:50 +0200

    Add traffic throttling middleware (#3402)

--- docs/admin/maintenance.rst
+++ docs/admin/maintenance.rst
@@ -47,8 +47,12 @@ In a distributed denial-of-service attack (`DDoS`_ attack), the incoming traffic
 flooding the victim originates from many different sources. This stops everyone
 else from accessing the website as there is too much traffic flowing to it.
 
-One way to mitigate DDoS attacks is to identify the IP addresses of the
-attackers (see the handy `IP detection script`_ to help with that) and block them.
+One way to mitigate DDoS attacks is to enable traffic throttling. Set the
+`THROTTLE_ENABLED` environment variable to True and configure other THROTTLE*
+variables to limit the number of requests per period from a single IP address.
+
+A more involved but also more controlled approach is to identify the IP addresses of
+the attackers (see the handy `IP detection script`_ to help with that) and block them.
 Find the attacking IP addresses in the Log Management Add-On (Papertrail)
 and add them to the BLOCKED_IPs config variable in Heroku Settings.
 
--- pontoon/base/middleware.py
+++ pontoon/base/middleware.py
@@ -1,17 +1,22 @@
 import logging
+import time
 
 from ipaddress import ip_address
 
 from raygun4py.middleware.django import Provider
 
 from django.conf import settings
+from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.http import Http404, HttpResponseForbidden
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
 
-from pontoon.base.utils import is_ajax
+from pontoon.base.utils import get_ip, is_ajax
+
+
+log = logging.getLogger(__name__)
 
 
 class RaygunExceptionMiddleware(Provider, MiddlewareMixin):
@@ -28,15 +33,7 @@ class RaygunExceptionMiddleware(Provider, MiddlewareMixin):
 
 class BlockedIpMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        try:
-            ip = request.META["HTTP_X_FORWARDED_FOR"]
-            # If comma-separated list of IPs, take just the last one
-            # http://stackoverflow.com/a/18517550
-            ip = ip.split(",")[-1]
-        except KeyError:
-            ip = request.META["REMOTE_ADDR"]
-
-        ip = ip.strip()
+        ip = get_ip(request)
 
         # Block client IP addresses via settings variable BLOCKED_IPS
         if ip in settings.BLOCKED_IPS:
@@ -47,7 +44,6 @@ class BlockedIpMiddleware(MiddlewareMixin):
         try:
             ip_obj = ip_address(ip)
         except ValueError:
-            log = logging.getLogger(__name__)
             log.error(f"Invalid IP detected in BlockedIpMiddleware: {ip}")
             return None
 
@@ -83,3 +79,60 @@ class EmailConsentMiddleware:
 
         request.session["next_path"] = request.get_full_path()
         return redirect(email_consent_url)
+
+
+class ThrottleIpMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.max_count = settings.THROTTLE_MAX_COUNT
+        self.observation_period = settings.THROTTLE_OBSERVATION_PERIOD
+        self.block_duration = settings.THROTTLE_BLOCK_DURATION
+
+        # Set to block_duration if longer, otherwise requests will be blocked until
+        # the observation_period expires rather than when the block_duration expires
+        if self.observation_period > self.block_duration:
+            self.observation_period = self.block_duration
+
+    def _throttle(self, request):
+        response = render(request, "429.html", status=429)
+        response["Retry-After"] = self.block_duration
+        return response
+
+    def __call__(self, request):
+        if settings.THROTTLE_ENABLED is False:
+            return self.get_response(request)
+
+        ip = get_ip(request)
+
+        # Generate cache keys
+        observed_key = f"observed_ip_{ip}"
+        blocked_key = f"blocked_ip_{ip}"
+
+        # Check if IP is currently blocked
+        if cache.get(blocked_key):
+            return self._throttle(request)
+
+        # Fetch current request count and timestamp
+        request_data = cache.get(observed_key)
+        now = time.time()
+
+        if request_data:
+            request_count, first_request_time = request_data
+            if request_count >= self.max_count:
+                # Block further requests for block_duration seconds
+                cache.set(blocked_key, True, self.block_duration)
+                log.error(f"Blocked IP {ip} for {self.block_duration} seconds")
+                return self._throttle(request)
+            else:
+                # Increment the request count and update cache
+                cache.set(
+                    observed_key,
+                    (request_count + 1, first_request_time),
+                    self.observation_period,
+                )
+        else:
+            # Reset the count and timestamp if first request in the period
+            cache.set(observed_key, (1, now), self.observation_period)
+
+        response = self.get_response(request)
+        return response
