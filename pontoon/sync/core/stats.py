68f94f73c5b818a57e34daeeede1c1d95bc7e034
Eemeli Aro <eemeli@mozilla.com>
2024-10-17 10:18:39 +0300

    Use new update_stats() for `manage.py calculate_stats` command

--- pontoon/sync/core/stats.py
+++ pontoon/sync/core/stats.py
@@ -10,7 +10,7 @@ from pontoon.base.models import Project
 log = logging.getLogger(__name__)
 
 
-def update_stats(project: Project) -> None:
+def update_stats(project: Project, *, update_locales: bool = True) -> None:
     """Uses raw SQL queries for performance."""
 
     with connection.cursor() as cursor:
@@ -117,7 +117,7 @@ def update_stats(project: Project) -> None:
             dedent(
                 """
                 UPDATE base_project proj
-                SET total_strings = agg.total
+                SET total_strings = GREATEST(agg.total, 0)
                 FROM (
                     SELECT SUM(total_strings) AS "total"
                     FROM "base_resource"
@@ -133,11 +133,11 @@ def update_stats(project: Project) -> None:
                 """
                 UPDATE base_project proj
                 SET
-                    approved_strings = agg.approved,
-                    pretranslated_strings = agg.pretranslated,
-                    strings_with_errors = agg.errors,
-                    strings_with_warnings = agg.warnings,
-                    unreviewed_strings = agg.unreviewed
+                    approved_strings = GREATEST(agg.approved, 0),
+                    pretranslated_strings = GREATEST(agg.pretranslated, 0),
+                    strings_with_errors = GREATEST(agg.errors, 0),
+                    strings_with_warnings = GREATEST(agg.warnings, 0),
+                    unreviewed_strings = GREATEST(agg.unreviewed, 0)
                 FROM (
                     SELECT
                         SUM(tr.approved_strings) AS "approved",
@@ -155,38 +155,57 @@ def update_stats(project: Project) -> None:
             [project.id, project.id],
         )
 
-        # All locales, counted from project locales
-        cursor.execute(
-            dedent(
-                """
-                UPDATE base_locale loc
-                SET
-                    total_strings = agg.total,
-                    approved_strings = agg.approved,
-                    pretranslated_strings = agg.pretranslated,
-                    strings_with_errors = agg.errors,
-                    strings_with_warnings = agg.warnings,
-                    unreviewed_strings = agg.unreviewed
-                FROM (
-                    SELECT
-                        pl.locale_id AS "locale_id",
-                        SUM(pl.total_strings) AS "total",
-                        SUM(pl.approved_strings) AS "approved",
-                        SUM(pl.pretranslated_strings) AS "pretranslated",
-                        SUM(pl.strings_with_errors) AS "errors",
-                        SUM(pl.strings_with_warnings) AS "warnings",
-                        SUM(pl.unreviewed_strings) AS "unreviewed"
-                    FROM "base_projectlocale" pl
-                    INNER JOIN "base_project" proj ON (pl.project_id = proj.id)
-                    WHERE NOT proj.disabled AND NOT proj.system_project AND proj.visibility = 'public'
-                    GROUP BY pl.locale_id
-                ) AS agg
-                WHERE agg.locale_id = loc.id
-                """
-            )
-        )
-        lc_count = cursor.rowcount
+        lc_count = _update_locales(cursor) if update_locales else 0
 
-    log.info(
-        f"[{project.slug}] Updated stats for {tr_count} translated resources, {pl_count} projectlocales, and {lc_count} locales"
+    tr_str = (
+        "1 translated resource" if tr_count == 1 else f"{tr_count} translated resources"
     )
+    pl_str = "1 projectlocale" if pl_count == 1 else f"{pl_count} projectlocales"
+    lc_str = "1 locale" if lc_count == 1 else f"{lc_count} locales"
+    summary = (
+        f"{tr_str} and {pl_str}"
+        if lc_count == 0
+        else f"{tr_str}, {pl_str}, and {lc_str}"
+    )
+    log.info(f"[{project.slug}] Updated stats for {summary}")
+
+
+def update_locale_stats() -> None:
+    with connection.cursor() as cursor:
+        lc_count = _update_locales(cursor)
+    lc_str = "1 locale" if lc_count == 1 else f"{lc_count} locales"
+    log.info(f"Updated stats for {lc_str}")
+
+
+def _update_locales(cursor) -> int:
+    # All locales, counted from project locales
+    cursor.execute(
+        dedent(
+            """
+            UPDATE base_locale loc
+            SET
+                total_strings = agg.total,
+                approved_strings = agg.approved,
+                pretranslated_strings = agg.pretranslated,
+                strings_with_errors = agg.errors,
+                strings_with_warnings = agg.warnings,
+                unreviewed_strings = agg.unreviewed
+            FROM (
+                SELECT
+                    pl.locale_id AS "locale_id",
+                    SUM(pl.total_strings) AS "total",
+                    SUM(pl.approved_strings) AS "approved",
+                    SUM(pl.pretranslated_strings) AS "pretranslated",
+                    SUM(pl.strings_with_errors) AS "errors",
+                    SUM(pl.strings_with_warnings) AS "warnings",
+                    SUM(pl.unreviewed_strings) AS "unreviewed"
+                FROM "base_projectlocale" pl
+                INNER JOIN "base_project" proj ON (pl.project_id = proj.id)
+                WHERE NOT proj.disabled AND NOT proj.system_project AND proj.visibility = 'public'
+                GROUP BY pl.locale_id
+            ) AS agg
+            WHERE agg.locale_id = loc.id
+            """
+        )
+    )
+    return cursor.rowcount
