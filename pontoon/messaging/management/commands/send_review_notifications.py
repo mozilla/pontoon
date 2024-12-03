6273ecb07a2523030ad9c898b261ba713979005e
Matjaž Horvat <matjaz.horvat@gmail.com>
2024-12-03 21:55:15 +0100

    Use full URLs in notifications (#3468)
    Some notification types (suggestions available for review, suggestions have been reviewed and new contributor has joined the team) use relative URLs. That means they won't work in emails. This patch fixes that.
    
    It also:
    * Moves notification content from multiline strings to HTML templates.
    * Moves management commands for sending notifications to the messaging module.
    * Moves notification template files to the messaging module.

--- pontoon/messaging/management/commands/send_review_notifications.py
+++ pontoon/messaging/management/commands/send_review_notifications.py
@@ -5,7 +5,7 @@ from notifications.signals import notify
 
 from django.core.management.base import BaseCommand
 from django.db.models import Q
-from django.urls import reverse
+from django.template.loader import render_to_string
 from django.utils import timezone
 
 from pontoon.base.models import Translation
@@ -13,37 +13,6 @@ from pontoon.base.models import Translation
 
 class Command(BaseCommand):
     help = "Notify translators about their newly reviewed suggestions"
-
-    def get_description(self, notifyData):
-        desc = "Your suggestions have been reviewed:\n<ul>"
-
-        for (locale, project), (approved, rejected) in notifyData.items():
-            url = reverse(
-                "pontoon.translate",
-                kwargs={
-                    "locale": locale.code,
-                    "project": project.slug,
-                    "resource": "all-resources",
-                },
-            )
-            list = map(str, approved + rejected)
-            url += "?list=" + ",".join(list)
-
-            # Filter out rejections where the author's own suggestion replaced the previous
-            rejected = [x for x in rejected if x not in approved]
-
-            if len(approved) == 0:
-                msg = f"{len(rejected)} Rejected"
-            else:
-                msg = f"{len(approved)} Approved"
-                if len(rejected) > 0:
-                    msg += f", {len(rejected)} Rejected"
-
-            desc += (
-                f'\n<li><a href="{url}">{project.name} ({locale.code})</a>: {msg}</li>'
-            )
-
-        return desc + "\n</ul>"
 
     def handle(self, *args, **options):
         """
@@ -57,6 +26,7 @@ class Command(BaseCommand):
         # (author) -> (locale, project) -> (approved, rejected)
         data = defaultdict(lambda: defaultdict(lambda: (list(), list())))
         start = timezone.now() - timedelta(days=1)
+
         for suggestion in Translation.objects.filter(
             (Q(approved_date__gt=start) | Q(rejected_date__gt=start))
             & Q(user__profile__review_notifications=True)
@@ -70,13 +40,41 @@ class Command(BaseCommand):
             elif suggestion.rejected and suggestion.rejected_user != author:
                 data[author][(locale, project)][1].append(suggestion.entity.pk)
 
-        for author, notifyData in data.items():
-            desc = self.get_description(notifyData)
+        for author, notification_data in data.items():
+            notifications = []
+
+            for (locale, project), (approved, rejected) in notification_data.items():
+                # Filter out rejections where the author's own suggestion replaced the previous
+                rejected = [id for id in rejected if id not in approved]
+
+                if len(approved) == 0:
+                    msg = f"{len(rejected)} Rejected"
+                else:
+                    msg = f"{len(approved)} Approved"
+                    if len(rejected) > 0:
+                        msg += f", {len(rejected)} Rejected"
+
+                notifications.append(
+                    {
+                        "locale": locale,
+                        "project": project,
+                        "ids": ",".join(map(str, approved + rejected)),
+                        "msg": msg,
+                    }
+                )
+
+            description = render_to_string(
+                "messaging/notifications/suggestions_reviewed.html",
+                {
+                    "notifications": notifications,
+                },
+            )
+
             notify.send(
                 sender=author,
                 recipient=author,
                 verb="has reviewed suggestions",
-                description=desc,
+                description=description,
                 category="review",
             )
 
