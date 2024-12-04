af296bc9315fd5d499f0c12d706b2758e19f662b
Harmit Goswami <hgoswami@harmits-mbp.home>
2024-12-04 13:12:55 -0500

    Fixed failing test case

--- pontoon/messaging/notifications.py
+++ pontoon/messaging/notifications.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 
 def send_badge_notification(user, badge, level):
     desc = render_to_string(
-        "messaging/badge_notification.html",
+        "messaging/notifications/badge_notification.html",
         {"badge": badge, "level": level, "user": user},
     )
     notify.send(
