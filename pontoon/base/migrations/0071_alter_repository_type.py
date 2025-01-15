4de659988a58f0bbf96936645b33e1cd69b396fa
Eemeli Aro <eemeli@gmail.com>
2025-01-15 10:23:45 +0200

    Satisfy ruff

--- pontoon/base/migrations/0071_alter_repository_type.py
+++ pontoon/base/migrations/0071_alter_repository_type.py
@@ -4,15 +4,16 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('base', '0070_userprofile_transactional_emails'),
+        ("base", "0070_userprofile_transactional_emails"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='repository',
-            name='type',
-            field=models.CharField(choices=[('git', 'Git'), ('hg', 'HG')], default='git', max_length=255),
+            model_name="repository",
+            name="type",
+            field=models.CharField(
+                choices=[("git", "Git"), ("hg", "HG")], default="git", max_length=255
+            ),
         ),
     ]
