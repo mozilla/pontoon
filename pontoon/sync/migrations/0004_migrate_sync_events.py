729cc7166411e79b32f08d250a10458ddd80249d
Eemeli Aro <eemeli@mozilla.com>
2025-01-22 12:34:18 +0200

    Refactor sync status logging (#3529)
    Here, sync status logging is collapsed into a single table of events, which is written to only from sync_project_task(), with the following states:
    
    * IN_PROGRESS
    * DONE - sync complete, with some changes to DB or repo
    * NO_CHANGES - sync complete, with no changed to DB or repo
    * NO_COMMIT - sync complete, but commit=False was set while a repo write should've occurred
    * PREV_BUSY - skipped due to previous sync still running
    * FAIL - an exception was raised
    * INCOMPLETE - sync never completed; set by a subsequent successful run
    
    For FAIL, an error message is retained in the error field.
    
    The logs are made available via the following views:
    
    * /sync - latest sync status and start time for each project
    * /sync/projects/<project> - sync events for each project
    * /sync/errors - all anomalous sync events with a status other than DONE or NO_CHANGES
    
    Old sync events are ported to the new format, setting the status as NO_CHANGES for all that were "skipped" and INCOMPLETE for ones with no end time set.
    
    Also included:
    * Do not clear ChangedEntityLocale during a sync with commit=False
    * Add Sync Log menu entry to the Profile menu
    
    Co-authored-by: Matjaž Horvat <matjaz.horvat@gmail.com>

---
+++ pontoon/sync/migrations/0004_migrate_sync_events.py
@@ -0,0 +1,43 @@
+from django.db import migrations
+
+
+def migrate_sync_events(apps, schema_editor):
+    Sync = apps.get_model("sync", "Sync")
+    ProjectSyncLog = apps.get_model("sync", "ProjectSyncLog")
+
+    sync_events = []
+    for prev in ProjectSyncLog.objects.all().prefetch_related("repository_sync_logs"):
+        sync = Sync(project=prev.project, start_time=prev.start_time)
+        if prev.skipped_end_time:
+            sync.status = 2  # Sync.Status.NO_CHANGE, though some may have been FAIL
+            sync.end_time = prev.skipped_end_time
+        else:
+            repo_logs = prev.repository_sync_logs.all()
+            if repo_logs and all(log.end_time is not None for log in repo_logs):
+                sync.status = 1  # Sync.Status.DONE
+                sync.end_time = max(log.end_time for log in repo_logs)
+            else:
+                sync.status = -2  # Sync.Status.INCOMPLETE
+        sync_events.append(sync)
+
+    Sync.objects.bulk_create(sync_events, batch_size=10000)
+
+
+def drop_sync_events(apps, schema_editor):
+    Sync = apps.get_model("sync", "Sync")
+    Sync.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = [
+        ("sync", "0003_new_sync_events"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=migrate_sync_events,
+            reverse_code=drop_sync_events,
+        )
+    ]
