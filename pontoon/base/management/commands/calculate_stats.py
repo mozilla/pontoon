0e5627208d164818ae6ae6d3443578efee9868ac
Francesco Lodolo <flodolo@mozilla.com>
2025-03-31 15:00:21 +0200

    Calculate stats only on enabled projects by default, add `-all` option (#3609)

--- pontoon/base/management/commands/calculate_stats.py
+++ pontoon/base/management/commands/calculate_stats.py
@@ -26,14 +26,26 @@ class Command(BaseCommand):
         See bug 1470337 for more details.
         """
 
-    def handle(self, *args, **options):
-        # Start with enabled projects in ascending order of resource count
-        projects = Project.objects.annotate(resource_count=Count("resources")).order_by(
-            "disabled", "resource_count"
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--all",
+            action="store_true",
+            help="Run on all projects, including disabled",
         )
 
-        log.info(f"Calculating stats for {len(projects)} projects...")
+    def handle(self, *args, **options):
+        if options["all"]:
+            projects = Project.objects.all()
+            action_txt = "all projects"
+        else:
+            projects = Project.objects.filter(disabled=False)
+            action_txt = "enabled projects"
+        projects = projects.annotate(resource_count=Count("resources")).order_by(
+            "disabled", "resource_count"
+        )
+        log.info(f"Calculating stats for {action_txt} ({len(projects)})...")
+
         for project in projects:
             update_stats(project)
 
-        log.info("Calculating stats complete for all projects.")
+        log.info("Calculating stats complete.")
