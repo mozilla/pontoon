33dc5c4003b044a7f24926f039bb061d623beef9
rafaeljohn9 <rafaeljohb@gmail.com>
2025-05-02 15:41:17 +0300

    Fix: Linting Errors: Prettier
    Signed-off-by: rafaeljohn9 <rafaeljohb@gmail.com>

--- translate/src/modules/editor/components/WarningFailedChecks.tsx
+++ translate/src/modules/editor/components/WarningFailedChecks.tsx
@@ -7,29 +7,29 @@ import './FailedChecks.css';
  * Displays a list of failed checks (errors and warnings) without any actions.
  */
 export function WarningFailedChecks(): null | React.ReactElement<'div'> {
-    const { errors, warnings } = useContext(FailedChecksData);
+  const { errors, warnings } = useContext(FailedChecksData);
 
-    if (!errors.length && !warnings.length) {
-        return null;
-    }
+  if (!errors.length && !warnings.length) {
+    return null;
+  }
 
-    return (
-        <div className='failed-checks'>
-            <Localized id='editor-FailedChecks--title'>
-                <p className='title'>THE FOLLOWING CHECKS HAVE FAILED</p>
-            </Localized>
-            <ul>
-                {errors.map((error, key) => (
-                    <li className='error' key={key}>
-                        {error}
-                    </li>
-                ))}
-                {warnings.map((warning, key) => (
-                    <li className='warning' key={key}>
-                        {warning}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className='failed-checks'>
+      <Localized id='editor-FailedChecks--title'>
+        <p className='title'>THE FOLLOWING CHECKS HAVE FAILED</p>
+      </Localized>
+      <ul>
+        {errors.map((error, key) => (
+          <li className='error' key={key}>
+            {error}
+          </li>
+        ))}
+        {warnings.map((warning, key) => (
+          <li className='warning' key={key}>
+            {warning}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
