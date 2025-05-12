eae99b8470b612bbb1183059747724dffcc3fbd2
Jamie Hu <jahu@mozilla.com>
2025-05-12 12:11:28 -0400

    added same date formatting to the notifications menu in the Translate app

--- translate/src/modules/user/components/UserNotification.tsx
+++ translate/src/modules/user/components/UserNotification.tsx
@@ -1,6 +1,11 @@
 import classNames from 'classnames';
 import parse from 'html-react-parser';
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  BlockquoteHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 // @ts-expect-error Working types are unavailable for react-linkify 0.2.2
 import Linkify from 'react-linkify';
 import ReactTimeAgo from 'react-time-ago';
@@ -11,6 +16,36 @@ type Props = {
   notification: Record<string, any>;
 };
 
+interface DateDisplayProps {
+  date_iso: string;
+}
+
+const DateDisplay: React.FC<DateDisplayProps> = ({ date_iso }) => {
+  const date: Date = new Date(date_iso);
+  const formattedDate: string = date.toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const isOld: boolean =
+    new Date().getTime() - date.getTime() > 7 * 24 * 60 * 60 * 1000;
+  return (
+    <>
+      {isOld ? (
+        <div className='date-display'>
+          <time dateTime={formattedDate}>{formattedDate}</time>
+        </div>
+      ) : (
+        <ReactTimeAgo
+          className='timeago'
+          date={new Date(date_iso)}
+          title={`${formattedDate} UTC`}
+        />
+      )}
+    </>
+  );
+};
+
 const Suggestion = ({ date, date_iso, description }: Props['notification']) => (
   <div className='item-content'>
     <span
@@ -19,11 +54,7 @@ const Suggestion = ({ date, date_iso, description }: Props['notification']) => (
       dangerouslySetInnerHTML={{ __html: description.content }}
     />
 
-    <ReactTimeAgo
-      className='timeago'
-      date={new Date(date_iso)}
-      title={`${date} UTC`}
-    />
+    <DateDisplay date_iso={date_iso} />
   </div>
 );
 
@@ -44,12 +75,6 @@ const Comment = ({
 
     <span className='target'>{target.anchor}</span>
 
-    <ReactTimeAgo
-      className='timeago'
-      date={new Date(date_iso)}
-      title={`${date} UTC`}
-    />
-
     <div className='message trim'>
       <Linkify properties={{ target: '_blank', rel: 'noopener noreferrer' }}>
         {
@@ -62,6 +87,8 @@ const Comment = ({
         }
       </Linkify>
     </div>
+
+    <DateDisplay date_iso={date_iso} />
   </div>
 );
 
@@ -88,12 +115,6 @@ const Other = ({
       </span>
     )}
 
-    <ReactTimeAgo
-      className='timeago'
-      date={new Date(date_iso)}
-      title={`${date} UTC`}
-    />
-
     {description ? (
       <div
         className='message'
@@ -104,6 +125,8 @@ const Other = ({
         dangerouslySetInnerHTML={{ __html: description.content }}
       />
     ) : null}
+
+    <DateDisplay date_iso={date_iso} />
   </div>
 );
 
