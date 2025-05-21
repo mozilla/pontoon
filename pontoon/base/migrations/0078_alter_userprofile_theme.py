5566728ade40bc3cf3b572448513ad5931a051c7
functionzz <164675620+functionzz@users.noreply.github.com>
2025-05-21 08:36:27 -0400

    merged another migration file

--- pontoon/base/migrations/0078_alter_userprofile_theme.py
+++ pontoon/base/migrations/0078_alter_userprofile_theme.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("base", "0076_remove_pontoon_intro_project"),
+        ("base", "0077_fix_android_newlines"),
     ]
 
     operations = [
