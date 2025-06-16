49dec89676c1241d8d0ba0cabb716e33f8a99022
functionzz <164675620+functionzz@users.noreply.github.com>
2025-06-16 15:35:22 -0400

    Setup for default page pagination
    Each Page size is set to 100 by default. Can be overwritten with custom Pagination classes.

--- pontoon/settings/dev.py
+++ pontoon/settings/dev.py
@@ -47,4 +47,5 @@ GRAPHENE = {"MIDDLEWARE": ["graphene_django.debug.DjangoDebugMiddleware"]}
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
