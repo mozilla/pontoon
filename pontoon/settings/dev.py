f83beb629e13c2edb701ba2369fb3e4bc1b6f1b2
functionzz <164675620+functionzz@users.noreply.github.com>
2025-06-16 15:54:12 -0400

    ran make format

--- pontoon/settings/dev.py
+++ pontoon/settings/dev.py
@@ -47,6 +47,6 @@ GRAPHENE = {"MIDDLEWARE": ["graphene_django.debug.DjangoDebugMiddleware"]}
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
