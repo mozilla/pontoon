63295487b99c98a7b5f8d38a1e6bde30450478e2
functionzz <164675620+functionzz@users.noreply.github.com>
2025-06-16 15:37:49 -0400

    forgot to add this line

--- pontoon/settings/dev.py
+++ pontoon/settings/dev.py
@@ -48,6 +48,7 @@ GRAPHENE = {"MIDDLEWARE": ["graphene_django.debug.DjangoDebugMiddleware"]}
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 if base.DJANGO_DEBUG_TOOLBAR:
