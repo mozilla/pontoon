68193450f05f353550ba3b8565639e26b00a4642
functionzz <164675620+functionzz@users.noreply.github.com>
2025-06-23 19:26:19 -0400

    /api/v2 root added, removed MultipleFieldMixin, changed url pattern format and order

--- pontoon/api/urls.py
+++ pontoon/api/urls.py
@@ -7,6 +7,50 @@ from pontoon.api import views
 from pontoon.api.schema import schema
 
 
+api_v2_patterns = [
+    path(
+        # Locales
+        "locales/",
+        views.LocaleListView.as_view(),
+        name="locale-list",
+    ),
+    path(
+        # Locale
+        "locales/<str:code>/",
+        views.LocaleIndividualView.as_view(),
+        name="locale-individual",
+    ),
+    path(
+        # Projects
+        "projects/",
+        views.ProjectListView.as_view(),
+        name="project-list",
+    ),
+    path(
+        # Project
+        "projects/<str:slug>/",
+        views.ProjectIndividualView.as_view(),
+        name="project-individual",
+    ),
+    path(
+        # Terminology Search
+        "search/terminology/",
+        views.TermSearchListView.as_view(),
+        name="term-search",
+    ),
+    path(
+        # Translation Memory Search
+        "search/tm/",
+        views.TranslationMemorySearchListView.as_view(),
+        name="translation-memory-search",
+    ),
+    path(
+        # ProjectLocale
+        "<str:code>/<str:slug>/",
+        views.ProjectLocaleIndividualView.as_view(),
+        name="project-locale-individual",
+    ),
+]
 urlpatterns = [
     # GraphQL endpoint. Serves the GraphiQL IDE if accessed with Accept: text/html.
     # Explicitly support URLs with or without trailing slash in order to support curl requests.
@@ -14,55 +58,9 @@ urlpatterns = [
         r"^graphql/?$",
         csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=True)),
     ),
-    path(
-        "api/v2/",
-        include(
-            [
-                path(
-                    # Locales
-                    "locales/",
-                    views.LocaleListView.as_view(),
-                    name="locale-list",
-                ),
-                path(
-                    # Locale
-                    "locales/<str:code>/",
-                    views.LocaleIndividualView.as_view(),
-                    name="locale-individual",
-                ),
-                path(
-                    # Projects
-                    "projects/",
-                    views.ProjectListView.as_view(),
-                    name="project-list",
-                ),
-                path(
-                    # Project
-                    "projects/<str:slug>/",
-                    views.ProjectIndividualView.as_view(),
-                    name="project-individual",
-                ),
-                path(
-                    # ProjectLocale
-                    "<str:code>/<str:slug>/",
-                    views.ProjectLocaleIndividualView.as_view(),
-                    name="project-locale-individual",
-                ),
-                path(
-                    # Terminology Search
-                    "search/terminology/",
-                    views.TermSearchListView.as_view(),
-                    name="term-search",
-                ),
-                path(
-                    # Translation Memory Search
-                    "search/tm/",
-                    views.TranslationMemorySearchListView.as_view(),
-                    name="translation-memory-search",
-                ),
-            ]
-        ),
-    ),
+    # REST API
+    path("api/v2/", views.APIV2RootView.as_view(), name="api-root"),
+    path("api/v2/", include(api_v2_patterns)),
     # API v1
     path(
         "api/v1/",
