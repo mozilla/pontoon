b864118e713a97eb8b1918714103902998024d02
functionzz <164675620+functionzz@users.noreply.github.com>
2025-06-23 19:26:19 -0400

    /api/v2 root added, removed MultipleFieldMixin, changed url pattern format and order

--- pontoon/api/views.py
+++ pontoon/api/views.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -122,22 +124,19 @@ def get_user_actions(request, date, slug):
     )
 
 
-class MultipleFieldLookupMixin:
-    """
-    Apply this mixin to any view or viewset to get multiple field filtering
-    based on a `lookup_fields` attribute, instead of the default single field filtering.
-    """
-
-    def get_object(self):
-        queryset = self.get_queryset()  # Get the base queryset
-        queryset = self.filter_queryset(queryset)  # Apply any filter backends
-        filter = {}
-        for field in self.lookup_fields:
-            if self.kwargs.get(field):  # Ignore empty fields.
-                filter[field] = self.kwargs[field]
-        obj = generics.get_object_or_404(queryset, **filter)  # Lookup the object
-        # self.check_object_permissions(self.request, obj)
-        return obj
+class APIV2RootView(APIView):
+    def get(self, request, format=None):
+        return Response(
+            [
+                "locales/",
+                "locales/{code}/",
+                "projects/?include_disabled&include_system",
+                "projects/{slug}/",
+                "{code}/{slug}/",
+                "search/terminology/?locale=LOCALECODE&text=TERMTEXT",
+                "search/tm/?locale=LOCALECODE&text=TMTEXT",
+            ]
+        )
 
 
 class LocaleListView(generics.ListAPIView):
