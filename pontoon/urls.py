d2c63779dfe5ee95063bd54a494d1103572aa411
Matjaž Horvat <matjaz.horvat@gmail.com>
2025-06-30 19:15:43 +0200

    Move Debug Toolbar URLs to the end

--- pontoon/urls.py
+++ pontoon/urls.py
@@ -78,7 +78,7 @@ urlpatterns = [
 try:
     import debug_toolbar
 
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
 except ModuleNotFoundError:
     pass  # debug_toolbar not installed, skip it
 
