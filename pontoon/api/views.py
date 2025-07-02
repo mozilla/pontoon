5790823c1ac049024fbd0763f7f2c42ac33f6bda
functionzz <164675620+functionzz@users.noreply.github.com>
2025-07-02 17:21:13 -0400

    removed indents and useless comments

--- pontoon/api/views.py
+++ pontoon/api/views.py
@@ -147,21 +147,18 @@ class ProjectListView(generics.ListAPIView):
         include_disabled = query_params.get("include_disabled")
         include_system = query_params.get("include_system")
 
-        # Base queryset
         queryset = (
             Project.objects.visible()
             .visible_for(self.request.user)
             .prefetch_related("project_locale__locale", "contact", "tags")
         )
-
-        if include_disabled is not None or include_system is not None:
-            filters = Q()
-            if include_disabled is not None:
-                filters |= Q(disabled=True)
-            if include_system is not None:
-                filters |= Q(system_project=True)
+        filters = Q()
+        if include_disabled is not None:
+            filters |= Q(disabled=True)
+        if include_system is not None:
+            filters |= Q(system_project=True)
+        if filters:
             queryset = queryset | Project.objects.filter(filters).distinct()
-
         return queryset.stats_data()
 
 
