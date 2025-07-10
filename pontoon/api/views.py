2a46e04dfcc29a55a3e0b516a933cd13ac3f761c
functionzz <164675620+functionzz@users.noreply.github.com>
2025-07-10 14:28:35 -0400

    added isauthenticated to view

--- pontoon/api/views.py
+++ pontoon/api/views.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
