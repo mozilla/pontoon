5b624afc28e8fd48106ea0f061deaa57ab2d734c
functionzz <164675620+functionzz@users.noreply.github.com>
2025-07-11 10:25:07 -0400

    added authentication logic, CRUD operations, some temporary migrations that I will likely delete

---
+++ pontoon/api/authentication.py
@@ -0,0 +1,43 @@
+import timezone
+from pontoon.api.models import PersonalAccessToken
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import generics
+
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+
+class PersonalAccessTokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            raise AuthenticationFailed(
+                {"detail": "Missing or invalid Authorization header"}
+            )
+
+        try:
+            id, unhashed_token = auth_header.split(" ")[1].split("_")
+        except ValueError:
+            raise AuthenticationFailed({"detail": "Malformed token format"})
+        
+        try:
+            pat = PersonalAccessToken.objects.get(id=id)
+        except PersonalAccessToken.DoesNotExist:
+            raise AuthenticationFailed({"detail": "Invalid token"})
+        
+        if not check_password(unhashed_token, pat.token_hash):
+            raise AuthenticationFailed({"detail": "Invalid token"})
+        
+        if pat.revoked:
+            raise AuthenticationFailed({"detail": "Token has been revoked"})
+        
+        if pat.expires_at and pat.expires_at < timezone.now():
+            raise AuthenticationFailed({"detail": "Token has expired"})
+        
+        user = pat.user
+        return (user, None)
+
+    # create the token model, api endpoints to create, revoke, edit tokens
+    # create the UI to add, delete tokens in user settings
+    # create token expiration, repeated wrong tries
