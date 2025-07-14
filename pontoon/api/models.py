d52972965facebe693a3327e6e4212de112b345f
functionzz <164675620+functionzz@users.noreply.github.com>
2025-07-14 09:56:54 -0400

    added create pat

--- pontoon/api/models.py
+++ pontoon/api/models.py
@@ -6,7 +6,9 @@ from pontoon.base.models.user import User
 class PersonalAccessToken(models.Model):
     token_id = models.CharField(max_length=16, unique=True)
     token_hash = models.CharField(max_length=128)  # SHA-256
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='personal_access_tokens')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="personal_access_tokens"
+    )
     note = models.CharField(max_length=255)
     expires_at = models.DateTimeField()
     revoked = models.BooleanField(default=False)
