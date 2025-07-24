c54a8cbf9e1642a89bfbb76737347d3ca41716b5
functionzz <164675620+functionzz@users.noreply.github.com>
2025-07-24 15:49:09 -0400

    add project filter checks + more comprehensive project checks
    also removed irrelevant isinstance dictionary checks

--- pontoon/api/tests/test_views.py
+++ pontoon/api/tests/test_views.py
@@ -19,7 +19,6 @@ def test_locale():
         f"/api/v2/locales/{locale_a.code}/", HTTP_ACCEPT="application/json"
     )
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
 
     assert response.data == {
         "code": "kg",
@@ -53,7 +52,6 @@ def test_locales(django_assert_num_queries):
     with django_assert_num_queries(3):
         response = APIClient().get("/api/v2/locales/")
 
-    assert isinstance(response.data, dict)
     assert response.status_code == 200
 
     locale_af = {
@@ -93,7 +91,6 @@ def test_project():
         f"/api/v2/projects/{project_a.slug}/", HTTP_ACCEPT="application/json"
     )
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
     assert response.data == {
         "slug": "project_a",
         "name": "Project A",
@@ -120,300 +117,213 @@ def test_project():
 
 
 @pytest.mark.django_db
-def test_projects(
+def test_regular_projects(
     django_assert_num_queries,
 ):
+    ProjectFactory.create_batch(3, disabled=True)
+    ProjectFactory.create_batch(3, system_project=True)
     with django_assert_num_queries(4):
         response = APIClient().get("/api/v2/projects/")
 
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
 
-    project_terminology = {
-        "slug": "terminology",
-        "name": "Terminology",
-        "priority": 1,
-        "deadline": None,
-        "visibility": "public",
-        "contact": None,
-        "info": "A project used to localize terminology.",
-        "system_project": False,
-        "disabled": False,
-        "sync_disabled": True,
-        "pretranslation_enabled": False,
-        "total_strings": 0,
-        "approved_strings": 0,
-        "pretranslated_strings": 0,
-        "strings_with_warnings": 0,
-        "strings_with_errors": 0,
-        "missing_strings": 0,
-        "unreviewed_strings": 0,
-        "complete": True,
-        "tags": [],
-        "locales": [
-            "fur",
-            "af",
-            "pt",
-            "an",
-            "uz",
-            "cy",
-            "ca",
-            "ms",
-            "zu",
-            "km",
-            "da",
-            "ko",
-            "lv",
-            "es-ES",
-            "ast",
-            "son",
-            "fa",
-            "kk",
-            "eo",
-            "nn-NO",
-            "ar",
-            "ne-NP",
-            "ff",
-            "ja",
-            "ta",
-            "nb-NO",
-            "ro",
-            "kn",
-            "mn",
-            "sr",
-            "mai",
-            "es-CL",
-            "eu",
-            "gd",
-            "vi",
-            "is",
-            "hy-AM",
-            "fy-NL",
-            "sl",
-            "si",
-            "el",
-            "es-MX",
-            "es-AR",
-            "sw",
-            "uk",
-            "gl",
-            "nso",
-            "fi",
-            "it",
-            "lt",
-            "hsb",
-            "bs",
-            "ta-LK",
-            "mr",
-            "wo",
-            "xh",
-            "cs",
-            "lij",
-            "bn-IN",
-            "ml",
-            "ak",
-            "bg",
-            "mk",
-            "pl",
-            "te",
-            "or",
-            "hu",
-            "ur",
-            "be",
-            "he",
-            "th",
-            "es",
-            "ru",
-            "ht",
-            "gu-IN",
-            "ilo",
-            "hi-IN",
-            "csb",
-            "fr",
-            "as",
-            "az",
-            "tr",
-            "de",
-            "br",
-            "pa-IN",
-            "ga",
-            "bn-BD",
-            "en-GB",
-            "sk",
-            "oc",
-            "tl",
-            "pt-PT",
-            "nl",
-            "ku",
-            "dsb",
-            "rm",
-            "zh-CN",
-            "pt-BR",
-            "sv-SE",
-            "et",
-            "hr",
-            "ga-IE",
-            "id",
-            "sq",
-            "my",
-            "en-ZA",
-            "ka",
-            "zh-TW",
-        ],
-    }
-    assert project_terminology in response.data["results"]
+    expected_results = [
+        {
+            "slug": p.slug,
+            "name": p.name,
+            "priority": p.priority,
+            "deadline": p.deadline,
+            "visibility": p.visibility,
+            "contact": p.contact,
+            "info": p.info,
+            "system_project": p.system_project,
+            "disabled": p.disabled,
+            "sync_disabled": p.sync_disabled,
+            "pretranslation_enabled": p.pretranslation_enabled,
+            "total_strings": p.total_strings,
+            "approved_strings": p.approved_strings,
+            "pretranslated_strings": p.pretranslated_strings,
+            "strings_with_warnings": p.strings_with_warnings,
+            "strings_with_errors": p.strings_with_errors,
+            "missing_strings": p.missing_strings,
+            "unreviewed_strings": p.unreviewed_strings,
+            "complete": p.complete,
+            "tags": [
+                tag.name for tag in sorted(p.tags.all(), key=lambda tag: tag.name)
+            ],
+            "locales": [
+                locale.code
+                for locale in sorted(p.locales.all(), key=lambda locale: locale.code)
+            ],
+        }
+        for p in sorted(
+            Project.objects.filter(disabled=False, system_project=False),
+            key=lambda p: p.pk,
+        )
+    ]
+
+    for project in response.data["results"]:
+        project["locales"] = sorted(project["locales"])
+        project["tags"] = sorted(project["tags"])
+
+    results = sorted(response.data["results"], key=lambda p: p["slug"])
+    expected_results = sorted(expected_results, key=lambda p: p["slug"])
+
+    # includes Terminology project
+    assert response.data["count"] == 1
+    assert results == expected_results
 
 
 @pytest.mark.django_db
-def test_projects_flags(
+def test_all_projects(
     django_assert_num_queries,
 ):
-    ProjectFactory.create_batch(3, visibility=Project.Visibility.PUBLIC) + list(
-        Project.objects.filter(slug__in=["terminology"])
-    )
     ProjectFactory.create_batch(3, disabled=True)
-    ProjectFactory.create_batch(3, system_project=True) + list(
-        Project.objects.filter(slug__in=["tutorial"])
-    )
+    ProjectFactory.create_batch(3, system_project=True)
     with django_assert_num_queries(4):
         response = APIClient().get("/api/v2/projects/?include_system&include_disabled")
 
-    assert isinstance(response.data, dict)
     assert response.status_code == 200
 
-    project_terminology = {
-        "slug": "terminology",
-        "name": "Terminology",
-        "priority": 1,
-        "deadline": None,
-        "visibility": "public",
-        "contact": None,
-        "info": "A project used to localize terminology.",
-        "system_project": False,
-        "disabled": False,
-        "sync_disabled": True,
-        "pretranslation_enabled": False,
-        "total_strings": 0,
-        "approved_strings": 0,
-        "pretranslated_strings": 0,
-        "strings_with_warnings": 0,
-        "strings_with_errors": 0,
-        "missing_strings": 0,
-        "unreviewed_strings": 0,
-        "complete": True,
-        "tags": [],
-        "locales": [
-            "fur",
-            "af",
-            "pt",
-            "an",
-            "uz",
-            "cy",
-            "ca",
-            "ms",
-            "zu",
-            "km",
-            "da",
-            "ko",
-            "lv",
-            "es-ES",
-            "ast",
-            "son",
-            "fa",
-            "kk",
-            "eo",
-            "nn-NO",
-            "ar",
-            "ne-NP",
-            "ff",
-            "ja",
-            "ta",
-            "nb-NO",
-            "ro",
-            "kn",
-            "mn",
-            "sr",
-            "mai",
-            "es-CL",
-            "eu",
-            "gd",
-            "vi",
-            "is",
-            "hy-AM",
-            "fy-NL",
-            "sl",
-            "si",
-            "el",
-            "es-MX",
-            "es-AR",
-            "sw",
-            "uk",
-            "gl",
-            "nso",
-            "fi",
-            "it",
-            "lt",
-            "hsb",
-            "bs",
-            "ta-LK",
-            "mr",
-            "wo",
-            "xh",
-            "cs",
-            "lij",
-            "bn-IN",
-            "ml",
-            "ak",
-            "bg",
-            "mk",
-            "pl",
-            "te",
-            "or",
-            "hu",
-            "ur",
-            "be",
-            "he",
-            "th",
-            "es",
-            "ru",
-            "ht",
-            "gu-IN",
-            "ilo",
-            "hi-IN",
-            "csb",
-            "fr",
-            "as",
-            "az",
-            "tr",
-            "de",
-            "br",
-            "pa-IN",
-            "ga",
-            "bn-BD",
-            "en-GB",
-            "sk",
-            "oc",
-            "tl",
-            "pt-PT",
-            "nl",
-            "ku",
-            "dsb",
-            "rm",
-            "zh-CN",
-            "pt-BR",
-            "sv-SE",
-            "et",
-            "hr",
-            "ga-IE",
-            "id",
-            "sq",
-            "my",
-            "en-ZA",
-            "ka",
-            "zh-TW",
-        ],
-    }
-    assert project_terminology in response.data["results"]
+    expected_results = [
+        {
+            "slug": p.slug,
+            "name": p.name,
+            "priority": p.priority,
+            "deadline": p.deadline,
+            "visibility": p.visibility,
+            "contact": p.contact,
+            "info": p.info,
+            "system_project": p.system_project,
+            "disabled": p.disabled,
+            "sync_disabled": p.sync_disabled,
+            "pretranslation_enabled": p.pretranslation_enabled,
+            "total_strings": p.total_strings,
+            "approved_strings": p.approved_strings,
+            "pretranslated_strings": p.pretranslated_strings,
+            "strings_with_warnings": p.strings_with_warnings,
+            "strings_with_errors": p.strings_with_errors,
+            "missing_strings": p.missing_strings,
+            "unreviewed_strings": p.unreviewed_strings,
+            "complete": p.complete,
+        }
+        for p in sorted(Project.objects.all(), key=lambda p: p.pk)
+    ]
+
+    for project in response.data["results"]:
+        project.pop("locales", None)
+        project.pop("tags", None)
+
+    results = sorted(response.data["results"], key=lambda p: p["slug"])
+    expected_results = sorted(expected_results, key=lambda p: p["slug"])
+
+    # includes Terminology project
+    assert response.data["count"] == 8
+    assert results == expected_results
+
+
+@pytest.mark.django_db
+def test_system_projects(
+    django_assert_num_queries,
+):
+    ProjectFactory.create_batch(3, disabled=True)
+    ProjectFactory.create_batch(3, system_project=True)
+    with django_assert_num_queries(4):
+        response = APIClient().get("/api/v2/projects/?include_system")
+
+    assert response.status_code == 200
+
+    expected_results = [
+        {
+            "slug": p.slug,
+            "name": p.name,
+            "priority": p.priority,
+            "deadline": p.deadline,
+            "visibility": p.visibility,
+            "contact": p.contact,
+            "info": p.info,
+            "system_project": p.system_project,
+            "disabled": p.disabled,
+            "sync_disabled": p.sync_disabled,
+            "pretranslation_enabled": p.pretranslation_enabled,
+            "total_strings": p.total_strings,
+            "approved_strings": p.approved_strings,
+            "pretranslated_strings": p.pretranslated_strings,
+            "strings_with_warnings": p.strings_with_warnings,
+            "strings_with_errors": p.strings_with_errors,
+            "missing_strings": p.missing_strings,
+            "unreviewed_strings": p.unreviewed_strings,
+            "complete": p.complete,
+        }
+        for p in sorted(
+            Project.objects.filter(system_project=True)
+            | Project.objects.filter(disabled=False, system_project=False),
+            key=lambda p: p.pk,
+        )
+    ]
+
+    for project in response.data["results"]:
+        project.pop("locales", None)
+        project.pop("tags", None)
+
+    results = sorted(response.data["results"], key=lambda p: p["slug"])
+    expected_results = sorted(expected_results, key=lambda p: p["slug"])
+
+    # includes Terminology, Tutorial and 3 system projects
+    assert response.data["count"] == 5
+    assert results == expected_results
+
+
+@pytest.mark.django_db
+def test_disabled_projects(
+    django_assert_num_queries,
+):
+    ProjectFactory.create_batch(3, disabled=True)
+    ProjectFactory.create_batch(3, system_project=True)
+    with django_assert_num_queries(4):
+        response = APIClient().get("/api/v2/projects/?include_disabled")
+
+        assert response.status_code == 200
+
+    expected_results = [
+        {
+            "slug": p.slug,
+            "name": p.name,
+            "priority": p.priority,
+            "deadline": p.deadline,
+            "visibility": p.visibility,
+            "contact": p.contact,
+            "info": p.info,
+            "system_project": p.system_project,
+            "disabled": p.disabled,
+            "sync_disabled": p.sync_disabled,
+            "pretranslation_enabled": p.pretranslation_enabled,
+            "total_strings": p.total_strings,
+            "approved_strings": p.approved_strings,
+            "pretranslated_strings": p.pretranslated_strings,
+            "strings_with_warnings": p.strings_with_warnings,
+            "strings_with_errors": p.strings_with_errors,
+            "missing_strings": p.missing_strings,
+            "unreviewed_strings": p.unreviewed_strings,
+            "complete": p.complete,
+        }
+        for p in sorted(
+            Project.objects.filter(disabled=True)
+            | Project.objects.filter(disabled=False, system_project=False),
+            key=lambda p: p.pk,
+        )
+    ]
+
+    for project in response.data["results"]:
+        project.pop("locales", None)
+        project.pop("tags", None)
+
+    results = sorted(response.data["results"], key=lambda p: p["slug"])
+    expected_results = sorted(expected_results, key=lambda p: p["slug"])
+
+    # includes Terminology and 3 other disabled projects
+    assert response.data["count"] == 4
+    assert results == expected_results
 
 
 @pytest.mark.django_db
@@ -422,7 +332,6 @@ def test_project_locale():
         "/api/v2/af/terminology/", HTTP_ACCEPT="application/json"
     )
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
 
     assert response.data == {
         "locale": {
@@ -505,7 +414,6 @@ def test_terminology_search(django_assert_num_queries):
         response = APIClient().get("/api/v2/search/terminology/?text=open&locale=kg")
 
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
 
     assert response.data == {
         "count": 1,
@@ -563,7 +471,6 @@ def test_tm_search(django_assert_num_queries):
         response = APIClient().get("/api/v2/search/tm/?text=hello&locale=kg")
 
     assert response.status_code == 200
-    assert isinstance(response.data, dict)
 
     assert response.data == {
         "count": 1,
