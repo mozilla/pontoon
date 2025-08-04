e5ffb1f48da5162414d858a4ca67c90477ed6f4b
Eemeli Aro <eemeli@mozilla.com>
2025-08-04 15:44:46 +0300

    Delete additional empty Translation & TranslationMemoryEntry plural gettext values

---
+++ pontoon/base/migrations/0093_delete_more_empty_gettext_plurals.py
@@ -0,0 +1,42 @@
+from django.db import migrations
+from django.utils import timezone
+
+
+def delete_more_empty_gettext_plurals(apps, schema_editor):
+    ActionLog = apps.get_model("actionlog", "ActionLog")
+    User = apps.get_model("auth", "User")
+    Translation = apps.get_model("base", "Translation")
+    TranslationMemoryEntry = apps.get_model("base", "TranslationMemoryEntry")
+
+    now = timezone.now()
+    sync_user = User.objects.get(username="pontoon-sync")
+
+    regex = r"^\.input {\$n :number}\s\.match \$n(\s[a-z*]+ {{}})+$"
+
+    TranslationMemoryEntry.objects.filter(target__regex=regex).delete()
+
+    translations = Translation.objects.filter(string__regex=regex)
+    ActionLog.objects.bulk_create(
+        [
+            ActionLog(
+                action_type="translation:deleted",
+                created_at=now,
+                performed_by=sync_user,
+                entity_id=t.entity_id,
+                locale_id=t.locale_id,
+            )
+            for t in translations
+        ]
+    )
+    translations.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("base", "0092_del_entity_old_key"),
+    ]
+    operations = [
+        migrations.RunPython(
+            delete_more_empty_gettext_plurals, reverse_code=migrations.RunPython.noop
+        ),
+    ]
