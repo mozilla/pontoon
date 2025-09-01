9d65f1774108cf9c52e398b63c2b76c13d8e7831
Eemeli Aro <eemeli@mozilla.com>
2025-09-01 15:50:08 +0300

    Use moz.l10n for simple preview generation, adding gettext support

--- pontoon/base/simple_preview.py
+++ pontoon/base/simple_preview.py
@@ -1,62 +1,51 @@
-from fluent.syntax import FluentParser, ast
-from fluent.syntax.serializer import serialize_expression
+from moz.l10n.formats import Format
+from moz.l10n.formats.fluent import fluent_parse_entry
+from moz.l10n.formats.mf2 import mf2_parse_message
+from moz.l10n.message import serialize_message
+from moz.l10n.model import CatchallKey, Message, PatternMessage, SelectMessage
 
 from pontoon.base.models import Resource
 
 
-parser = FluentParser()
-
-
-def serialize_value(value):
-    """Serialize AST value into a simple string.
-
-    Presumes that the default variant is the most appropriate to use as a generic
-    representation of the message
-    """
-    response = ""
-
-    for element in value.elements:
-        if isinstance(element, ast.TextElement):
-            response += element.value
-
-        elif isinstance(element, ast.Placeable):
-            if isinstance(element.expression, ast.SelectExpression):
-                default_variant = next(
-                    variant
-                    for variant in element.expression.variants
-                    if variant.default
-                )
-                response += serialize_value(default_variant.value)
-            else:
-                response += "{ " + serialize_expression(element.expression) + " }"
-
-    return response
-
-
 def get_simple_preview(format: str, string: str):
     """
-    Return content without modifications if it isn't a valid Fluent message.
+    Flatten a message entry as a simple string.
 
-    Turn a Fluent message into a simple string, without any syntax sigils. Return the
-    most pertinent content that can be found in the message, without the ID, attributes
-    or selectors.
+    For multi-variant messages, selects the fallback variant.
+
+    For Fluent, selects the value if it's not empty,
+    or the first non-empty attribute.
     """
-    if format != Resource.Format.FLUENT:
-        return string
+    try:
+        match format:
+            case Resource.Format.FLUENT:
+                entry = fluent_parse_entry(string, with_linepos=False)
+                if not entry.value.is_empty():
+                    msg = entry.value
+                else:
+                    msg = next(
+                        prop
+                        for prop in entry.properties.values()
+                        if not prop.is_empty()
+                    )
+                flat_msg = as_pattern_message(msg)
+                return serialize_message(Format.fluent, flat_msg)
+            case Resource.Format.GETTEXT:
+                msg = mf2_parse_message(string)
+                flat_msg = as_pattern_message(msg)
+                return serialize_message(None, flat_msg)
+    except Exception:
+        pass
+    return string
 
-    translation_ast = parser.parse_entry(string)
 
-    # Non-FTL string or string with an error
-    if isinstance(translation_ast, ast.Junk):
-        return string
-
-    # Value: use entire AST
-    if translation_ast.value:
-        tree = translation_ast
-
-    # Attributes (must be present in valid AST if value isn't):
-    # use AST of the first attribute
+def as_pattern_message(msg: Message) -> PatternMessage:
+    if isinstance(msg, SelectMessage):
+        default_pattern = next(
+            pattern
+            for keys, pattern in msg.variants.items()
+            if all(isinstance(key, CatchallKey) for key in keys)
+        )
+        return PatternMessage(default_pattern)
     else:
-        tree = translation_ast.attributes[0]
-
-    return serialize_value(tree.value)
+        return msg
--- pontoon/base/tests/test_simple_preview.py
+++ pontoon/base/tests/test_simple_preview.py
@@ -1,76 +1,110 @@
-from collections import OrderedDict
+from textwrap import dedent
 
 import pytest
-
-from fluent.syntax import FluentParser
 
 from pontoon.base.simple_preview import get_simple_preview
 
 
-parser = FluentParser()
+FLUENT_TRANSLATION_TESTS = {
+    "empty": ("", ""),
+    "non-ftl": ("Simple string", "Simple string"),
+    "simple": ("key = Simple string", "Simple string"),
+    "multiline": (
+        dedent("""\
+                    key =
+                        Simple String
+                        In Multiple
+                        Lines
+                    """),
+        "Simple String\nIn Multiple\nLines",
+    ),
+    "plural": (
+        dedent("""\
+                    key =
+                        { $number ->
+                            [1] Simple String
+                        *[other] Other Simple String
+                        }
+                    """),
+        "Other Simple String",
+    ),
+    "wrapped-expression": (
+        dedent("""\
+                    key =
+                        Anne liked your comment on { $photo_count ->
+                            [male] his
+                            [female] her
+                        *[other] their
+                        } post.
+                    """),
+        "Anne liked your comment on their post.",
+    ),
+    "attribute": (
+        dedent("""\
+                    key =
+                        .placeholder = Simple String
+                    """),
+        "Simple String",
+    ),
+    "attributes": (
+        dedent("""\
+                    key =
+                        .attribute = Simple String
+                        .other = Other Simple String
+                    """),
+        "Simple String",
+    ),
+    "attributes-select-expression": (
+        dedent("""\
+                    key =
+                        .placeholder = { PLATFORM() ->
+                            [win] Simple String
+                            *[other] Other Simple String
+                        }
+                    """),
+        "Other Simple String",
+    ),
+    "variable-reference": ("key = { $variable }", "{ $variable }"),
+    "message-reference": ("key = { message }", "{ message }"),
+    "message-reference-attribute": ("key = { foo.bar }", "{ foo.bar }"),
+    "term-reference": ("key = { -term }", "{ -term }"),
+    "function-reference": (
+        'warning-upgrade = { LINK("Link text", title: "Link title") }Simple String',
+        '{ LINK("Link text", title: "Link title") }Simple String',
+    ),
+    "string-literal": ('key = { "" }', '{ "" }'),
+    "number-literal": ("key = { 1 }", "{ 1 }"),
+}
 
 
-MULTILINE_SOURCE = """key =
-    Simple String
-    In Multiple
-    Lines"""
-PLURAL_SOURCE = """key =
-    { $number ->
-        [1] Simple String
-       *[other] Other Simple String
-    }"""
-WRAPPED_SELECT_SOURCE = """key =
-    Anne liked your comment on { $photo_count ->
-        [male] his
-        [female] her
-       *[other] their
-    } post."""
-ATTRIBUTE_SOURCE = """key =
-    .placeholder = Simple String"""
-ATTRIBUTES_SOURCE = """key =
-    .attribute = Simple String
-    .other = Other Simple String"""
-ATTRIBUTE_SELECT_SOURCE = """key =
-    .placeholder = { PLATFORM() ->
-        [win] Simple String
-        *[other] Other Simple String
-    }"""
-
-SIMPLE_TRANSLATION_TESTS = OrderedDict(
-    (
-        ("empty", ("", "")),
-        ("non-ftl", ("Simple string", "Simple string")),
-        ("simple", ("key = Simple string", "Simple string")),
-        ("multiline", (MULTILINE_SOURCE, "Simple String\nIn Multiple\nLines")),
-        ("plural", (PLURAL_SOURCE, "Other Simple String")),
-        (
-            "wrapped-expression",
-            (WRAPPED_SELECT_SOURCE, "Anne liked your comment on their post."),
-        ),
-        ("attribute", (ATTRIBUTE_SOURCE, "Simple String")),
-        ("attributes", (ATTRIBUTES_SOURCE, "Simple String")),
-        (
-            "attributes-select-expression",
-            (ATTRIBUTE_SELECT_SOURCE, "Other Simple String"),
-        ),
-        ("variable-reference", ("key = { $variable }", "{ $variable }")),
-        ("message-reference", ("key = { message }", "{ message }")),
-        ("message-reference-attribute", ("key = { foo.bar }", "{ foo.bar }")),
-        ("term-reference", ("key = { -term }", "{ -term }")),
-        (
-            "function-reference",
-            (
-                'warning-upgrade = { LINK("Link text", title: "Link title") }Simple String',
-                '{ LINK("Link text", title: "Link title") }Simple String',
-            ),
-        ),
-        ("string-literal", ('key = { "" }', '{ "" }')),
-        ("number-literal", ("key = { 1 }", "{ 1 }")),
-    )
-)
+GETTEXT_TRANSLATION_TESTS = {
+    "empty": ("", ""),
+    "simple": ("Simple string", "Simple string"),
+    "escape": (r"Literals \{ \}", "Literals { }"),
+    "multiline": (
+        "Simple String\nIn Multiple\nLines",
+        "Simple String\nIn Multiple\nLines",
+    ),
+    "select": (
+        dedent("""\
+            .input {$n :number}
+            .match $n
+            1 {{Simple String}}
+            * {{Other Simple String}}
+        """),
+        "Other Simple String",
+    ),
+    "variable-reference": ("text {$x @source=|$y|}", "text $y"),
+}
 
 
-@pytest.mark.parametrize("k", SIMPLE_TRANSLATION_TESTS)
-def test_get_simple_preview(k):
-    string, expected = SIMPLE_TRANSLATION_TESTS[k]
+@pytest.mark.parametrize("name", FLUENT_TRANSLATION_TESTS)
+def test_fluent_simple_preview(name):
+    string, expected = FLUENT_TRANSLATION_TESTS[name]
     assert get_simple_preview("fluent", string) == expected
+
+
+@pytest.mark.parametrize("name", GETTEXT_TRANSLATION_TESTS)
+def test_gettext_simple_preview(name):
+    string, expected = GETTEXT_TRANSLATION_TESTS[name]
+    assert get_simple_preview("gettext", string) == expected
