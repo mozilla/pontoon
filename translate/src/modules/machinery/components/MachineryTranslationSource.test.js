730b42cfcd9276292eaad21480ac24b54d337e84
Eemeli Aro <eemeli@mozilla.com>
2025-09-04 14:07:56 +0300

    Drop jest-each, replace its use with a for loop

--- translate/src/modules/machinery/components/MachineryTranslationSource.test.js
+++ translate/src/modules/machinery/components/MachineryTranslationSource.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import each from 'jest-each';
 
 import { MachineryTranslationSource } from './MachineryTranslationSource';
 
@@ -9,22 +8,24 @@ const DEFAULT_TRANSLATION = {
 };
 
 describe('<MachineryTranslationSource>', () => {
-  each([
+  for (const [type, component] of [
     ['translation-memory', 'TranslationMemory'],
     ['google-translate', 'GoogleTranslation'],
     ['microsoft-translator', 'MicrosoftTranslation'],
     ['microsoft-terminology', 'MicrosoftTerminology'],
     ['caighdean', 'CaighdeanTranslation'],
-  ]).it('renders `%s` type for `%s` component correctly', (type, component) => {
-    const translation = {
-      sources: [type],
-    };
-    const wrapper = shallow(
-      <MachineryTranslationSource translation={translation} />,
-    );
+  ]) {
+    it(`renders ${type} type for ${component} component correctly`, () => {
+      const translation = {
+        sources: [type],
+      };
+      const wrapper = shallow(
+        <MachineryTranslationSource translation={translation} />,
+      );
 
-    expect(wrapper.find(component)).toHaveLength(1);
-  });
+      expect(wrapper.find(component)).toHaveLength(1);
+    });
+  }
 
   it('shows several sources', () => {
     const translation = {
