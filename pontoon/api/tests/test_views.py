1b22e99ea5df32fc348e2562578a43f52329a534
Jamie <164675620+functionzz@users.noreply.github.com>
2025-09-24 11:59:47 -0400

    Update pontoon/api/tests/test_views.py
    Co-authored-by: Matjaž Horvat <matjaz.horvat@gmail.com>

--- pontoon/api/tests/test_views.py
+++ pontoon/api/tests/test_views.py
@@ -1254,7 +1254,7 @@ def test_translation_search(django_assert_num_queries):
         },
     ]
 
-    # Search multiple parameters
+    # Test search with multiple parameters
     with django_assert_num_queries(7):
         response = APIClient().get(
             f"/api/v2/search/translations/?locale={locale_a.code}&project={project_a.slug}&text=the%20Test&search_match_whole_word=True&search_match_case=True",
