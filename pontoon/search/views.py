8167d3f42801f953b075ae73155475594fac5b99
functionzz <164675620+functionzz@users.noreply.github.com>
2025-10-08 10:03:15 -0400

    edit entity view to use internal drf views

--- pontoon/search/views.py
+++ pontoon/search/views.py
@@ -1,34 +1,68 @@
-import requests
+from rest_framework.request import Request
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
+from pontoon.api.views import EntityIndividualView
 from pontoon.base.models.entity import Entity
 
 
 BASE_URL = "http://localhost:8000/api/v2"
 
 
+def translation_search(request):
+    """Get corresponding entity given entity."""
+    expected_params = {
+        "search": None,
+        "p": None,
+        "l": None,
+        "identifiers": None,
+        "match_case": None,
+        "match_word": None,
+    }
+
+    # Extract query parameters dynamically
+    query_params = {key: request.GET.get(key, default) for key, default in expected_params.items()}
+
+    # Example: Use the extracted parameters
+    if query_params["search"]:
+        # Do something with query_params["search"]
+        pass
+
+    # Build the API URL or perform further logic
+    api_url = f"{BASE_URL}/search/translations/"
+    return render(request, "search/results.html", {"query_params": query_params})
+
+
+
+
 def entity(request, pk):
     """Get corresponding entity given entity."""
-    api_url = f"{BASE_URL}/entities/{pk}/?include_translations"
-    res = requests.get(api_url)
 
-    if res.status_code == 200:
-        entity = res.json()
+    # Modify the request object to include translations
+    drf_request = Request(request)
+    query_params = drf_request.query_params.copy()
+    query_params["include_translations"] = ""
+    drf_request._request.GET = query_params
+
+    entity_view = EntityIndividualView.as_view()
+    response = entity_view(request, pk=pk)
+
+    if response.status_code == 200:
+        entity_data = response.data
         return render(
             request,
             "search/entity.html",
             {
-                "entity": entity.get("entity", []),
-                "project": entity.get("project", []),
-                "resource": entity.get("resource", []),
-                "translations": entity.get("translations", []),
+                "entity": entity_data.get("entity", []),
+                "project": entity_data.get("project", []),
+                "resource": entity_data.get("resource", []),
+                "translations": entity_data.get("translations", []),
             },
         )
     else:
-        raise Http404
+        raise Http404("Entity not found")
 
 
 def entity_alternate(request, project, resource, entity):
