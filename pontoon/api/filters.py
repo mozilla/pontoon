bc29c836ab4d0464adb9b70ec0622bac6eded5c1
Matjaž Horvat <matjaz.horvat@gmail.com>
2025-11-04 16:22:19 +0100

    Exclude private projects from TM search endpoint (#3820)
    * Simplify TM and Terminology API views
    * Exclude private projects from TM search endpoint

--- pontoon/api/filters.py
+++ pontoon/api/filters.py
@@ -3,6 +3,7 @@ from django_filters import CharFilter, FilterSet
 from django.db.models import Q
 
 from pontoon.base.models import (
+    Project,
     TranslationMemoryEntry,
 )
 from pontoon.terminology.models import (
@@ -11,8 +12,8 @@ from pontoon.terminology.models import (
 
 
 class TermFilter(FilterSet):
-    text = CharFilter(method="filter_text")
-    locale = CharFilter(method="filter_locale")
+    text = CharFilter(method="filter_text", required=True)
+    locale = CharFilter(method="filter_locale", required=True)
 
     class Meta:
         model = Term
@@ -26,8 +27,8 @@ class TermFilter(FilterSet):
 
 
 class TranslationMemoryFilter(FilterSet):
-    text = CharFilter(method="filter_text")
-    locale = CharFilter(method="filter_locale")
+    text = CharFilter(method="filter_text", required=True)
+    locale = CharFilter(method="filter_locale", required=True)
 
     class Meta:
         model = TranslationMemoryEntry
@@ -40,3 +41,8 @@ class TranslationMemoryFilter(FilterSet):
 
     def filter_locale(self, queryset, name, value):
         return queryset.filter(locale__code=value)
+
+    @property
+    def qs(self):
+        qs = super().qs
+        return qs.exclude(project__visibility=Project.Visibility.PRIVATE)
