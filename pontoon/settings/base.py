570c615a911ff480127a561214598bfedbaeb9d5
functionzz <164675620+functionzz@users.noreply.github.com>
2025-11-10 11:49:16 -0500

    add clipboard

--- pontoon/settings/base.py
+++ pontoon/settings/base.py
@@ -769,6 +769,7 @@ PIPELINE_JS = {
         "source_filenames": (
             "js/search.js",
             "js/team_selector.js",
+            "js/lib/clipboard.min.js",
         ),
         "output_filename": "js/search.min.js",
     },
