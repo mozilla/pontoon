a9d674060190bd03f3345067400d0d45b2119f67
functionzz <164675620+functionzz@users.noreply.github.com>
2025-11-18 16:12:15 -0500

    reinstate auth

--- pontoon/api/views.py
+++ pontoon/api/views.py
@@ -17,7 +17,7 @@ from django.views.decorators.http import require_GET
 
 from pontoon.actionlog.models import ActionLog
 
-# from pontoon.api.authentication import PersonalAccessTokenAuthentication
+from pontoon.api.authentication import PersonalAccessTokenAuthentication
 from pontoon.api.filters import TermFilter, TranslationMemoryFilter
 from pontoon.base import forms
 from pontoon.base.models import (
@@ -515,7 +515,7 @@ class PretranslationView(APIView):
         locale = generics.get_object_or_404(Locale, code=locale)
 
         project = SimpleNamespace(slug="temp-project")
-        resource = SimpleNamespace(project=project, format=string_format or "no-format")
+        resource = SimpleNamespace(project=project, format=string_format or None)
         entity = SimpleNamespace(resource=resource, string=text)
 
         try:
