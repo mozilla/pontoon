45ff5590af7d69e7242b0b791e268fb98994a8ff
nishit mistry <92210055+nishitmistry@users.noreply.github.com>
2025-12-08 00:07:28 +0530

    Add vistest configuration & CI action (#3845)
    * rename test files requiring jsx extension
    * change node types version to 22

---
+++ translate/vitest.config.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    coverage: {
+      reportOnFailure: true,
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+    },
+    setupFiles: ['./src/setupTests.ts'],
+    mockReset: true,
+    testTimeout: 10000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)$/,
+        replacement: path.resolve(import.meta.dirname, 'src/$1'),
+      },
+      {
+        find: /\.svg$/,
+        replacement: path.resolve(import.meta.dirname, '__mocks__/svg.js'),
+      },
+    ],
+  },
+});
