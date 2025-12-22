bd54ec7298e567b8f8560a9e1900f86f79eee0b5
functionzz <164675620+functionzz@users.noreply.github.com>
2025-12-22 12:14:06 -0500

    refactor provider urlpatterns due to get_list() deprecation
    https://github.com/pennersr/django-allauth/blob/0770788e9dc68a65a94262e5cbf90fce8ee73c5a/allauth/urls.py#L12

--- pontoon/allauth_urls.py
+++ pontoon/allauth_urls.py
@@ -6,12 +6,29 @@ views and don't allow user to tamper with the state of an account.
 
 import importlib
 
+from typing import List, Union
+
 from allauth.account import views as account_views
 from allauth.socialaccount import providers, views as socialaccount_views
 
 from django.conf import settings
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import URLPattern, URLResolver, path
+
+
+def build_provider_urlpatterns() -> List[Union[URLPattern, URLResolver]]:
+    provider_urlpatterns: List[Union[URLPattern, URLResolver]] = []
+    provider_classes = providers.registry.get_class_list()
+
+    provider_classes = [
+        cls for cls in provider_classes if cls.id != "openid_connect"
+    ] + [cls for cls in provider_classes if cls.id == "openid_connect"]
+    for provider_class in provider_classes:
+        prov_mod = importlib.import_module(provider_class.get_package() + ".urls")
+        prov_urlpatterns = getattr(prov_mod, "urlpatterns", None)
+        if prov_urlpatterns:
+            provider_urlpatterns += prov_urlpatterns
+    return provider_urlpatterns
 
 
 if settings.AUTHENTICATION_METHOD == "django":
@@ -24,7 +41,7 @@ if settings.AUTHENTICATION_METHOD == "django":
         ),
     ]
 else:
-    urlpatterns = [
+    urlpatterns: List[Union[URLPattern, URLResolver]] = [
         path("login/", account_views.login, name="account_login"),
         path("logout/", account_views.logout, name="account_logout"),
         path("inactive/", account_views.account_inactive, name="account_inactive"),
@@ -39,12 +56,4 @@ else:
             name="socialaccount_login_error",
         ),
     ]
-
-for provider in providers.registry.get_list():
-    try:
-        prov_mod = importlib.import_module(provider.get_package() + ".urls")
-    except ImportError:
-        continue
-    prov_urlpatterns = getattr(prov_mod, "urlpatterns", None)
-    if prov_urlpatterns:
-        urlpatterns += prov_urlpatterns
+    urlpatterns += build_provider_urlpatterns()
