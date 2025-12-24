4031120cb08580c157fedac9253d1b275aa968f3
Nishit Mistry <nishitmistry94@gmail.com>
2025-12-24 12:00:18 +0530

    ported Hooks tests to vitest

--- translate/src/hooks/usePluralExamples.test.js
+++ translate/src/hooks/usePluralExamples.test.js
@@ -1,38 +1,44 @@
 /* eslint-disable no-console */
 
 import React from 'react';
-import sinon from 'sinon';
 import { usePluralExamples } from './usePluralExamples';
+import { vi } from 'vitest';
 
 describe('usePluralExamples', () => {
   beforeAll(() => {
-    sinon.stub(console, 'error');
-    sinon.stub(React, 'useMemo').callsFake((cb) => cb());
+    vi.mock('react', async (importOriginal) => {
+      const actual = await importOriginal();
+      return {
+        ...actual,
+        useMemo: (cb) => cb(),
+      };
+    });
   });
-  afterEach(() => console.error.resetHistory());
+
   afterAll(() => {
-    console.error.restore();
-    React.useMemo.restore();
+    vi.restoreAllMocks()
   });
 
   it('returns a map of Slovenian plural examples', () => {
+    const consoleErrorSpy = vi.spyOn(console, 'error');
     const res = usePluralExamples({
-      cldrPlurals: [1, 2, 3, 5],
-      pluralRule:
-        '(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3)',
-    });
+        cldrPlurals: [1, 2, 3, 5],
+        pluralRule:
+          '(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3)',
+      });
 
     expect(res).toEqual({ 1: 1, 2: 2, 3: 3, 5: 0 });
-    expect(console.error.callCount).toBe(0);
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(0);
   });
 
   it('prevents infinite loop if locale plurals are not configured properly', () => {
+    const consoleErrorSpy = vi.spyOn(console, 'error');
     const res = usePluralExamples({
       cldrPlurals: [0, 1, 2, 3, 4, 5],
       pluralRule: '(n != 1)',
     });
 
     expect(res).toEqual({ 0: 1, 1: 2 });
-    expect(console.error.callCount).toBe(1);
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
   });
 });
