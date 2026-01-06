42aba162287ea0a49b7b8fd9f663ac38209db6ff
functionzz <164675620+functionzz@users.noreply.github.com>
2026-01-06 00:56:22 -0500

    update deprecated urls

--- pontoon/allauth_urls.py
+++ pontoon/allauth_urls.py
@@ -4,31 +4,15 @@ Because of concerns related to the security concerns it's a better to keep only 
 views and don't allow user to tamper with the state of an account.
 """
 
-import importlib
-
 from typing import List, Union
 
 from allauth.account import views as account_views
-from allauth.socialaccount import providers, views as socialaccount_views
+from allauth.socialaccount import views as socialaccount_views
+from allauth.urls import build_provider_urlpatterns
 
 from django.conf import settings
 from django.contrib.auth import views
 from django.urls import URLPattern, URLResolver, path
-
-
-def build_provider_urlpatterns() -> List[Union[URLPattern, URLResolver]]:
-    provider_urlpatterns: List[Union[URLPattern, URLResolver]] = []
-    provider_classes = providers.registry.get_class_list()
-
-    provider_classes = [
-        cls for cls in provider_classes if cls.id != "openid_connect"
-    ] + [cls for cls in provider_classes if cls.id == "openid_connect"]
-    for provider_class in provider_classes:
-        prov_mod = importlib.import_module(provider_class.get_package() + ".urls")
-        prov_urlpatterns = getattr(prov_mod, "urlpatterns", None)
-        if prov_urlpatterns:
-            provider_urlpatterns += prov_urlpatterns
-    return provider_urlpatterns
 
 
 if settings.AUTHENTICATION_METHOD == "django":
@@ -46,12 +30,12 @@ else:
         path("logout/", account_views.logout, name="account_logout"),
         path("inactive/", account_views.account_inactive, name="account_inactive"),
         path(
-            "social/login/cancelled/",
+            "/login/cancelled/",
             socialaccount_views.login_cancelled,
             name="socialaccount_login_cancelled",
         ),
         path(
-            "social/login/error/",
+            "/login/error/",
             socialaccount_views.login_error,
             name="socialaccount_login_error",
         ),
