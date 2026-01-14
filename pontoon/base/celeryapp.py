4d3a366f22075ca48cc526edbfea27f06a0cfd19
Matjaž Horvat <matjaz.horvat@gmail.com>
2026-01-14 10:18:08 +0100

    Prevent Celery log message duplication

--- pontoon/base/celeryapp.py
+++ pontoon/base/celeryapp.py
@@ -2,7 +2,10 @@ import os
 
 import dotenv
 
+import logging.config
+
 from celery import Celery
+from celery.signals import setup_logging
 
 
 # Read dotenv file and inject its values into the environment
@@ -20,3 +23,12 @@ app = Celery("pontoon")
 # INSTALLED_APPS.
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@setup_logging.connect
+def configure_celery_logging(*args, **kwargs):
+    """
+    Prevent Celery from configuring its own logging (which adds its handler /
+    TaskFormatter), and use Django's LOGGING config instead.
+    """
+    logging.config.dictConfig(settings.LOGGING)
