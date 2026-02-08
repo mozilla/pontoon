f2586d7ce29ccfa4c8baaa871467efe3ef920a19
Francesco Lodolo <flod@lodolo.net>
2026-02-08 09:00:11 +0100

    Memoize log

--- translate/src/hooks/useLogUXAction.ts
+++ translate/src/hooks/useLogUXAction.ts
@@ -2,6 +2,8 @@ import { useAppSelector } from '~/hooks';
 import { USER } from '~/modules/user';
 import { logUXAction } from '../api/uxaction';
 
+import { useCallback } from 'react';
+
 export function useLogUXAction(): (
   action_type: string,
   experiment: string | null,
@@ -11,8 +13,11 @@ export function useLogUXAction(): (
     Boolean(state[USER].isAuthenticated),
   );
 
-  return (action_type, experiment, data) => {
-    if (!isAuthenticated) return;
-    return logUXAction(action_type, experiment, data);
-  };
+  return useCallback(
+    (action_type, experiment, data) => {
+      if (!isAuthenticated) return;
+      return logUXAction(action_type, experiment, data);
+    },
+    [isAuthenticated],
+  );
 }
