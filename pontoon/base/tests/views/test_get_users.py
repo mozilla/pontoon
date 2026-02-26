61e5baacf2b9168710ffd159067f5675db77c451
Serah Nderi <sarahngima77@gmail.com>
2026-02-26 21:52:36 +0300

    Fix test for system exclusion

--- pontoon/base/tests/views/test_get_users.py
+++ pontoon/base/tests/views/test_get_users.py
@@ -2,6 +2,8 @@ import json
 
 import pytest
 
+from django.contrib.auth.models import User
+
 from pontoon.base.views import get_users
 from pontoon.test.factories import (
     EntityFactory,
@@ -20,7 +22,10 @@ def test_get_users_excludes_system_users(rf, admin):
     response = get_users(request)
     data = json.loads(response.content)
     usernames = [u["username"] for u in data]
-    assert "system" not in usernames
+
+    system_users = User.objects.filter(profile__system_user=True)
+    for u in system_users:
+        assert u.username not in usernames
 
 
 @pytest.mark.django_db
@@ -33,8 +38,10 @@ def test_get_users_sorted_by_locale_activity(rf, admin):
     other_locale = LocaleFactory()
 
     # create a user that is from a different locale, not the current one
-    other_locale_user = UserFactory(first_name="Michael", last_name="OtherLocale")
-    TranslationFactory(user=other_locale_user, locale=other_locale, entity=entity)
+    other_locale_user = UserFactory(
+        first_name="Michael", last_name="OtherLocale")
+    TranslationFactory(user=other_locale_user,
+                       locale=other_locale, entity=entity)
 
     # create a user active in the current locale
     locale_user = UserFactory(first_name="Alice", last_name="ActiveLocale")
