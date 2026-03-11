1ce2ed32ed77c37abb970ee9e791d63bf2d33d53
Matjaž Horvat <matjaz.horvat@gmail.com>
2026-03-11 20:46:53 +0100

    Remove unused file

--- pontoon/static/js/errors/index.js
+++
@@ -1,8 +0,0 @@
-export class NotImplementedError extends Error {
-  constructor(...args) {
-    super(...args);
-    Error.captureStackTrace(this, NotImplementedError);
-  }
-}
-
-NotImplementedError.prototype = Error.prototype;
