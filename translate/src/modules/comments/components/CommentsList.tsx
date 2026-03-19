b4937a7ac434eae23a564e2e61aeff4ede67b956
Serah Nderi <sarahngima77@gmail.com>
2026-03-19 22:04:58 +0300

    Fixing identation in translation comments

--- translate/src/modules/comments/components/CommentsList.tsx
+++ translate/src/modules/comments/components/CommentsList.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import type { HistoryTranslation } from '~/api/translation';
 import type { UserState } from '~/modules/user';
-import { useAddCommentAndRefresh, useDeleteCommentAndRefresh } from '../hooks';
+import {
+  useAddCommentAndRefresh,
+  useEditCommentAndRefresh,
+  useDeleteCommentAndRefresh,
+} from '../hooks';
 
 import { AddComment } from './AddComment';
 import { Comment } from './Comment';
@@ -19,6 +23,8 @@ export function CommentsList({
 }: Props): React.ReactElement<'div'> {
   const onAddComment = useAddCommentAndRefresh(translation);
   const onDeleteComment = useDeleteCommentAndRefresh(translation);
+  const onEditComment = useEditCommentAndRefresh(translation);
+
   return (
     <div className='comments-list'>
       <section className='all-comments'>
@@ -28,6 +34,7 @@ export function CommentsList({
               comment={comment}
               key={comment.id}
               onDeleteComment={onDeleteComment}
+              onEditComment={onEditComment}
               canEdit={user.username === comment.username}
               canDelete={user.username === comment.username || user.isPM}
               user={user}
