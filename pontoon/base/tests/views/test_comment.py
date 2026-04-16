fcbd4b43996c9a634a4c7f3a3e63f89646106156
Matjaž Horvat <matjaz.horvat@gmail.com>
2026-04-16 17:50:18 +0200

    Add tests

--- pontoon/base/tests/views/test_comment.py
+++ pontoon/base/tests/views/test_comment.py
@@ -8,6 +8,51 @@ from pontoon.test.factories import TranslationCommentFactory
 
 
 @pytest.mark.django_db
+def test_add_comment(member, translation_a):
+    url = reverse("pontoon.add_comment")
+
+    response = member.client.post(
+        url,
+        {
+            "translation": translation_a.pk,
+            "entity": translation_a.entity.pk,
+            "locale": translation_a.locale.code,
+            "comment": "new comment",
+        },
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+    )
+
+    assert response.status_code == 200
+
+    comment = Comment.objects.get(translation=translation_a, author=member.user)
+    assert comment.content == "new comment"
+
+
+@pytest.mark.django_db
+def test_add_comment_sanitizes_html(member, entity_a, locale_a, project_locale_a):
+    url = reverse("pontoon.add_comment")
+
+    payload = "<svg><script>alert(1)</script>safe"
+
+    response = member.client.post(
+        url,
+        {
+            "entity": entity_a.pk,
+            "locale": locale_a.code,
+            "comment": payload,
+        },
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+    )
+
+    assert response.status_code == 200
+
+    comment = Comment.objects.latest("id")
+
+    assert "<script>" not in comment.content
+    assert "safe" in comment.content
+
+
+@pytest.mark.django_db
 def test_pin_comment(member, client, comment_a):
     url = reverse("pontoon.pin_comment")
 
@@ -66,10 +111,10 @@ def test_unpin_comment(member, client, team_comment_a):
 
 
 @pytest.mark.django_db
-def test_edit_comment(member, client, comment_a):
+def test_edit_comment(member, comment_a):
     url = reverse("pontoon.edit_comment")
 
-    # a user cannot edit someone else's comment
+    # A user cannot edit someone else's comment
     response = member.client.post(
         url,
         {"comment_id": comment_a.pk, "content": "edited"},
@@ -91,6 +136,34 @@ def test_edit_comment(member, client, comment_a):
     assert response.status_code == 200
     comment_a.refresh_from_db()
     assert comment_a.content == "edited content"
+
+
+@pytest.mark.django_db
+def test_edit_comment_sanitizes_html(member, comment_a):
+    url = reverse("pontoon.edit_comment")
+
+    # Make member the author
+    comment_a.author = member.user
+    comment_a.save()
+
+    payload = "<img src=x onerror=alert(1)>safe"
+
+    response = member.client.post(
+        url,
+        {"comment_id": comment_a.pk, "content": payload},
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+    )
+
+    assert response.status_code == 200
+
+    comment_a.refresh_from_db()
+
+    # Dangerous part should be removed
+    assert "onerror" not in comment_a.content
+    assert "<img" not in comment_a.content
+
+    # Safe content should remain
+    assert "safe" in comment_a.content
 
 
 @pytest.mark.django_db
