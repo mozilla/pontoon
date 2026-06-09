651bacf45941ac01836ed2d91f6d7f91eff9b455
Ayush Ranjan <ayshushus@gmail.com>
2026-06-08 23:44:54 -0400

    update editor-theme application and light-mode editor styling

--- translate/src/modules/editor/components/Editor.tsx
+++ translate/src/modules/editor/components/Editor.tsx
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { ThemeContext } from '~/context/Theme';
 import { TranslationForm } from '~/modules/translationform';
 
 import './Editor.css';
@@ -8,18 +7,11 @@ import { EditorMenu } from './EditorMenu';
 import { NewContributorTooltip } from './NewContributorTooltip';
 import { MachinerySourceIndicator } from './MachinerySourceIndicator';
 
-export const Editor = () => {
-  const { editorTheme } = useContext(ThemeContext);
-  const overrideClass =
-    editorTheme === 'dark' || editorTheme === 'light'
-      ? `${editorTheme}-theme`
-      : '';
-  return (
-    <div className={`editor ${overrideClass}`.trim()}>
-      <NewContributorTooltip />
-      <TranslationForm />
-      <MachinerySourceIndicator />
-      <EditorMenu />
-    </div>
-  );
-};
+export const Editor = () => (
+  <div className='editor'>
+    <NewContributorTooltip />
+    <TranslationForm />
+    <MachinerySourceIndicator />
+    <EditorMenu />
+  </div>
+);
