d13c0949d61876e675e0dd0b6e67a43e53218080
Francesco Lodolo <flod@lodolo.net>
2026-07-26 11:15:03 +0200

    Fix pontoon-sync system_user status
    Also add tests that would have caught the issue.

---
+++ pontoon/base/tests/test_system_users.py
@@ -0,0 +1,31 @@
+import pytest
+
+from pontoon.base import system_users
+from pontoon.base.models.user import User
+from pontoon.base.system_users import (
+    get_pretranslation_authors,
+    get_pretranslation_user_pks,
+    get_sync_user,
+)
+
+
+@pytest.mark.django_db
+def test_system_users_exist_and_are_flagged():
+    """Every account in ALL must exist and be marked as a system user."""
+    for email in system_users.ALL:
+        user = User.objects.get(email=email)
+        assert user.profile.system_user, f"{email} is not marked as a system user"
+
+
+@pytest.mark.django_db
+def test_pretranslation_authors_are_system_users():
+    """Pretranslation authors must be a subset of the known system users."""
+    assert set(system_users.PRETRANSLATION_AUTHORS.values()) <= set(system_users.ALL)
+
+
+@pytest.mark.django_db
+def test_lookups_return_the_expected_users(sync_user, gt_user, tm_user):
+    """The lookups must return the accounts the constants point at."""
+    assert get_sync_user() == sync_user
+    assert get_pretranslation_authors() == {"gt": gt_user, "tm": tm_user}
+    assert get_pretranslation_user_pks() == {gt_user.pk, tm_user.pk}
