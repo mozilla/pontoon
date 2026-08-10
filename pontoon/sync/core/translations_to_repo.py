324878714ea80c33253dc78155bfc4cb7508e3ef
Denys <mr.denis.goncharov@gmail.com>
2026-08-10 12:00:10 +0200

    Fix co-authoring in commit messages (#4378)

--- pontoon/sync/core/translations_to_repo.py
+++ pontoon/sync/core/translations_to_repo.py
@@ -103,9 +103,10 @@ def sync_translations_to_repo(
     if translators:
         commit_msg += "\n"
         for translator, lc_set in translators.items():
-            tr_str = translator.display_name_and_email
+            tr_name = translator.display_name
+            tr_email = translator.email
             lc_str = ", ".join(sorted(lc_set))
-            commit_msg += f"\nCo-authored-by: {tr_str} ({lc_str})"
+            commit_msg += f"\nCo-authored-by: {tr_name} ({lc_str}) <{tr_email}>"
 
     commit_author = f"{settings.VCS_SYNC_NAME} <{settings.VCS_SYNC_EMAIL}>"
 
