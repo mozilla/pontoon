42555760baef21b2249876a5242ddc8c3d27e5a6
Francesco Lodolo <flod@lodolo.net>
2026-09-15 21:20:33 +0200

    Restore optimization (address comment)

--- pontoon/checks/libraries/custom.py
+++ pontoon/checks/libraries/custom.py
@@ -319,7 +319,7 @@ def require_placeholders_match(
         for pm in ph_re.finditer(pat_src):
             rest = pat_src[pm.start() :]
             for ph in src_ph_strings:
-                if rest.startswith(ph):
+                if pat_src.startswith(ph, pm.start()):
                     found_ph.add(ph)
                     break
             else:
